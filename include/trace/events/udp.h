7451167fa40be564ed276591b639b5a9bf1e9fe3
Xiongchun Duan <duanxiongchun@bytedance.com>
2019-03-03 19:04:22 +0800

    velinux: add send recv length tracepoint in udp and tcp
    Signed-off-by: Kunkun Li <likunkun@bytedance.com>

--- include/trace/events/udp.h
+++ include/trace/events/udp.h
@@ -27,6 +27,43 @@ TRACE_EVENT(udp_fail_queue_rcv_skb,
 	TP_printk("rc=%d port=%hu", __entry->rc, __entry->lport)
 );
 
+DECLARE_EVENT_CLASS(udp_stream_length,
+
+    TP_PROTO(struct sock *sk, int length, int error, int flags),
+
+    TP_ARGS(sk, length, error, flags),
+
+    TP_STRUCT__entry(
+        __field(void *, sk)
+        __field(int, length)
+        __field(int, error)
+        __field(int, flags)
+    ),
+
+    TP_fast_assign(
+        __entry->sk = sk;
+        __entry->length = length;
+        __entry->error = error;
+        __entry->flags = flags;
+    ),
+
+    TP_printk("sk address = %p, length = %d, error=%d, flags = %u ",
+        __entry->sk, __entry->length, __entry->error, __entry->flags)
+);
+
+DEFINE_EVENT(udp_stream_length, udp_send_length,
+    TP_PROTO(struct sock *sk, int length, int error, int flags),
+
+    TP_ARGS(sk, length, error, flags)
+);
+
+DEFINE_EVENT(udp_stream_length, udp_recv_length,
+    TP_PROTO(struct sock *sk, int length, int error, int flags),
+
+    TP_ARGS(sk, length, error, flags)
+);
+
+
 #endif /* _TRACE_UDP_H */
 
 /* This part must be outside protection */
