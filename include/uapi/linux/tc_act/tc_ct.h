a4c62e750dc6c6381c546ab1f376213a7e894dce
Roi Dayan <roid@mellanox.com>
2019-03-10 09:21:50 +0200

    net/sched: Introduce action CT
    Signed-off-by: Roi Dayan <roid@mellanox.com>
    Signed-off-by: logan <logan@bytedance.com>
    Signed-off-by: Kunkun Li <likunkun@bytedance.com>

---
+++ include/uapi/linux/tc_act/tc_ct.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef __UAPI_TC_CT_H
+#define __UAPI_TC_CT_H
+
+#include <linux/types.h>
+#include <linux/pkt_cls.h>
+#include <stdbool.h>
+
+#define TCA_ACT_CONNTRACK 18
+
+struct tc_conntrack {
+	tc_gen;
+	__u16 zone;
+	__u32 labels[4];
+	__u32 labels_mask[4];
+	__u32 mark;
+	__u32 mark_mask;
+	bool commit;
+};
+
+enum {
+	TCA_CONNTRACK_UNSPEC,
+	TCA_CONNTRACK_PARMS,
+	TCA_CONNTRACK_TM,
+	TCA_CONNTRACK_PAD,
+	__TCA_CONNTRACK_MAX
+};
+#define TCA_CONNTRACK_MAX (__TCA_CONNTRACK_MAX - 1)
+
+#endif /* __UAPI_TC_CT_H */
