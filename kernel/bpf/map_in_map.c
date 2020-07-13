5227e6493010bc894f2126edc47d596a5ebd3acc
zangchunxin <zangchunxin@bytedance.com>
2020-07-13 22:35:49 +0800

    velinux: suport bpf reuseport quic
    Signed-off-by: Kunkun Li <likunkun@bytedance.com>

--- kernel/bpf/map_in_map.c
+++ kernel/bpf/map_in_map.c
@@ -81,8 +81,8 @@ bool bpf_map_meta_equal(const struct bpf_map *meta0,
 	return meta0->map_type == meta1->map_type &&
 		meta0->key_size == meta1->key_size &&
 		meta0->value_size == meta1->value_size &&
-		meta0->map_flags == meta1->map_flags &&
-		meta0->max_entries == meta1->max_entries;
+		meta0->map_flags == meta1->map_flags;
+		/*meta0->max_entries == meta1->max_entries;*/
 }
 
 void *bpf_map_fd_get_ptr(struct bpf_map *map,
