242202c47d82d44546124282d47ffdf9122b9fad
Jens Axboe <axboe@kernel.dk>
2020-10-03 10:49:22 -0600

    tracehook: clear TIF_NOTIFY_RESUME in tracehook_notify_resume()
    All the callers currently do this, clean it up and move the clearing
    into tracehook_notify_resume() instead.
    
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 3c532798ec96b6c2d77706f04ed1d8b566a805df)
    Signed-off-by: Chunxin Zang <zangchunxin@bytedance.com>

--- arch/csky/kernel/signal.c
+++ arch/csky/kernel/signal.c
@@ -256,7 +256,6 @@ asmlinkage void do_notify_resume(struct pt_regs *regs,
 		do_signal(regs);
 
 	if (thread_info_flags & _TIF_NOTIFY_RESUME) {
-		clear_thread_flag(TIF_NOTIFY_RESUME);
 		tracehook_notify_resume(regs);
 	}
 }
--- arch/nios2/kernel/signal.c
+++ arch/nios2/kernel/signal.c
@@ -316,7 +316,7 @@ asmlinkage int do_notify_resume(struct pt_regs *regs)
 			 */
 			return restart;
 		}
-	} else if (test_and_clear_thread_flag(TIF_NOTIFY_RESUME))
+	} else if (test_thread_flag(TIF_NOTIFY_RESUME))
 		tracehook_notify_resume(regs);
 
 	return 0;
