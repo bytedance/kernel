e9d8c2822e7a866cef9b0391bb81b369f9aa285b
Jens Axboe <axboe@kernel.dk>
2020-10-22 20:09:23 -0600

    arm64: add support for TIF_NOTIFY_SIGNAL
    Wire up TIF_NOTIFY_SIGNAL handling for arm64.
    
    Cc: linux-arm-kernel@lists.infradead.org
    Acked-by: Will Deacon <will@kernel.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 192caabd4dd92c98d23ed4334d7596af05af2fb4)
    Signed-off-by: Chunxin Zang <zangchunxin@bytedance.com>

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -914,7 +914,7 @@ asmlinkage void do_notify_resume(struct pt_regs *regs,
 			if (thread_flags & _TIF_UPROBE)
 				uprobe_notify_resume(regs);
 
-			if (thread_flags & _TIF_SIGPENDING)
+			if (thread_flags & (_TIF_SIGPENDING | _TIF_NOTIFY_SIGNAL))
 				do_signal(regs);
 
 			if (thread_flags & _TIF_NOTIFY_RESUME) {
