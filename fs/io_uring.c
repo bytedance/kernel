ac09af43118b44d0ce8485e0c30cb4f9720f4adf
Pavel Begunkov <asml.silence@gmail.com>
2020-11-05 14:06:19 +0000

    io_uring: don't forget to task-cancel drained reqs
    If there is a long-standing request of one task locking up execution of
    deferred requests, and the defer list contains requests of another task
    (all files-less), then a potential execution of __io_uring_task_cancel()
    by that another task will sleep until that first long-standing request
    completion, and that may take long.
    
    E.g.
    tsk1: req1/read(empty_pipe) -> tsk2: req(DRAIN)
    Then __io_uring_task_cancel(tsk2) waits for req1 completion.
    
    It seems we even can manufacture a complicated case with many tasks
    sharing many rings that can lock them forever.
    
    Cancel deferred requests for __io_uring_task_cancel() as well.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit ef9865a442286e2737f37f56eb54c12ef8465905)
    Signed-off-by: Chunxin Zang <zangchunxin@bytedance.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -8501,6 +8501,7 @@ static void io_attempt_cancel(struct io_ring_ctx *ctx, struct io_kiocb *req)
 }
 
 static void io_cancel_defer_files(struct io_ring_ctx *ctx,
+				  struct task_struct *task,
 				  struct files_struct *files)
 {
 	struct io_defer_entry *de = NULL;
@@ -8508,7 +8509,8 @@ static void io_cancel_defer_files(struct io_ring_ctx *ctx,
 
 	spin_lock_irq(&ctx->completion_lock);
 	list_for_each_entry_reverse(de, &ctx->defer_list, list) {
-		if (io_match_files(de->req, files)) {
+		if (io_task_match(de->req, task) &&
+		    io_match_files(de->req, files)) {
 			list_cut_position(&list, &ctx->defer_list, &de->list);
 			break;
 		}
@@ -8534,7 +8536,6 @@ static bool io_uring_cancel_files(struct io_ring_ctx *ctx,
 	if (list_empty_careful(&ctx->inflight_list))
 		return false;
 
-	io_cancel_defer_files(ctx, files);
 	/* cancel all at once, should be faster than doing it one by one*/
 	io_wq_cancel_cb(ctx->io_wq, io_wq_files_match, files, true);
 
@@ -8625,6 +8626,11 @@ static void io_uring_cancel_task_requests(struct io_ring_ctx *ctx,
 		atomic_inc(&task->io_uring->in_idle);
 		io_sq_thread_park(ctx->sq_data);
 	}
+
+	if (files)
+		io_cancel_defer_files(ctx, NULL, files);
+	else
+		io_cancel_defer_files(ctx, task, NULL);
 
 	io_cqring_overflow_flush(ctx, true, task, files);
 
