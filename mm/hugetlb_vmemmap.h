47b0ca35122f32e156434f8a9bab63db75dfc11b
Muchun Song <songmuchun@bytedance.com>
2020-11-23 11:55:43 +0800

    mm/hugetlb: Introduce pgtable allocation/freeing helpers
    On some architectures (e.g. x86_64, arm64), vmemmap is always PMD
    mapped if the machine has hugepages support. If we want to free the
    unused vmemmap pages, we have to split the huge PMD firstly. Before
    splitting the PMD page table, we should pre-allocate pgtable prepare
    for the PMD splitting.
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Kunkun Li <likunkun@bytedance.com>

--- mm/hugetlb_vmemmap.h
+++ mm/hugetlb_vmemmap.h
@@ -15,6 +15,9 @@ void alloc_huge_page_vmemmap(struct hstate *h, struct page *head);
 void free_huge_page_vmemmap(struct hstate *h, struct page *head);
 void hugetlb_vmemmap_init(struct hstate *h);
 
+int vmemmap_pgtable_prealloc(struct hstate *h, struct list_head *pgtables);
+void vmemmap_pgtable_free(struct list_head *pgtables);
+
 /*
  * How many vmemmap pages associated with a HugeTLB page that can be freed
  * to the buddy allocator. The checking of the is_power_of_2() aims to let
@@ -30,6 +33,16 @@ static inline void alloc_huge_page_vmemmap(struct hstate *h, struct page *head)
 }
 
 static inline void free_huge_page_vmemmap(struct hstate *h, struct page *head)
+{
+}
+
+static inline int vmemmap_pgtable_prealloc(struct hstate *h,
+					   struct list_head *pgtables)
+{
+	return 0;
+}
+
+static inline void vmemmap_pgtable_free(struct list_head *pgtables)
 {
 }
 
