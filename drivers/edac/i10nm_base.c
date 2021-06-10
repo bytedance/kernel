intel/mainline-tracking

EDAC, i10nm: Retrieve and print retry_rd_err_log registers
Retrieve and print retry_rd_err_log registers as like the previous commit:
commit e80634a75aba ("EDAC, skx: Retrieve and print retry_rd_err_log registers")
Add a module parameter retry_rd_err_log(default=0):
- 0: Don't print retry_rd_err_log registers.
- 1: Enable retry_rd_err_log when loading driver.
     Print retry_rd_err_log registers when error occurs.
     Restore retry_rd_err_log default configuration when unloading driver.

Co-developed-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
Signed-off-by: Youquan Song <youquan.song@intel.com>

drivers/edac/i10nm_base.c
@@ -32,13 +32,125 @@
 	readl((m)->mbase + 0x20ef8 + (i) * (m)->chan_mmio_sz)
 #define I10NM_GET_AMAP(m, i)		\
 	readl((m)->mbase + 0x20814 + (i) * (m)->chan_mmio_sz)
+#define I10NM_GET_REG(m, i, offset)	\
+	readl((m)->mbase + (i) * (m)->chan_mmio_sz + (offset))
+#define I10NM_SET_REG(m, i, offset, v)	\
+	writel(v, (m)->mbase + (i) * (m)->chan_mmio_sz + (offset))
 
 #define I10NM_GET_SCK_MMIO_BASE(reg)	(GET_BITFIELD(reg, 0, 28) << 23)
 #define I10NM_GET_IMC_MMIO_OFFSET(reg)	(GET_BITFIELD(reg, 0, 10) << 12)
 #define I10NM_GET_IMC_MMIO_SIZE(reg)	((GET_BITFIELD(reg, 13, 23) - \
 					 GET_BITFIELD(reg, 0, 10) + 1) << 12)
 
+#define RETRY_RD_ERR_LOG_BIT0		BIT(0)
+#define RETRY_RD_ERR_LOG_BIT1		BIT(1)
+#define RETRY_RD_ERR_LOG_BIT14		BIT(14)
+#define RETRY_RD_ERR_LOG_BIT15		BIT(15)
+#define RETRY_RD_ERR_LOG_BIT14_1	(BIT(14) | BIT(1))
+
 static struct list_head *i10nm_edac_list;
+static int retry_rd_err_log;
+
+static u32 offsets_scrub[]  = {0x22c60, 0x22c54, 0x22c5c, 0x22c58, 0x22c28, 0x20ed8, 0x20edc};
+static u32 offsets_demand[] = {0x22e54, 0x22e60, 0x22e64, 0x22e58, 0x22e5c, 0x20ee0, 0x20ee4};
+
+static void __enable_retry_rd_err_log(struct skx_imc *imc, int chan, bool enable)
+{
+	u32 s, d;
+
+	if (!imc->mbase)
+		return;
+
+	s = I10NM_GET_REG(imc, chan, offsets_scrub[0]);
+	d = I10NM_GET_REG(imc, chan, offsets_demand[0]);
+
+	if (enable) {
+		/* Save default configurations */
+		imc->chan[chan].retry_rd_err_log_s = s;
+		imc->chan[chan].retry_rd_err_log_d = d;
+
+		s &= ~RETRY_RD_ERR_LOG_BIT14_1;
+		s |=  RETRY_RD_ERR_LOG_BIT15;
+		d &= ~RETRY_RD_ERR_LOG_BIT14_1;
+		d |=  RETRY_RD_ERR_LOG_BIT15;
+	} else {
+		/* Restore default configurations */
+		if (imc->chan[chan].retry_rd_err_log_s & RETRY_RD_ERR_LOG_BIT1)
+			s |=  RETRY_RD_ERR_LOG_BIT1;
+		if (imc->chan[chan].retry_rd_err_log_s & RETRY_RD_ERR_LOG_BIT14)
+			s |=  RETRY_RD_ERR_LOG_BIT14;
+		if (!(imc->chan[chan].retry_rd_err_log_s & RETRY_RD_ERR_LOG_BIT15))
+			s &= ~RETRY_RD_ERR_LOG_BIT15;
+		if (imc->chan[chan].retry_rd_err_log_d & RETRY_RD_ERR_LOG_BIT1)
+			d |=  RETRY_RD_ERR_LOG_BIT1;
+		if (imc->chan[chan].retry_rd_err_log_d & RETRY_RD_ERR_LOG_BIT14)
+			d |=  RETRY_RD_ERR_LOG_BIT14;
+		if (!(imc->chan[chan].retry_rd_err_log_d & RETRY_RD_ERR_LOG_BIT15))
+			d &= ~RETRY_RD_ERR_LOG_BIT15;
+	}
+
+	I10NM_SET_REG(imc, chan, offsets_scrub[0], s);
+	I10NM_SET_REG(imc, chan, offsets_demand[0], d);
+}
+
+static void enable_retry_rd_err_log(bool enable)
+{
+	struct skx_dev *d;
+	int i, j;
+
+	list_for_each_entry(d, i10nm_edac_list, list)
+		for (i = 0; i < I10NM_NUM_IMC; i++)
+			for (j = 0; j < I10NM_NUM_CHANNELS; j++)
+				__enable_retry_rd_err_log(&d->imc[i], j, enable);
+}
+
+static void show_retry_rd_err_log(struct decoded_addr *res, char *msg,
+				  int len, bool scrub_err)
+{
+	struct skx_imc *imc = &res->dev->imc[res->imc];
+	u32 log0, log1, log2, log3, log4;
+	u32 corr0, corr1, corr2, corr3;
+	u32 *offsets;
+	u64 log5;
+	int n;
+
+	offsets = scrub_err ? offsets_scrub : offsets_demand;
+
+	log0 = I10NM_GET_REG(imc, res->channel, offsets[0]);
+	if (!(log0 & RETRY_RD_ERR_LOG_BIT0)) {
+		pr_warn_once("Invalid retry_rd_err_log contents. Need to disable eMCA in BIOS menu.\n");
+		return;
+	}
+
+	log1 = I10NM_GET_REG(imc, res->channel, offsets[1]);
+	log2 = I10NM_GET_REG(imc, res->channel, offsets[2]);
+	log3 = I10NM_GET_REG(imc, res->channel, offsets[3]);
+	log4 = I10NM_GET_REG(imc, res->channel, offsets[4]);
+	log5 = I10NM_GET_REG(imc, res->channel, offsets[5]);
+	log5 |= (u64)I10NM_GET_REG(imc, res->channel, offsets[6]) << 32;
+
+	n = snprintf(msg, len, " retry_rd_err_log[%.8x %.8x %.8x %.8x %.8x %.16llx]",
+		     log0, log1, log2, log3, log4, log5);
+
+	corr0 = I10NM_GET_REG(imc, res->channel, 0x22c18);
+	corr1 = I10NM_GET_REG(imc, res->channel, 0x22c1c);
+	corr2 = I10NM_GET_REG(imc, res->channel, 0x22c20);
+	corr3 = I10NM_GET_REG(imc, res->channel, 0x22c24);
+
+	if (len - n > 0)
+		snprintf(msg + n, len - n,
+			 " correrrcnt[%.4x %.4x %.4x %.4x %.4x %.4x %.4x %.4x]",
+			 corr0 & 0xffff, corr0 >> 16,
+			 corr1 & 0xffff, corr1 >> 16,
+			 corr2 & 0xffff, corr2 >> 16,
+			 corr3 & 0xffff, corr3 >> 16);
+
+	/* Clear uncorrectable error status bit */
+	if (log0 & RETRY_RD_ERR_LOG_BIT1) {
+		log0 &= ~RETRY_RD_ERR_LOG_BIT1;
+		I10NM_SET_REG(imc, res->channel, offsets[0], log0);
+	}
+}
 
 static struct pci_dev *pci_get_dev_wrapper(int dom, unsigned int bus,
 					   unsigned int dev, unsigned int fun)
@@ -336,6 +448,11 @@ static int __init i10nm_init(void)
 	mce_register_decode_chain(&i10nm_mce_dec);
 	setup_i10nm_debug();
 
+	if (retry_rd_err_log) {
+		skx_set_decode(NULL, show_retry_rd_err_log);
+		enable_retry_rd_err_log(true);
+	}
+
 	i10nm_printk(KERN_INFO, "%s\n", I10NM_REVISION);
 
 	return 0;
@@ -347,6 +464,10 @@ fail:
 static void __exit i10nm_exit(void)
 {
 	edac_dbg(2, "\n");
+
+	if (retry_rd_err_log)
+		enable_retry_rd_err_log(false);
+
 	teardown_i10nm_debug();
 	mce_unregister_decode_chain(&i10nm_mce_dec);
 	skx_adxl_put();
@@ -355,6 +476,9 @@ static void __exit i10nm_exit(void)
 
 module_init(i10nm_init);
 module_exit(i10nm_exit);
+
+module_param(retry_rd_err_log, int, 0444);
+MODULE_PARM_DESC(retry_rd_err_log, "retry_rd_err_log: 0=default,1=enable(also need to disable eMCA in BIOS menu)");
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("MC Driver for Intel 10nm server processors");

bytedance/kernel

The patch is not upstream and shared to Intel customers under NDA.
Backport summary: backport to kernel 5.4.49 for ICX EDAC to report
retry_rd_err_log values.
Retrieve and print retry_rd_err_log registers like the earlier change:
This is a little trickier than on Skylake because of potential
interference with BIOS use of the same registers. The default
behavior is to ignore these registers.
A module parameter retry_rd_err_log(default=0) controls the mode of operation:
- 0=off  : Default.
- 1=bios : Linux doesn't reset any control bits, but just reports values.
           This is "no harm" mode, but it may miss reporting some data.
- 2=linux: Linux tries to take control and resets mode bits,
           clears valid/UC bits after reading. This should be
           more reliable (especially if BIOS interference is reduced
           by disabling eMCA reporting mode in BIOS setup).
Signed-off-by: Kunkun Li <likunkun@bytedance.com>

drivers/edac/i10nm_base.c
@@ -32,13 +32,119 @@
 	readl((m)->mbase + 0x20ef8 + (i) * (m)->chan_mmio_sz)
 #define I10NM_GET_AMAP(m, i)		\
 	readl((m)->mbase + 0x20814 + (i) * (m)->chan_mmio_sz)
+#define I10NM_GET_REG(m, i, offset)	\
+	readl((m)->mbase + (i) * (m)->chan_mmio_sz + (offset))
+#define I10NM_SET_REG(m, i, offset, v)	\
+	writel(v, (m)->mbase + (i) * (m)->chan_mmio_sz + (offset))
 
 #define I10NM_GET_SCK_MMIO_BASE(reg)	(GET_BITFIELD(reg, 0, 28) << 23)
 #define I10NM_GET_IMC_MMIO_OFFSET(reg)	(GET_BITFIELD(reg, 0, 10) << 12)
 #define I10NM_GET_IMC_MMIO_SIZE(reg)	((GET_BITFIELD(reg, 13, 23) - \
 					 GET_BITFIELD(reg, 0, 10) + 1) << 12)
 
+#define RETRY_RD_ERR_LOG_UC		BIT(1)
+#define RETRY_RD_ERR_LOG_NOOVER		BIT(14)
+#define RETRY_RD_ERR_LOG_EN		BIT(15)
+#define RETRY_RD_ERR_LOG_NOOVER_UC	(BIT(14) | BIT(1))
+
 static struct list_head *i10nm_edac_list;
+static int retry_rd_err_log;
+
+static u32 offsets_scrub[]  = {0x22c60, 0x22c54, 0x22c5c, 0x22c58, 0x22c28, 0x20ed8, 0x20edc};
+static u32 offsets_demand[] = {0x22e54, 0x22e60, 0x22e64, 0x22e58, 0x22e5c, 0x20ee0, 0x20ee4};
+
+static void __enable_retry_rd_err_log(struct skx_imc *imc, int chan, bool enable)
+{
+	u32 s, d;
+
+	if (!imc->mbase)
+		return;
+
+	s = I10NM_GET_REG(imc, chan, offsets_scrub[0]);
+	d = I10NM_GET_REG(imc, chan, offsets_demand[0]);
+
+	if (enable) {
+		/* Save default configurations */
+		imc->chan[chan].retry_rd_err_log_s = s;
+		imc->chan[chan].retry_rd_err_log_d = d;
+
+		s &= ~RETRY_RD_ERR_LOG_NOOVER_UC;
+		s |=  RETRY_RD_ERR_LOG_EN;
+		d &= ~RETRY_RD_ERR_LOG_NOOVER_UC;
+		d |=  RETRY_RD_ERR_LOG_EN;
+	} else {
+		/* Restore default configurations */
+		if (imc->chan[chan].retry_rd_err_log_s & RETRY_RD_ERR_LOG_UC)
+			s |=  RETRY_RD_ERR_LOG_UC;
+		if (imc->chan[chan].retry_rd_err_log_s & RETRY_RD_ERR_LOG_NOOVER)
+			s |=  RETRY_RD_ERR_LOG_NOOVER;
+		if (!(imc->chan[chan].retry_rd_err_log_s & RETRY_RD_ERR_LOG_EN))
+			s &= ~RETRY_RD_ERR_LOG_EN;
+		if (imc->chan[chan].retry_rd_err_log_d & RETRY_RD_ERR_LOG_UC)
+			d |=  RETRY_RD_ERR_LOG_UC;
+		if (imc->chan[chan].retry_rd_err_log_d & RETRY_RD_ERR_LOG_NOOVER)
+			d |=  RETRY_RD_ERR_LOG_NOOVER;
+		if (!(imc->chan[chan].retry_rd_err_log_d & RETRY_RD_ERR_LOG_EN))
+			d &= ~RETRY_RD_ERR_LOG_EN;
+	}
+
+	I10NM_SET_REG(imc, chan, offsets_scrub[0], s);
+	I10NM_SET_REG(imc, chan, offsets_demand[0], d);
+}
+
+static void enable_retry_rd_err_log(bool enable)
+{
+	struct skx_dev *d;
+	int i, j;
+
+	list_for_each_entry(d, i10nm_edac_list, list)
+		for (i = 0; i < I10NM_NUM_IMC; i++)
+			for (j = 0; j < I10NM_NUM_CHANNELS; j++)
+				__enable_retry_rd_err_log(&d->imc[i], j, enable);
+}
+
+static void show_retry_rd_err_log(struct decoded_addr *res, char *msg,
+				  int len, bool scrub_err)
+{
+	struct skx_imc *imc = &res->dev->imc[res->imc];
+	u32 log0, log1, log2, log3, log4;
+	u32 corr0, corr1, corr2, corr3;
+	u32 *offsets;
+	u64 log5;
+	int n;
+
+	offsets = scrub_err ? offsets_scrub : offsets_demand;
+
+	log0 = I10NM_GET_REG(imc, res->channel, offsets[0]);
+	log1 = I10NM_GET_REG(imc, res->channel, offsets[1]);
+	log2 = I10NM_GET_REG(imc, res->channel, offsets[2]);
+	log3 = I10NM_GET_REG(imc, res->channel, offsets[3]);
+	log4 = I10NM_GET_REG(imc, res->channel, offsets[4]);
+	log5 = I10NM_GET_REG(imc, res->channel, offsets[5]);
+	log5 |= (u64)I10NM_GET_REG(imc, res->channel, offsets[6]) << 32;
+
+	n = snprintf(msg, len, " retry_rd_err_log[%.8x %.8x %.8x %.8x %.8x %.16llx]",
+		     log0, log1, log2, log3, log4, log5);
+
+	corr0 = I10NM_GET_REG(imc, res->channel, 0x22c18);
+	corr1 = I10NM_GET_REG(imc, res->channel, 0x22c1c);
+	corr2 = I10NM_GET_REG(imc, res->channel, 0x22c20);
+	corr3 = I10NM_GET_REG(imc, res->channel, 0x22c24);
+
+	if (len - n > 0)
+		snprintf(msg + n, len - n,
+			 " correrrcnt[%.4x %.4x %.4x %.4x %.4x %.4x %.4x %.4x]",
+			 corr0 & 0xffff, corr0 >> 16,
+			 corr1 & 0xffff, corr1 >> 16,
+			 corr2 & 0xffff, corr2 >> 16,
+			 corr3 & 0xffff, corr3 >> 16);
+
+	/* Clear uncorrectable error status bit */
+	if (retry_rd_err_log == 2 && (log0 & RETRY_RD_ERR_LOG_UC)) {
+		log0 &= ~RETRY_RD_ERR_LOG_UC;
+		I10NM_SET_REG(imc, res->channel, offsets[0], log0);
+	}
+}
 
 static struct pci_dev *pci_get_dev_wrapper(int dom, unsigned int bus,
 					   unsigned int dev, unsigned int fun)
@@ -339,6 +445,12 @@ static int __init i10nm_init(void)
 	mce_register_decode_chain(&i10nm_mce_dec);
 	setup_i10nm_debug();
 
+	if (retry_rd_err_log) {
+		skx_set_decode(NULL, show_retry_rd_err_log);
+		if (retry_rd_err_log == 2)
+			enable_retry_rd_err_log(true);
+	}
+
 	i10nm_printk(KERN_INFO, "%s\n", I10NM_REVISION);
 
 	return 0;
@@ -350,6 +462,10 @@ fail:
 static void __exit i10nm_exit(void)
 {
 	edac_dbg(2, "\n");
+
+	if (retry_rd_err_log == 2)
+		enable_retry_rd_err_log(false);
+
 	teardown_i10nm_debug();
 	mce_unregister_decode_chain(&i10nm_mce_dec);
 	skx_adxl_put();
@@ -358,6 +474,9 @@ static void __exit i10nm_exit(void)
 
 module_init(i10nm_init);
 module_exit(i10nm_exit);
+
+module_param(retry_rd_err_log, int, 0444);
+MODULE_PARM_DESC(retry_rd_err_log, "retry_rd_err_log: 0=off(default), 1=bios(Linux doesn't reset any control bits, but just reports values.), 2=linux(Linux tries to take control and resets mode bits, clear valid/UC bits after reading.)");
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("MC Driver for Intel 10nm server processors");