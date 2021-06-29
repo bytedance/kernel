dbd576b79b334dc86d0d667f475723809a1c42a5
Roman Gushchin <guro@fb.com>
2021-06-28 19:36:03 -0700

    writeback, cgroup: release dying cgwbs by switching attached inodes
    commit c22d70a162d3cc177282c4487be4d54876ca55c8 upstream.
    
    Asynchronously try to release dying cgwbs by switching attached inodes to
    the nearest living ancestor wb.  It helps to get rid of per-cgroup
    writeback structures themselves and of pinned memory and block cgroups,
    which are significantly larger structures (mostly due to large per-cpu
    statistics data).  This prevents memory waste and helps to avoid different
    scalability problems caused by large piles of dying cgroups.
    
    Reuse the existing mechanism of inode switching used for foreign inode
    detection.  To speed things up batch up to 115 inode switching in a single
    operation (the maximum number is selected so that the resulting struct
    inode_switch_wbs_context can fit into 1024 bytes).  Because every
    switching consists of two steps divided by an RCU grace period, it would
    be too slow without batching.  Please note that the whole batch counts as
    a single operation (when increasing/decreasing isw_nr_in_flight).  This
    allows to keep umounting working (flush the switching queue), however
    prevents cleanups from consuming the whole switching quota and effectively
    blocking the frn switching.
    
    A cgwb cleanup operation can fail due to different reasons (e.g.  not
    enough memory, the cgwb has an in-flight/pending io, an attached inode in
    a wrong state, etc).  In this case the next scheduled cleanup will make a
    new attempt.  An attempt is made each time a new cgwb is offlined (in
    other words a memcg and/or a blkcg is deleted by a user).  In the future
    an additional attempt scheduled by a timer can be implemented.
    
    [guro@fb.com: replace open-coded "115" with arithmetic]
      Link: https://lkml.kernel.org/r/YMEcSBcq/VXMiPPO@carbon.dhcp.thefacebook.com
    [guro@fb.com: add smp_mb() to inode_prepare_wbs_switch()]
      Link: https://lkml.kernel.org/r/YMFa+guFw7OFjf3X@carbon.dhcp.thefacebook.com
    [willy@infradead.org: fix documentation]
      Link: https://lkml.kernel.org/r/20210615200242.1716568-2-willy@infradead.org
    
    Link: https://lkml.kernel.org/r/20210608230225.2078447-9-guro@fb.com
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Acked-by: Tejun Heo <tj@kernel.org>
    Acked-by: Dennis Zhou <dennis@kernel.org>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Dave Chinner <dchinner@redhat.com>
    Cc: Jan Kara <jack@suse.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>

--- mm/backing-dev.c
+++ mm/backing-dev.c
@@ -385,9 +385,8 @@ static void wb_exit(struct bdi_writeback *wb)
 #include <linux/memcontrol.h>
 
 /*
- * cgwb_lock protects bdi->cgwb_tree, bdi->cgwb_congested_tree,
- * blkcg->cgwb_list, and memcg->cgwb_list.  bdi->cgwb_tree is also RCU
- * protected.
+ * cgwb_lock protects bdi->cgwb_tree, blkcg->cgwb_list, offline_cgwbs and
+ * memcg->cgwb_list.  bdi->cgwb_tree is also RCU protected.
  */
 static DEFINE_SPINLOCK(cgwb_lock);
 static struct workqueue_struct *cgwb_release_wq;
@@ -477,6 +476,9 @@ void wb_congested_put(struct bdi_writeback_congested *congested)
 	spin_unlock_irqrestore(&cgwb_lock, flags);
 	kfree(congested);
 }
+static LIST_HEAD(offline_cgwbs);
+static void cleanup_offline_cgwbs_workfn(struct work_struct *work);
+static DECLARE_WORK(cleanup_offline_cgwbs_work, cleanup_offline_cgwbs_workfn);
 
 static void cgwb_release_workfn(struct work_struct *work)
 {
@@ -496,6 +498,11 @@ static void cgwb_release_workfn(struct work_struct *work)
 
 	fprop_local_destroy_percpu(&wb->memcg_completions);
 	percpu_ref_exit(&wb->refcnt);
+
+	spin_lock_irq(&cgwb_lock);
+	list_del(&wb->offline_node);
+	spin_unlock_irq(&cgwb_lock);
+
 	wb_exit(wb);
 	WARN_ON_ONCE(!list_empty(&wb->b_attached));
 	kfree_rcu(wb, rcu);
@@ -515,6 +522,7 @@ static void cgwb_kill(struct bdi_writeback *wb)
 	WARN_ON(!radix_tree_delete(&wb->bdi->cgwb_tree, wb->memcg_css->id));
 	list_del(&wb->memcg_node);
 	list_del(&wb->blkcg_node);
+	list_add(&wb->offline_node, &offline_cgwbs);
 	percpu_ref_kill(&wb->refcnt);
 }
 
@@ -737,6 +745,54 @@ static void cgwb_bdi_unregister(struct backing_dev_info *bdi)
 	mutex_unlock(&bdi->cgwb_release_mutex);
 }
 
+/*
+ * cleanup_offline_cgwbs_workfn - try to release dying cgwbs
+ *
+ * Try to release dying cgwbs by switching attached inodes to the nearest
+ * living ancestor's writeback. Processed wbs are placed at the end
+ * of the list to guarantee the forward progress.
+ */
+static void cleanup_offline_cgwbs_workfn(struct work_struct *work)
+{
+	struct bdi_writeback *wb;
+	LIST_HEAD(processed);
+
+	spin_lock_irq(&cgwb_lock);
+
+	while (!list_empty(&offline_cgwbs)) {
+		wb = list_first_entry(&offline_cgwbs, struct bdi_writeback,
+				      offline_node);
+		list_move(&wb->offline_node, &processed);
+
+		/*
+		 * If wb is dirty, cleaning up the writeback by switching
+		 * attached inodes will result in an effective removal of any
+		 * bandwidth restrictions, which isn't the goal.  Instead,
+		 * it can be postponed until the next time, when all io
+		 * will be likely completed.  If in the meantime some inodes
+		 * will get re-dirtied, they should be eventually switched to
+		 * a new cgwb.
+		 */
+		if (wb_has_dirty_io(wb))
+			continue;
+
+		if (!wb_tryget(wb))
+			continue;
+
+		spin_unlock_irq(&cgwb_lock);
+		while (cleanup_offline_cgwb(wb))
+			cond_resched();
+		spin_lock_irq(&cgwb_lock);
+
+		wb_put(wb);
+	}
+
+	if (!list_empty(&processed))
+		list_splice_tail(&processed, &offline_cgwbs);
+
+	spin_unlock_irq(&cgwb_lock);
+}
+
 /**
  * wb_memcg_offline - kill all wb's associated with a memcg being offlined
  * @memcg: memcg being offlined
@@ -753,6 +809,8 @@ void wb_memcg_offline(struct mem_cgroup *memcg)
 		cgwb_kill(wb);
 	memcg_cgwb_list->next = NULL;	/* prevent new wb's */
 	spin_unlock_irq(&cgwb_lock);
+
+	queue_work(system_unbound_wq, &cleanup_offline_cgwbs_work);
 }
 
 /**
