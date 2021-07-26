52a10165db596bd58da4fae03b39342d79524241
Muchun Song <songmuchun@bytedance.com>
2021-07-26 17:59:00 +0800

    bytedance: x86: add segment fault tracepoint
    It will create a coredump file when the program encounter a segment
    fault, which is useful for debugging. Sometimes the coredump will
    not be created because of the filter, but TCE also want to know the
    call trace and send it to a platform for showing. It need to know
    all the user registers to dump the call trace (or parameters). This
    patch add a tracepoint to send all the user registers when a program
    crashes.
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Zhang Yuchen <zhangyuchen.lcr@bytedance.com>

--- arch/x86/include/asm/trace/exceptions.h
+++ arch/x86/include/asm/trace/exceptions.h
@@ -44,6 +44,76 @@ DEFINE_EVENT_FN(x86_exceptions, name,				\
 DEFINE_PAGE_FAULT_EVENT(page_fault_user);
 DEFINE_PAGE_FAULT_EVENT(page_fault_kernel);
 
+TRACE_EVENT(segfault,
+
+	TP_PROTO(unsigned long address, struct pt_regs *regs,
+		 unsigned long error_code, struct task_struct *task),
+
+	TP_ARGS(address, regs, error_code, task),
+
+	TP_STRUCT__entry(
+		__array(	char,	comm,	TASK_COMM_LEN	)
+		__field(	pid_t,		pid		)
+		__field(	unsigned long,	error_code	)
+		__field(	unsigned long,	address		)
+#ifndef __i386__
+		__field(	unsigned long,	r15		)
+		__field(	unsigned long,	r14		)
+		__field(	unsigned long,	r13		)
+		__field(	unsigned long,	r12		)
+		__field(	unsigned long,	r11		)
+		__field(	unsigned long,	r10		)
+		__field(	unsigned long,	r9		)
+		__field(	unsigned long,	r8		)
+#endif
+		__field(	unsigned long,	bp		)
+		__field(	unsigned long,	bx		)
+		__field(	unsigned long,	ax		)
+		__field(	unsigned long,	cx		)
+		__field(	unsigned long,	dx		)
+		__field(	unsigned long,	si		)
+		__field(	unsigned long,	di		)
+		__field(	unsigned long,	ip		)
+		__field(	unsigned long,	cs		)
+		__field(	unsigned long,	flags		)
+		__field(	unsigned long,	sp		)
+		__field(	unsigned long,	ss		)
+	),
+
+	TP_fast_assign(
+		memcpy(__entry->comm, task->comm, TASK_COMM_LEN);
+		__entry->pid		= task_pid_nr(task);
+		__entry->error_code	= error_code;
+		__entry->address	= address;
+#ifndef __i386__
+		__entry->r15		= regs->r15;
+		__entry->r14		= regs->r14;
+		__entry->r13		= regs->r13;
+		__entry->r12		= regs->r12;
+		__entry->r11		= regs->r11;
+		__entry->r10		= regs->r10;
+		__entry->r9		= regs->r9;
+		__entry->r8		= regs->r8;
+#endif
+		__entry->bp		= regs->bp;
+		__entry->bx		= regs->bx;
+		__entry->ax		= regs->ax;
+		__entry->cx		= regs->cx;
+		__entry->dx		= regs->dx;
+		__entry->si		= regs->si;
+		__entry->di		= regs->di;
+		__entry->ip		= regs->ip;
+		__entry->cs		= regs->cs;
+		__entry->flags		= regs->flags;
+		__entry->sp		= regs->sp;
+		__entry->ss		= regs->ss;
+	),
+
+	TP_printk("%s[%d]: segfault at %lx ip %px sp %px error %lx",
+		  __entry->comm, __entry->pid, __entry->address,
+		  (void *)__entry->ip, (void *)__entry->sp, __entry->error_code)
+);
+
 #undef TRACE_INCLUDE_PATH
 #undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_PATH .
