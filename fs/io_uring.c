840ff126d36e966876aa01c7a64b05223a15f61f
Chunxin Zang <zangchunxin@bytedance.com>
2021-07-30 15:34:42 +0800

    bytedance: io_uring: fix enable ring failed.
    If io_uring_setup with IORING_SETUP_R_DISABLED, we can't
    enable it by IORING_REGISTER_ENABLE_RINGS.
    
    Reference:
    commit: 0298ef969a110ca03654f0cea9b50e3f3b331acc
    io_uring: clean R_DISABLED startup mess
    
    Signed-off-by: Chunxin Zang <zangchunxin@bytedance.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -9990,6 +9990,7 @@ static int io_register_enable_rings(struct io_ring_ctx *ctx)
 	if (ctx->restrictions.registered)
 		ctx->restricted = 1;
 
+	ctx->flags &= ~IORING_SETUP_R_DISABLED;
 	io_sq_offload_start(ctx);
 	return 0;
 }
