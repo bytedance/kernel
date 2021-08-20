cc4429fd040f86cef7e0297e200a743ad0b2cfee
Josh Don <joshdon@google.com>
2021-08-19 18:04:02 -0700

    sched: reduce sched slice for SCHED_IDLE entities
    commit 51ce83ed523b00d58f2937ec014b12daaad55185 upstream.
    
    Use a small, non-scaled min granularity for SCHED_IDLE entities, when
    competing with normal entities. This reduces the latency of getting
    a normal entity back on cpu, at the expense of increased context
    switch frequency of SCHED_IDLE entities.
    
    The benefit of this change is to reduce the round-robin latency for
    normal entities when competing with a SCHED_IDLE entity.
    
    Example: on a machine with HZ=1000, spawned two threads, one of which is
    SCHED_IDLE, and affined to one cpu. Without this patch, the SCHED_IDLE
    thread runs for 4ms then waits for 1.4s. With this patch, it runs for
    1ms and waits 340ms (as it round-robins with the other thread).
    
    Signed-off-by: Josh Don <joshdon@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20210820010403.946838-4-joshdon@google.com
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -740,6 +740,7 @@ static void sched_debug_header(struct seq_file *m)
 	SEQ_printf(m, "  .%-40s: %Ld.%06ld\n", #x, SPLIT_NS(x))
 	PN(sysctl_sched_latency);
 	PN(sysctl_sched_min_granularity);
+	PN(sysctl_sched_idle_min_granularity);
 	PN(sysctl_sched_wakeup_granularity);
 	P(sysctl_sched_child_runs_first);
 	P(sysctl_sched_features);
