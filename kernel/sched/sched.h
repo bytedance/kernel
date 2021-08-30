9359e76a2fc6bcc77a83dcd87e4baa1a95ddbf9b
Huaixin Chang <changhuaixin@linux.alibaba.com>
2021-08-30 11:22:14 +0800

    sched/fair: Add cfs bandwidth burst statistics
    commit bcb1704a1ed2de580a46f28922e223a65f16e0f5 upstream.
    
    Two new statistics are introduced to show the internal of burst feature
    and explain why burst helps or not.
    
    nr_bursts:  number of periods bandwidth burst occurs
    burst_time: cumulative wall-time (in nanoseconds) that any cpus has
    	    used above quota in respective periods
    
    Co-developed-by: Shanpei Chen <shanpeic@linux.alibaba.com>
    Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>
    Co-developed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Signed-off-by: Huaixin Chang <changhuaixin@linux.alibaba.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://lore.kernel.org/r/20210830032215.16302-2-changhuaixin@linux.alibaba.com
    Signed-off-by: Hao Jia <jiahao.os@bytedance.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -368,6 +368,7 @@ struct cfs_bandwidth {
 	u64			quota;
 	u64			runtime;
 	u64			burst;
+	u64			runtime_snap;
 	s64			hierarchical_quota;
 
 	u8			idle;
@@ -380,7 +381,9 @@ struct cfs_bandwidth {
 	/* Statistics: */
 	int			nr_periods;
 	int			nr_throttled;
+	int			nr_burst;
 	u64			throttled_time;
+	u64			burst_time;
 #endif
 };
 
