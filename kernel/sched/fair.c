3de78105ff1e714d66c04b38f2c9181627f42b79
Vincent Guittot <vincent.guittot@linaro.org>
2021-09-28 12:35:44 +0200

    sched/fair: Removed useless update of p->recent_used_cpu
    commit a7ba894821b6ade7bb420455f87020b2838d6180 upstream.
    
    Since commit 89aafd67f28c ("sched/fair: Use prev instead of new target as recent_used_cpu"),
    p->recent_used_cpu is unconditionnaly set with prev.
    
    Fixes: 89aafd67f28c ("sched/fair: Use prev instead of new target as recent_used_cpu")
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Link: https://lkml.kernel.org/r/20210928103544.27489-1-vincent.guittot@linaro.org
    Signed-off-by: Hao Jia <jiahao.os@bytedance.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6136,11 +6136,6 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 	    cpus_share_cache(recent_used_cpu, target) &&
 	    (available_idle_cpu(recent_used_cpu) || sched_idle_cpu(recent_used_cpu)) &&
 	    cpumask_test_cpu(p->recent_used_cpu, p->cpus_ptr)) {
-		/*
-		 * Replace recent_used_cpu with prev as it is a potential
-		 * candidate for the next wake:
-		 */
-		p->recent_used_cpu = prev;
 		return recent_used_cpu;
 	}
 
