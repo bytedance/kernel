f4d08af33f238c7f0b7ffbf291414a02e2cfd3c7
Xie Yongji <xieyongji@bytedance.com>
2021-10-19 14:16:18 +0800

    bytedance: vduse: Support non-privileged user
    Enable non-privileged user to emulate vDPA device.
    
    Signed-off-by: Xie Yongji <xieyongji@bytedance.com>

--- drivers/vdpa/vdpa_user/vduse_dev.c
+++ drivers/vdpa/vdpa_user/vduse_dev.c
@@ -2003,6 +2003,9 @@ static const struct file_operations vduse_ctrl_fops = {
 
 static char *vduse_devnode(struct device *dev, umode_t *mode)
 {
+	if (mode)
+		*mode = 0666;
+
 	return kasprintf(GFP_KERNEL, "vduse/%s", dev_name(dev));
 }
 
