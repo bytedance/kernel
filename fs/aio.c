feefa6ab4131e93cccdc63d1bf481cb845aabce4
Jens Axboe <axboe@kernel.dk>
2021-10-21 09:22:35 -0600

    fs: get rid of the res2 iocb->ki_complete argument
    commit 6b19b766e8 upstream
    fix conflict.
    
    The second argument was only used by the USB gadget code, yet everyone
    pays the overhead of passing a zero to be passed into aio, where it
    ends up being part of the aio res2 value.
    
    Now that everybody is passing in zero, kill off the extra argument.
    
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- fs/aio.c
+++ fs/aio.c
@@ -1429,7 +1429,7 @@ static void aio_remove_iocb(struct aio_kiocb *iocb)
 	spin_unlock_irqrestore(&ctx->ctx_lock, flags);
 }
 
-static void aio_complete_rw(struct kiocb *kiocb, long res, long res2)
+static void aio_complete_rw(struct kiocb *kiocb, long res)
 {
 	struct aio_kiocb *iocb = container_of(kiocb, struct aio_kiocb, rw);
 
@@ -1449,7 +1449,7 @@ static void aio_complete_rw(struct kiocb *kiocb, long res, long res2)
 	}
 
 	iocb->ki_res.res = res;
-	iocb->ki_res.res2 = res2;
+	iocb->ki_res.res2 = 0;
 	iocb_put(iocb);
 }
 
@@ -1520,7 +1520,7 @@ static inline void aio_rw_done(struct kiocb *req, ssize_t ret)
 		ret = -EINTR;
 		fallthrough;
 	default:
-		req->ki_complete(req, ret, 0);
+		req->ki_complete(req, ret);
 	}
 }
 
