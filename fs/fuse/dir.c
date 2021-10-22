8447247db7c5c645843842af73d0d0ea9d2b8313
Miklos Szeredi <mszeredi@redhat.com>
2021-10-22 17:03:02 +0200

    fuse: don't increment nlink in link()
    commit 97f044f690bac2b094bfb7fb2d177ef946c85880 upstream.
    
    The fuse_iget() call in create_new_entry() already updated the inode with
    all the new attributes and incremented the attribute version.
    
    Incrementing the nlink will result in the wrong count.  This wasn't noticed
    because the attributes were invalidated right after this.
    
    Updating ctime is still needed for the writeback case when the ctime is not
    refreshed.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Xin Yin <yinxin.x@bytedance.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -929,25 +929,11 @@ static int fuse_link(struct dentry *entry, struct inode *newdir,
 	args.in_args[1].size = newent->d_name.len + 1;
 	args.in_args[1].value = newent->d_name.name;
 	err = create_new_entry(fm, &args, newdir, newent, inode->i_mode);
-	/* Contrary to "normal" filesystems it can happen that link
-	   makes two "logical" inodes point to the same "physical"
-	   inode.  We invalidate the attributes of the old one, so it
-	   will reflect changes in the backing inode (link count,
-	   etc.)
-	*/
-	if (!err) {
-		struct fuse_inode *fi = get_fuse_inode(inode);
-
-		spin_lock(&fi->lock);
-		fi->attr_version = atomic64_inc_return(&fm->fc->attr_version);
-		if (likely(inode->i_nlink < UINT_MAX))
-			inc_nlink(inode);
-		spin_unlock(&fi->lock);
-		fuse_invalidate_attr(inode);
+	if (!err)
 		fuse_update_ctime(inode);
-	} else if (err == -EINTR) {
+	else if (err == -EINTR)
 		fuse_invalidate_attr(inode);
-	}
+
 	return err;
 }
 
