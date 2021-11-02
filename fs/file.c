c52532c88d3bccbcad854726e07bf522387286c4
Gou Hao <gouhao@uniontech.com>
2021-11-02 10:46:48 +0800

    fs: remove fget_many and fput_many interface
    commit 81132a39c1 upstream
    
    These two interface were added in 091141a42 commit,
    but now there is no place to call them.
    
    The only user of fput/fget_many() was removed in commit
    62906e89e63b ("io_uring: remove file batch-get optimisation").
    
    A user of get_file_rcu_many() were removed in commit
    f073531070d2 ("init: add an init_dup helper").
    
    And replace atomic_long_sub/add to atomic_long_dec/inc
    can improve performance.
    
    Here are the test results of unixbench:
    
    Cmd: ./Run -c 64 context1
    
    Without patch:
    System Benchmarks Partial Index              BASELINE       RESULT    INDEX
    Pipe-based Context Switching                   4000.0    2798407.0   6996.0
                                                                       ========
    System Benchmarks Index Score (Partial Only)                         6996.0
    
    With patch:
    System Benchmarks Partial Index              BASELINE       RESULT    INDEX
    Pipe-based Context Switching                   4000.0    3486268.8   8715.7
                                                                       ========
    System Benchmarks Index Score (Partial Only)                         8715.7
    
    Signed-off-by: Gou Hao <gouhao@uniontech.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- fs/file.c
+++ fs/file.c
@@ -853,7 +853,7 @@ void do_close_on_exec(struct files_struct *files)
 }
 
 static inline struct file *__fget_files_rcu(struct files_struct *files,
-	unsigned int fd, fmode_t mask, unsigned int refs)
+	unsigned int fd, fmode_t mask)
 {
 	for (;;) {
 		struct file *file;
@@ -879,10 +879,9 @@ static inline struct file *__fget_files_rcu(struct files_struct *files,
 		 * Such a race can take two forms:
 		 *
 		 *  (a) the file ref already went down to zero,
-		 *      and get_file_rcu_many() fails. Just try
-		 *      again:
+		 *      and get_file_rcu() fails. Just try again:
 		 */
-		if (unlikely(!get_file_rcu_many(file, refs)))
+		if (unlikely(!get_file_rcu(file)))
 			continue;
 
 		/*
@@ -891,11 +890,11 @@ static inline struct file *__fget_files_rcu(struct files_struct *files,
 		 *       pointer having changed, because it always goes
 		 *       hand-in-hand with 'fdt'.
 		 *
-		 * If so, we need to put our refs and try again.
+		 * If so, we need to put our ref and try again.
 		 */
 		if (unlikely(rcu_dereference_raw(files->fdt) != fdt) ||
 		    unlikely(rcu_dereference_raw(*fdentry) != file)) {
-			fput_many(file, refs);
+			fput(file);
 			continue;
 		}
 
@@ -908,37 +907,31 @@ static inline struct file *__fget_files_rcu(struct files_struct *files,
 }
 
 static struct file *__fget_files(struct files_struct *files, unsigned int fd,
-				 fmode_t mask, unsigned int refs)
+				 fmode_t mask)
 {
 	struct file *file;
 
 	rcu_read_lock();
-	file = __fget_files_rcu(files, fd, mask, refs);
+	file = __fget_files_rcu(files, fd, mask);
 	rcu_read_unlock();
 
 	return file;
 }
 
-static inline struct file *__fget(unsigned int fd, fmode_t mask,
-				  unsigned int refs)
+static inline struct file *__fget(unsigned int fd, fmode_t mask)
 {
-	return __fget_files(current->files, fd, mask, refs);
-}
-
-struct file *fget_many(unsigned int fd, unsigned int refs)
-{
-	return __fget(fd, FMODE_PATH, refs);
+	return __fget_files(current->files, fd, mask);
 }
 
 struct file *fget(unsigned int fd)
 {
-	return __fget(fd, FMODE_PATH, 1);
+	return __fget(fd, FMODE_PATH);
 }
 EXPORT_SYMBOL(fget);
 
 struct file *fget_raw(unsigned int fd)
 {
-	return __fget(fd, 0, 1);
+	return __fget(fd, 0);
 }
 EXPORT_SYMBOL(fget_raw);
 
@@ -948,7 +941,7 @@ struct file *fget_task(struct task_struct *task, unsigned int fd)
 
 	task_lock(task);
 	if (task->files)
-		file = __fget_files(task->files, fd, 0, 1);
+		file = __fget_files(task->files, fd, 0);
 	task_unlock(task);
 
 	return file;
@@ -1026,7 +1019,7 @@ static unsigned long __fget_light(unsigned int fd, fmode_t mask)
 			return 0;
 		return (unsigned long)file;
 	} else {
-		file = __fget(fd, mask, 1);
+		file = __fget(fd, mask);
 		if (!file)
 			return 0;
 		return FDPUT_FPUT | (unsigned long)file;
