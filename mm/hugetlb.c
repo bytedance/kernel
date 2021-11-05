e9f2e905b66a30080cb13d8f37f1ceb85c4f8e6a
Mike Kravetz <mike.kravetz@oracle.com>
2021-11-05 13:42:04 -0700

    hugetlb: remove unnecessary set_page_count in prep_compound_gigantic_page
    commit 2c0078a7d820e55ce6a176721a94f3c585833436 upstream.
    
    In commit 7118fc2906e2 ("hugetlb: address ref count racing in
    prep_compound_gigantic_page"), page_ref_freeze is used to atomically
    zero the ref count of tail pages iff they are 1.  The unconditional call
    to set_page_count(0) was left in the code.  This call is after
    page_ref_freeze so it is really a noop.
    
    Remove redundant and unnecessary set_page_count call.
    
    Link: https://lkml.kernel.org/r/20211026220635.35187-1-mike.kravetz@oracle.com
    Fixes: 7118fc2906e29 ("hugetlb: address ref count racing in prep_compound_gigantic_page")
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Suggested-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1714,7 +1714,6 @@ static bool prep_compound_gigantic_page(struct page *page, unsigned int order)
 			pr_warn("HugeTLB page can not be used due to unexpected inflated ref count\n");
 			goto out_error;
 		}
-		set_page_count(p, 0);
 		set_compound_head(p, page);
 	}
 	atomic_set(compound_mapcount_ptr(page), -1);
