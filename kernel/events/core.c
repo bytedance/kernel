e2a4f29e0c98a25bd62fc313ca3d5642f2b80cbc
Sean Christopherson <seanjc@google.com>
2021-11-11 02:07:25 +0000

    perf: Stop pretending that perf can handle multiple guest callbacks
    commit 2934e3d09350c1a7ca2433fbeabfcd831e48a575 upstream.
    
    Drop the 'int' return value from the perf (un)register callbacks helpers
    and stop pretending perf can support multiple callbacks.  The 'int'
    returns are not future proofing anything as none of the callers take
    action on an error.  It's also not obvious that there will ever be
    co-tenant hypervisors, and if there are, that allowing multiple callbacks
    to be registered is desirable or even correct.
    
    Opportunistically rename callbacks=>cbs in the affected declarations to
    match their definitions.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://lore.kernel.org/r/20211111020738.2512932-5-seanjc@google.com
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>
    Signed-off-by: Liangyan <liangyan.peng@bytedance.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -6637,31 +6637,24 @@ static void perf_pending_task(struct callback_head *head)
 	put_event(event);
 }
 
-/*              
- * We assume there is only KVM supporting the callbacks.
- * Later on, we might change it to a list if there is
- * another virtualization implementation supporting the callbacks.
- */
 struct perf_guest_info_callbacks __rcu *perf_guest_cbs;
 
-int perf_register_guest_info_callbacks(struct perf_guest_info_callbacks *cbs)
+void perf_register_guest_info_callbacks(struct perf_guest_info_callbacks *cbs)
 {
 	if (WARN_ON_ONCE(rcu_access_pointer(perf_guest_cbs)))
-		return -EBUSY;
+		return;
 
 	rcu_assign_pointer(perf_guest_cbs, cbs);
-	return 0;
 }
 EXPORT_SYMBOL_GPL(perf_register_guest_info_callbacks);
 
-int perf_unregister_guest_info_callbacks(struct perf_guest_info_callbacks *cbs)
+void perf_unregister_guest_info_callbacks(struct perf_guest_info_callbacks *cbs)
 {
 	if (WARN_ON_ONCE(rcu_access_pointer(perf_guest_cbs) != cbs))
-		return -EINVAL;
+		return;
 
 	rcu_assign_pointer(perf_guest_cbs, NULL);
 	synchronize_rcu();
-	return 0;
 }
 EXPORT_SYMBOL_GPL(perf_unregister_guest_info_callbacks);
 
