730b33cfa5d8ebbcf7c7b0591465c555785a2e64
Jiri Olsa <jolsa@redhat.com>
2021-12-08 20:32:44 +0100

    bpf: Add get_func_[arg|ret|arg_cnt] helpers
    commit f92c1e183604c20ce00eb889315fdaa8f2d9e509 upstream.
    
    Adding following helpers for tracing programs:
    
    Get n-th argument of the traced function:
      long bpf_get_func_arg(void *ctx, u32 n, u64 *value)
    
    Get return value of the traced function:
      long bpf_get_func_ret(void *ctx, u64 *value)
    
    Get arguments count of the traced function:
      long bpf_get_func_arg_cnt(void *ctx)
    
    The trampoline now stores number of arguments on ctx-8
    address, so it's easy to verify argument index and find
    return value argument's position.
    
    Moving function ip address on the trampoline stack behind
    the number of functions arguments, so it's now stored on
    ctx-16 address if it's needed.
    
    All helpers above are inlined by verifier.
    
    Also bit unrelated small change - using newly added function
    bpf_prog_has_trampoline in check_get_func_ip.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20211208193245.172141-5-jolsa@kernel.org
    Signed-off-by: Feng Zhou <zhoufeng.zf@bytedance.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -846,6 +846,7 @@ void bpf_ksym_add(struct bpf_ksym *ksym);
 void bpf_ksym_del(struct bpf_ksym *ksym);
 int bpf_jit_charge_modmem(u32 pages);
 void bpf_jit_uncharge_modmem(u32 pages);
+bool bpf_prog_has_trampoline(const struct bpf_prog *prog);
 #else
 static inline int bpf_trampoline_link_prog(struct bpf_tramp_link *link,
 					   struct bpf_trampoline *tr)
@@ -871,6 +872,10 @@ static inline void bpf_dispatcher_change_prog(struct bpf_dispatcher *d,
 					      struct bpf_prog *from,
 					      struct bpf_prog *to) {}
 static inline bool is_bpf_image_address(unsigned long address)
+{
+	return false;
+}
+static inline bool bpf_prog_has_trampoline(const struct bpf_prog *prog)
 {
 	return false;
 }
