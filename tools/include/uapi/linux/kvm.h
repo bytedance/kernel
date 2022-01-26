641fd299304dd0d09f819cec6c6bea61a3d15191
Paolo Bonzini <pbonzini@redhat.com>
2022-01-26 07:51:00 -0500

    selftests: kvm: check dynamic bits against KVM_X86_XCOMP_GUEST_SUPP
    commit b19c99b9f4486f23e3b7248dd4ce3d83e19b9032 upstream.
    
    Provide coverage for the new API.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Jiawei Gu <gujiawei.will@bytedance.com>

--- tools/include/uapi/linux/kvm.h
+++ tools/include/uapi/linux/kvm.h
@@ -1114,6 +1114,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_ARM_MTE 205
 #define KVM_CAP_VM_GPA_BITS 207
 #define KVM_CAP_XSAVE2 208
+#define KVM_CAP_SYS_ATTRIBUTES 209
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
