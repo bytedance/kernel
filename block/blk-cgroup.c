d4a1233e574884e2b2c49e203e33f37312bbf670
Chengming Zhou <zhouchengming@bytedance.com>
2022-02-13 16:59:02 +0800

    blk-cgroup: set blkg iostat after percpu stat aggregation
    commit f122d103b564e5fb7c82de902c6f8f6cbdf50ec9 upstream.
    
    Don't need to do blkg_iostat_set for top blkg iostat on each CPU,
    so move it after percpu stat aggregation.
    
    Fixes: ef45fe470e1e ("blk-cgroup: show global disk stats in root cgroup io.stat")
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://lore.kernel.org/r/20220213085902.88884-1-zhouchengming@bytedance.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -850,11 +850,11 @@ static void blkcg_fill_root_iostats(void)
 		struct blkcg_gq *blkg = blk_queue_root_blkg(disk->queue);
 		struct blkg_iostat tmp;
 		int cpu;
+		unsigned long flags;
 
 		memset(&tmp, 0, sizeof(tmp));
 		for_each_possible_cpu(cpu) {
 			struct disk_stats *cpu_dkstats;
-			unsigned long flags;
 
 			cpu_dkstats = per_cpu_ptr(part->dkstats, cpu);
 			tmp.ios[BLKG_IOSTAT_READ] +=
@@ -870,11 +870,11 @@ static void blkcg_fill_root_iostats(void)
 				cpu_dkstats->sectors[STAT_WRITE] << 9;
 			tmp.bytes[BLKG_IOSTAT_DISCARD] +=
 				cpu_dkstats->sectors[STAT_DISCARD] << 9;
-
-			flags = u64_stats_update_begin_irqsave(&blkg->iostat.sync);
-			blkg_iostat_set(&blkg->iostat.cur, &tmp);
-			u64_stats_update_end_irqrestore(&blkg->iostat.sync, flags);
 		}
+
+		flags = u64_stats_update_begin_irqsave(&blkg->iostat.sync);
+		blkg_iostat_set(&blkg->iostat.cur, &tmp);
+		u64_stats_update_end_irqrestore(&blkg->iostat.sync, flags);
 	}
 }
 
