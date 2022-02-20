2d6b2101c9c0ffb306af4bf4b6055ff1812cfdc3
Muchun Song <songmuchun@bytedance.com>
2022-02-20 20:05:33 +0800

    bytedance: checkpatch: Ignore checking format of upstream patterns
    There are a lot of patches which need to be backported to our own branch,
    some patches with a string like "[ Upstream commit $upstream_id ]" or
    "commit $upstream_id upstream" to the front of the commit log.  But
    checkpatch always complains about this and our (ByteDance) CI/CD always
    runs checkpatch first.  Obviously, the test always fails, so ignore the
    check of the format of "[ Upstream commit $id ]" and "commit $upstream_id
    upstream".
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -3185,6 +3185,8 @@ sub process {
 		    $in_commit_log && !$commit_log_possible_stack_dump &&
 		    $line !~ /^\s*(?:Link|Patchwork|http|https|BugLink|base-commit):/i &&
 		    $line !~ /^This reverts commit [0-9a-f]{7,40}/ &&
+		    $line !~ /^commit [0-9a-f]{7,40} upstream/ &&
+		    $line !~ /^[ Upstream commit [0-9a-f]{7,40}/ &&
 		    (($line =~ /\bcommit\s+[0-9a-f]{5,}\b/i ||
 		      ($line =~ /\bcommit\s*$/i && defined($rawlines[$linenr]) && $rawlines[$linenr] =~ /^\s*[0-9a-f]{5,}\b/i)) ||
 		     ($line =~ /(?:\s|^)[0-9a-f]{12,40}(?:[\s"'\(\[]|$)/i &&
