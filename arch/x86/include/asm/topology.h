c679b449116dbe38cd4a693792b80d2562a06c4f
Ionela Voinescu <ionela.voinescu@arm.com>
2022-03-10 14:54:49 +0000

    x86, ACPI: rename init_freq_invariance_cppc() to arch_init_invariance_cppc()
    commit 1132e6de11cfc334b44f609792664f1bc3055c52 upstream.
    
    init_freq_invariance_cppc() was called in acpi_cppc_processor_probe(),
    after CPU performance information and controls were populated from the
    per-cpu _CPC objects.
    
    But these _CPC objects provide information that helps with both CPU
    (u-arch) and frequency invariance. Therefore, change the function name
    to a more generic one, while adding the arch_ prefix, as this function
    is expected to be defined differently by different architectures.
    
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Liangyan <liangyan.peng@bytedance.com>

--- arch/x86/include/asm/topology.h
+++ arch/x86/include/asm/topology.h
@@ -225,7 +225,7 @@ static inline void ap_init_freq_invariance(void) { }
 
 #ifdef CONFIG_ACPI_CPPC_LIB
 void init_freq_invariance_cppc(void);
-#define init_freq_invariance_cppc init_freq_invariance_cppc
+#define arch_init_invariance_cppc init_freq_invariance_cppc
 
 bool amd_set_max_freq_ratio(u64 *ratio);
 #else
