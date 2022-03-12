dc2bc4147fac28c82faacc822c593e57c2b80bf5
Isaku Yamahata <isaku.yamahata@intel.com>
2022-03-12 11:19:45 -0800

    x86/virt/tdx: Export information about the TDX module via sysfs
    TDX requires a firmware, TDX module, to load and initialized.  The TDX
    module comes with its attributes, vendor id, build date, build number,
    minor version, major version, etc.  Export those information via sysfs for
    administrator or VM management software like libvirt.
    
    [backport]
    export sysinfo.module_version as the tdsysinfo;
    
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -2073,6 +2073,37 @@ static const struct attribute_group tdx_metadata_attr_group = {
 	.bin_attrs = tdx_metadata_attrs,
 };
 
+#define TDX_MODULE_ATTR_SHOW(_name, fmt)				\
+static ssize_t tdx_module_ ## _name ## _show(				\
+	struct kobject *kobj, struct kobj_attribute *attr, char *buf)	\
+{									\
+	return sprintf(buf, fmt, sysinfo.module_version._name);			\
+}									\
+static struct kobj_attribute tdx_module_##_name = {			\
+	.attr = { .name = __stringify(_name), .mode = 0444 },		\
+	.show = tdx_module_ ## _name ## _show,				\
+}
+
+TDX_MODULE_ATTR_SHOW(internal, "0x%08x");
+TDX_MODULE_ATTR_SHOW(build_date, "%d");
+TDX_MODULE_ATTR_SHOW(build_num, "0x%08x");
+TDX_MODULE_ATTR_SHOW(minor, "0x%08x");
+TDX_MODULE_ATTR_SHOW(major, "0x%08x");
+
+static struct attribute *tdx_module_attrs[] = {
+	&tdx_module_internal.attr,
+	&tdx_module_build_date.attr,
+	&tdx_module_build_num.attr,
+	&tdx_module_minor.attr,
+	&tdx_module_major.attr,
+	NULL,
+};
+
+static const struct attribute_group tdx_module_attr_group = {
+	.attrs = tdx_module_attrs,
+};
+
+
 int tdx_sysfs_init(void)
 {
 	int ret;
@@ -2108,6 +2139,12 @@ int tdx_sysfs_init(void)
 	if (!tdx_metadata_kobj) {
 		pr_err("Sysfs exporting tdx global metadata failed %d\n", ret);
 		return -EINVAL;
+	}
+
+	ret = sysfs_create_group(tdx_module_kobj, &tdx_module_attr_group);
+	if (ret) {
+		pr_err("Sysfs exporting tdx module attributes failed %d\n", ret);
+		return ret;
 	}
 
 	tdx_metadata_cpuid_leaves.size = tdx_info->num_cpuid_config *
