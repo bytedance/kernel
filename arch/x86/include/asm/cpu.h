bff81743737c275154506c3f512ca29c0aa35ae1
Jithu Joseph <jithu.joseph@intel.com>
2022-05-06 15:53:59 -0700

    x86/microcode/intel: Expose collect_cpu_info_early() for IFS
    commit d3287fb0d3c8afdfd4870a6cd4a852abc9008b3b upstream.
    
    IFS is a CPU feature that allows a binary blob, similar to microcode,
    to be loaded and consumed to perform low level validation of CPU
    circuitry. In fact, it carries the same Processor Signature
    (family/model/stepping) details that are contained in Intel microcode
    blobs.
    
    In support of an IFS driver to trigger loading, validation, and running
    of these tests blobs, make the functionality of cpu_signatures_match()
    and collect_cpu_info_early() available outside of the microcode driver.
    
    Add an "intel_" prefix and drop the "_early" suffix from
    collect_cpu_info_early() and EXPORT_SYMBOL_GPL() it. Add
    declaration to x86 <asm/cpu.h>
    
    Make cpu_signatures_match() an inline function in x86 <asm/cpu.h>,
    and also give it an "intel_" prefix.
    
    No functional change intended.
    
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Co-developed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20220506225410.1652287-2-tony.luck@intel.com
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Jiawei Gu <gujiawei.will@bytedance.com>

--- arch/x86/include/asm/cpu.h
+++ arch/x86/include/asm/cpu.h
@@ -72,4 +72,23 @@ void init_ia32_feat_ctl(struct cpuinfo_x86 *c);
 #else
 static inline void init_ia32_feat_ctl(struct cpuinfo_x86 *c) {}
 #endif
+
+struct ucode_cpu_info;
+
+int intel_cpu_collect_info(struct ucode_cpu_info *uci);
+
+static inline bool intel_cpu_signatures_match(unsigned int s1, unsigned int p1,
+					      unsigned int s2, unsigned int p2)
+{
+	if (s1 != s2)
+		return false;
+
+	/* Processor flags are either both 0 ... */
+	if (!p1 && !p2)
+		return true;
+
+	/* ... or they intersect. */
+	return p1 & p2;
+}
+
 #endif /* _ASM_X86_CPU_H */
