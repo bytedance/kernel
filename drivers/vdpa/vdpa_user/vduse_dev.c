a4d33a6fdc9d1388723c2cb530c554fe597805bc
Xie Yongji <xieyongji@bytedance.com>
2022-05-27 13:29:10 +0800

    bytedance: vduse: Add irq_use_wq sysfs interface
    Add irq_use_wq sysfs interface to control whether
    use workqueue to inject irq or not.
    
    Signed-off-by: Xie Yongji <xieyongji@bytedance.com>

--- drivers/vdpa/vdpa_user/vduse_dev.c
+++ drivers/vdpa/vdpa_user/vduse_dev.c
@@ -52,6 +52,7 @@ struct vduse_virtqueue {
 	struct work_struct inject;
 	struct kobject kobj;
 	int irq_affinity;
+	bool irq_use_wq;
 	u16 avail_index;
 	u32 num;
 	u64 desc_addr;
@@ -1124,8 +1125,7 @@ static long vduse_dev_ioctl(struct file *file, unsigned int cmd,
 		vq = dev->vqs[vq_index];
 		ret = 0;
 
-		/* virtio-fs driver already uses workqueue in irq handler */
-		if (dev->device_id == VIRTIO_ID_FS) {
+		if (!vq->irq_use_wq) {
 			spin_lock_irq(&vq->irq_lock);
 			if (vq->ready && vq->cb.callback)
 				vq->cb.callback(vq->cb.private);
@@ -1256,6 +1256,26 @@ static const struct file_operations vduse_dev_fops = {
 	.mmap		= vduse_dev_mmap,
 };
 
+static ssize_t irq_use_wq_show(struct vduse_virtqueue *vq, char *buf)
+{
+	return sprintf(buf, "%d\n", vq->irq_use_wq);
+}
+
+static ssize_t irq_use_wq_store(struct vduse_virtqueue *vq,
+				const char *buf, size_t count)
+{
+	bool enabled;
+	int ret;
+
+	ret = kstrtobool(buf, &enabled);
+	if (ret)
+		return ret;
+
+	vq->irq_use_wq = enabled;
+
+	return count;
+}
+
 static ssize_t irq_affinity_show(struct vduse_virtqueue *vq, char *buf)
 {
 	return sprintf(buf, "%d\n", vq->irq_affinity);
@@ -1313,10 +1333,13 @@ static struct vq_sysfs_entry irq_inject_attr = __ATTR_WO(irq_inject);
 
 static struct vq_sysfs_entry kick_attr = __ATTR_WO(kick);
 
+static struct vq_sysfs_entry irq_use_wq_attr = __ATTR_RW(irq_use_wq);
+
 static struct attribute *vq_attrs[] = {
 	&irq_affinity_attr.attr,
 	&irq_inject_attr.attr,
 	&kick_attr.attr,
+	&irq_use_wq_attr.attr,
 	NULL,
 };
 
@@ -1407,6 +1430,10 @@ static int vduse_dev_init_vqs(struct vduse_dev *dev, u32 vq_align,
 		dev->vqs[i]->index = i;
 		dev->vqs[i]->dev = dev;
 		dev->vqs[i]->irq_affinity = -1;
+		/* virtio-fs driver already uses workqueue in irq handler */
+		dev->vqs[i]->irq_use_wq = (dev->device_id == VIRTIO_ID_FS) ?
+					  false : true;
+
 		vringh_set_iotlb(&dev->vqs[i]->vring, dev->domain->iotlb,
 				 &dev->domain->iotlb_lock);
 		vringh_kiov_init(&dev->vqs[i]->out_iov, NULL, 0);
