f29c0b3cd4915c63c8a0009fe49cd529f6a18e40
Ravi Bangoria <ravi.bangoria@amd.com>
2022-06-04 10:15:12 +0530

    perf record ibs: Warn about sampling period skew
    commit 9ab95b0b15a092abb7c3309f3580f572a041f9ab  upstream.
    
    Samples without an L3 miss are discarded and counter is reset with
    random value (between 1-15 for fetch PMU and 1-127 for op PMU) when IBS
    L3 miss filtering is enabled. This causes a sampling period skew but
    there is no way to reconstruct aggregated sampling period. So print a
    warning at perf record if user sets l3missonly=1.
    
    [Backport Changes]
    Adding additional header files util/env.h, util/pmu.h, linux/string.h
    to get the definition of struct perf_env and function prototypes
    of perf_env__cpuid(), perf_pmu__find(), strstarts().
    
    Ex:
    
      # perf record -c 10000 -C 0 -e ibs_op/l3missonly=1/
      WARNING: Hw internally resets sampling period when L3 Miss Filtering is enabled
      and tagged operation does not cause L3 Miss. This causes sampling period skew.
    
    Signed-off-by: Ravi Bangoria <ravi.bangoria@amd.com>
    Acked-by: Ian Rogers <irogers@google.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Ananth Narayan <ananth.narayan@amd.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: James Clark <james.clark@arm.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Kim Phillips <kim.phillips@amd.com>
    Cc: Leo Yan <leo.yan@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Robert Richter <rrichter@amd.com>
    Cc: Sandipan Das <sandipan.das@amd.com>
    Cc: Santosh Shukla <santosh.shukla@amd.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: like.xu.linux@gmail.com
    Cc: x86@kernel.org
    Link: http://lore.kernel.org/lkml/20220604044519.594-2-ravi.bangoria@amd.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>
    Signed-off-by: Liangyan <liangyan.peng@bytedance.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -1047,6 +1047,11 @@ void __weak arch_evsel__set_sample_weight(struct evsel *evsel)
 	evsel__set_sample_bit(evsel, WEIGHT);
 }
 
+void __weak arch__post_evsel_config(struct evsel *evsel __maybe_unused,
+				    struct perf_event_attr *attr __maybe_unused)
+{
+}
+
 static void evsel__set_default_freq_period(struct record_opts *opts,
 					   struct perf_event_attr *attr)
 {
@@ -1322,6 +1327,8 @@ void evsel__config(struct evsel *evsel, struct record_opts *opts,
 	 */
 	if (evsel__is_dummy_event(evsel))
 		evsel__reset_sample_bit(evsel, BRANCH_STACK);
+
+	arch__post_evsel_config(evsel, attr);
 }
 
 int evsel__set_filter(struct evsel *evsel, const char *filter)
