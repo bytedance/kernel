4eebcf98edb2bca94ca0a5057ef6a7d8183d7904
Dietmar Eggemann <dietmar.eggemann@arm.com>
2022-06-23 11:11:02 +0200

    sched/fair: Rename select_idle_mask to select_rq_mask
    commit ec4fc801a02d96180c597238fe87141471b70971 upstream.
    
    On 21/06/2022 11:04, Vincent Donnefort wrote:
    > From: Dietmar Eggemann <dietmar.eggemann@arm.com>
    
    https://lkml.kernel.org/r/202206221253.ZVyGQvPX-lkp@intel.com discovered
    that this patch doesn't build anymore (on tip sched/core or linux-next)
    because of commit f5b2eeb499910 ("sched/fair: Consider CPU affinity when
    allowing NUMA imbalance in find_idlest_group()").
    
    New version of [PATCH v11 4/7] sched/fair: Rename select_idle_mask to
    select_rq_mask below.
    
    -- >8 --
    
    Decouple the name of the per-cpu cpumask select_idle_mask from its usage
    in select_idle_[cpu/capacity]() of the CFS run-queue selection
    (select_task_rq_fair()).
    
    This is to support the reuse of this cpumask in the Energy Aware
    Scheduling (EAS) path (find_energy_efficient_cpu()) of the CFS run-queue
    selection.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Tested-by: Lukasz Luba <lukasz.luba@arm.com>
    Link: https://lkml.kernel.org/r/250691c7-0e2b-05ab-bedf-b245c11d9400@arm.com
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5995,7 +5995,7 @@ dequeue_throttle:
 
 /* Working cpumask for: load_balance, load_balance_newidle. */
 DEFINE_PER_CPU(cpumask_var_t, load_balance_mask);
-DEFINE_PER_CPU(cpumask_var_t, select_idle_mask);
+DEFINE_PER_CPU(cpumask_var_t, select_rq_mask);
 
 #ifdef CONFIG_NO_HZ_COMMON
 
@@ -6486,7 +6486,7 @@ static inline int select_idle_smt(struct task_struct *p, struct sched_domain *sd
  */
 static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, bool has_idle_core, int target)
 {
-	struct cpumask *cpus = this_cpu_cpumask_var_ptr(select_idle_mask);
+	struct cpumask *cpus = this_cpu_cpumask_var_ptr(select_rq_mask);
 	int i, cpu, idle_cpu = -1, nr = INT_MAX;
 	struct sched_domain_shared *sd_share;
 	struct rq *this_rq = this_rq();
@@ -6608,7 +6608,7 @@ select_idle_capacity(struct task_struct *p, struct sched_domain *sd, int target)
 	int cpu, best_cpu = -1;
 	struct cpumask *cpus;
 
-	cpus = this_cpu_cpumask_var_ptr(select_idle_mask);
+	cpus = this_cpu_cpumask_var_ptr(select_rq_mask);
 	cpumask_and(cpus, sched_domain_span(sd), p->cpus_ptr);
 
 	task_util = task_util_est(p);
@@ -6665,7 +6665,7 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 	}
 
 	/*
-	 * per-cpu select_idle_mask usage
+	 * per-cpu select_rq_mask usage
 	 */
 	lockdep_assert_irqs_disabled();
 
