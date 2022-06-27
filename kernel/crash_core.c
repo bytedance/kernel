f0debfa2a4c42641bc55f59b4636e50a88d864ad
Tao Liu <ltao@redhat.com>
2022-06-27 15:44:41 +0800

    kdump: round up the total memory size to 128M for crashkernel reservation
    commit 46d36b1be18b745fc9f6be2087633ba2f9895ffe upstream.
    
    The total memory size we get in kernel is usually slightly less than the
    actual memory size because BIOS/firmware will reserve some memory region.
    So it won't export all memory as usable.
    
    E.g, on my x86_64 kvm guest with 1G memory, the total_mem value shows:
    UEFI boot with ovmf: 0x3faef000 Legacy boot kvm guest: 0x3ff7ec00
    
    When specifying crashkernel=1G-2G:128M, if we have a 1G memory machine, we
    get total size 1023M from firmware.  Then it will not fall into 1G-2G,
    thus no memory reserved.  User will never know this, it is hard to let
    user know the exact total value in kernel.
    
    One way is to use dmi/smbios to get physical memory size, but it's not
    reliable as well.  According to Prarit hardware vendors sometimes screw
    this up.  Thus round up total size to 128M to work around this problem.
    
    This patch is a resend of [1] and rebased onto v5.19-rc2, and the
    original credit goes to Dave Young.
    
    [1]: http://lists.infradead.org/pipermail/kexec/2018-April/020568.html
    
    Link: https://lkml.kernel.org/r/20220627074440.187222-1-ltao@redhat.com
    Signed-off-by: Tao Liu <ltao@redhat.com>
    Acked-by: Baoquan He <bhe@redhat.com>
    Cc: Dave Young <dyoung@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Rui Qi <qirui.001@bytedance.com>

--- kernel/crash_core.c
+++ kernel/crash_core.c
@@ -9,6 +9,7 @@
 #include <linux/init.h>
 #include <linux/utsname.h>
 #include <linux/vmalloc.h>
+#include <linux/sizes.h>
 
 #include <asm/page.h>
 #include <asm/sections.h>
@@ -43,6 +44,15 @@ static int __init parse_crashkernel_mem(char *cmdline,
 					unsigned long long *crash_base)
 {
 	char *cur = cmdline, *tmp;
+	unsigned long long total_mem = system_ram;
+
+	/*
+	 * Firmware sometimes reserves some memory regions for its own use,
+	 * so the system memory size is less than the actual physical memory
+	 * size. Work around this by rounding up the total size to 128M,
+	 * which is enough for most test cases.
+	 */
+	total_mem = roundup(total_mem, SZ_128M);
 
 	/* for each entry of the comma-separated list */
 	do {
@@ -87,13 +97,13 @@ static int __init parse_crashkernel_mem(char *cmdline,
 			return -EINVAL;
 		}
 		cur = tmp;
-		if (size >= system_ram) {
+		if (size >= total_mem) {
 			pr_warn("crashkernel: invalid size\n");
 			return -EINVAL;
 		}
 
 		/* match ? */
-		if (system_ram >= start && system_ram < end) {
+		if (total_mem >= start && total_mem < end) {
 			*crash_size = size;
 			break;
 		}
