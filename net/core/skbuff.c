9d4cd40689c7e4a327cba8d1ecf80def72be0613
David Ahern <dsahern@kernel.org>
2022-07-12 21:52:30 +0100

    net: Allow custom iter handler in msghdr
    commit ebe73a284f upstream
    fix conflict.
    
    Add support for custom iov_iter handling to msghdr. The idea is that
    in-kernel subsystems want control over how an SG is split.
    
    Signed-off-by: David Ahern <dsahern@kernel.org>
    [pavel: move callback into msghdr]
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -1353,7 +1353,7 @@ EXPORT_SYMBOL_GPL(msg_zerocopy_put_abort);
 
 int skb_zerocopy_iter_dgram(struct sk_buff *skb, struct msghdr *msg, int len)
 {
-	return __zerocopy_sg_from_iter(skb->sk, skb, &msg->msg_iter, len);
+	return __zerocopy_sg_from_iter(msg, skb->sk, skb, &msg->msg_iter, len);
 }
 EXPORT_SYMBOL_GPL(skb_zerocopy_iter_dgram);
 
@@ -1371,7 +1371,7 @@ int skb_devmem_iter_stream(struct sock *sk, struct sk_buff *skb,
 	if (orig_uarg && uarg != orig_uarg)
 		return -EEXIST;
 
-	err = __zerocopy_sg_from_iter(sk, skb, iov_iter, len);
+	err = __zerocopy_sg_from_iter(NULL, sk, skb, iov_iter, len);
 	if (err == -EFAULT || (err == -EMSGSIZE && skb->len == orig_len)) {
 		struct sock *save_sk = skb->sk;
 
@@ -1402,7 +1402,7 @@ int skb_zerocopy_iter_stream(struct sock *sk, struct sk_buff *skb,
 	if (orig_uarg && uarg != orig_uarg)
 		return -EEXIST;
 
-	err = __zerocopy_sg_from_iter(sk, skb, &msg->msg_iter, len);
+	err = __zerocopy_sg_from_iter(msg, sk, skb, &msg->msg_iter, len);
 	if (err == -EFAULT || (err == -EMSGSIZE && skb->len == orig_len)) {
 		struct sock *save_sk = skb->sk;
 
