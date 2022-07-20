b9a7a245dbbfeb8eadddb7b9b0434678a6de78c9
Kuniyuki Iwashima <kuniyu@amazon.com>
2022-07-20 09:50:22 -0700

    tcp: Fix a data-race around sysctl_tcp_min_tso_segs.
    [ Upstream commit e0bb4ab9dfddd872622239f49fb2bd403b70853b ]
    
    While reading sysctl_tcp_min_tso_segs, it can be changed concurrently.
    Thus, we need to add READ_ONCE() to its reader.
    
    Fixes: 95bd09eb2750 ("tcp: TSO packets automatic sizing")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2092,7 +2092,7 @@ u32 tcp_tso_segs(struct sock *sk, unsigned int mss_now)
 	tso_segs = ca_ops->tso_segs ?
 			ca_ops->tso_segs(sk, mss_now) :
 			tcp_tso_autosize(sk, mss_now,
-					 sock_net(sk)->ipv4.sysctl_tcp_min_tso_segs);
+					 READ_ONCE(sock_net(sk)->ipv4.sysctl_tcp_min_tso_segs));
 	return min_t(u32, tso_segs, sk->sk_gso_max_segs);
 }
 EXPORT_SYMBOL(tcp_tso_segs);
