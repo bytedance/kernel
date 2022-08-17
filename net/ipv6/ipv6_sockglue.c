5150e5d05ca4c4537f96ae37fae28536f7b15337
Martin KaFai Lau <kafai@fb.com>
2022-08-16 23:17:44 -0700

    bpf: net: Change do_ipv6_setsockopt() to use the sockopt's lock_sock() and capable()
    commit 40cd308ea57cf68ad67f912b98fca570d107cca0 upstream.
    
    Similar to the earlier patch that avoids sk_setsockopt() from
    taking sk lock and doing capable test when called by bpf.  This patch
    changes do_ipv6_setsockopt() to use the sockopt_{lock,release}_sock()
    and sockopt_[ns_]capable().
    
    Reviewed-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/r/20220817061744.4176893-1-kafai@fb.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Feng Zhou <zhoufeng.zf@bytedance.com>

--- net/ipv6/ipv6_sockglue.c
+++ net/ipv6/ipv6_sockglue.c
@@ -325,7 +325,7 @@ static int ipv6_set_opt_hdr(struct sock *sk, int optname, sockptr_t optval,
 	int err;
 
 	/* hop-by-hop / destination options are privileged option */
-	if (optname != IPV6_RTHDR && !ns_capable(net->user_ns, CAP_NET_RAW))
+	if (optname != IPV6_RTHDR && !sockopt_ns_capable(net->user_ns, CAP_NET_RAW))
 		return -EPERM;
 
 	/* remove any sticky options header with a zero option
@@ -415,7 +415,7 @@ static int do_ipv6_setsockopt(struct sock *sk, int level, int optname,
 
 	if (needs_rtnl)
 		rtnl_lock();
-	lock_sock(sk);
+	sockopt_lock_sock(sk);
 
 	/* Another thread has converted the socket into IPv4 with
 	 * IPV6_ADDRFORM concurrently.
@@ -628,8 +628,8 @@ static int do_ipv6_setsockopt(struct sock *sk, int level, int optname,
 		break;
 
 	case IPV6_TRANSPARENT:
-		if (valbool && !ns_capable(net->user_ns, CAP_NET_RAW) &&
-		    !ns_capable(net->user_ns, CAP_NET_ADMIN)) {
+		if (valbool && !sockopt_ns_capable(net->user_ns, CAP_NET_RAW) &&
+		    !sockopt_ns_capable(net->user_ns, CAP_NET_ADMIN)) {
 			retv = -EPERM;
 			break;
 		}
@@ -940,7 +940,7 @@ done:
 	case IPV6_IPSEC_POLICY:
 	case IPV6_XFRM_POLICY:
 		retv = -EPERM;
-		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
+		if (!sockopt_ns_capable(net->user_ns, CAP_NET_ADMIN))
 			break;
 		retv = xfrm_user_policy(sk, optname, optval, optlen);
 		break;
@@ -982,14 +982,14 @@ done:
 	}
 
 unlock:
-	release_sock(sk);
+	sockopt_release_sock(sk);
 	if (needs_rtnl)
 		rtnl_unlock();
 
 	return retv;
 
 e_inval:
-	release_sock(sk);
+	sockopt_release_sock(sk);
 	if (needs_rtnl)
 		rtnl_unlock();
 	return -EINVAL;
