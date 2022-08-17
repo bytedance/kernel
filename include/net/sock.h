62e9a2edfd621f74596bae5c2dbf3f886aec5b61
Martin KaFai Lau <kafai@fb.com>
2022-08-16 23:18:04 -0700

    bpf: Change bpf_setsockopt(SOL_SOCKET) to reuse sk_setsockopt()
    commit 29003875bd5bab262a29d1c6e76a2124bd07e4c2 upstream.
    
    After the prep work in the previous patches,
    this patch removes most of the dup code from bpf_setsockopt(SOL_SOCKET)
    and reuses them from sk_setsockopt().
    
    The sock ptr test is added to the SO_RCVLOWAT because
    the sk->sk_socket could be NULL in some of the bpf hooks.
    
    The existing optname white-list is refactored into a new
    function sol_socket_setsockopt().
    
    Reviewed-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/r/20220817061804.4178920-1-kafai@fb.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Feng Zhou <zhoufeng.zf@bytedance.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1830,6 +1830,8 @@ void sock_pfree(struct sk_buff *skb);
 #define sock_edemux sock_efree
 #endif
 
+int sk_setsockopt(struct sock *sk, int level, int optname,
+		  sockptr_t optval, unsigned int optlen);
 int sock_setsockopt(struct socket *sock, int level, int op,
 		    sockptr_t optval, unsigned int optlen);
 
