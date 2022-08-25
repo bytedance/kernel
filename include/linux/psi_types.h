f062f2f4738eb0ea4e39541301e170f01012ea2a
Chengming Zhou <zhouchengming@bytedance.com>
2022-08-26 00:41:08 +0800

    sched/psi: Add PSI_IRQ to track IRQ/SOFTIRQ pressure
    commit 52b1364ba0b105122d6de0e719b36db705011ac1 upstream.
    
    Now PSI already tracked workload pressure stall information for
    CPU, memory and IO. Apart from these, IRQ/SOFTIRQ could have
    obvious impact on some workload productivity, such as web service
    workload.
    
    When CONFIG_IRQ_TIME_ACCOUNTING, we can get IRQ/SOFTIRQ delta time
    from update_rq_clock_task(), in which we can record that delta
    to CPU curr task's cgroups as PSI_IRQ_FULL status.
    
    Note we don't use PSI_IRQ_SOME since IRQ/SOFTIRQ always happen in
    the current task on the CPU, make nothing productive could run
    even if it were runnable, so we only use PSI_IRQ_FULL.
    
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lore.kernel.org/r/20220825164111.29534-8-zhouchengming@bytedance.com
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- include/linux/psi_types.h
+++ include/linux/psi_types.h
@@ -41,7 +41,10 @@ enum psi_res {
 	PSI_IO,
 	PSI_MEM,
 	PSI_CPU,
-	NR_PSI_RESOURCES = 3,
+#ifdef CONFIG_IRQ_TIME_ACCOUNTING
+	PSI_IRQ,
+#endif
+	NR_PSI_RESOURCES,
 };
 
 /*
@@ -57,9 +60,12 @@ enum psi_states {
 	PSI_MEM_FULL,
 	PSI_CPU_SOME,
 	PSI_CPU_FULL,
+#ifdef CONFIG_IRQ_TIME_ACCOUNTING
+	PSI_IRQ_FULL,
+#endif
 	/* Only per-CPU, to weigh the CPU in the global average: */
 	PSI_NONIDLE,
-	NR_PSI_STATES = 7,
+	NR_PSI_STATES,
 };
 
 /* Use one bit in the state mask to track TSK_ONCPU */
