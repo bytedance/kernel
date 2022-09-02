4ae7ea9cff0881367620b5619e562e3902ff3a83
James Morse <james.morse@arm.com>
2022-09-02 15:48:09 +0000

    x86/resctrl: Kill off alloc_enabled
    commit 4d269ed485298e8a09485a664e7b35b370ab4ada upstream.
    
    rdt_resources_all[] used to have extra entries for L2CODE/L2DATA.
    These were hidden from resctrl by the alloc_enabled value.
    
    Now that the L2/L2CODE/L2DATA resources have been merged together,
    alloc_enabled doesn't mean anything, it always has the same value as
    alloc_capable which indicates allocation is supported by this resource.
    
    Remove alloc_enabled and its helpers.
    
    Intel-SIG: commit 4d269ed48529 x86/resctrl: Kill off alloc_enabled.
    Incremental backporting patches for Intel RDT on Intel Xeon platform.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Jamie Iles <quic_jiles@quicinc.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@fujitsu.com>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Tested-by: Xin Hao <xhao@linux.alibaba.com>
    Tested-by: Shaopeng Tan <tan.shaopeng@fujitsu.com>
    Tested-by: Cristian Marussi <cristian.marussi@arm.com>
    Link: https://lore.kernel.org/r/20220902154829.30399-2-james.morse@arm.com
    [ Xiaochen Shen: amend commit log ]
    Signed-off-by: Xiaochen Shen <xiaochen.shen@intel.com>
    Signed-off-by: Gang Li <ligang.bdlg@bytedance.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -141,7 +141,6 @@ static inline void cache_alloc_hsw_probe(void)
 	r->cache.shareable_bits = 0xc0000;
 	r->cache.min_cbm_bits = 2;
 	r->alloc_capable = true;
-	r->alloc_enabled = true;
 
 	rdt_alloc_capable = true;
 }
@@ -205,7 +204,6 @@ static bool __get_mem_config_intel(struct rdt_resource *r)
 	thread_throttle_mode_init();
 
 	r->alloc_capable = true;
-	r->alloc_enabled = true;
 
 	return true;
 }
@@ -236,7 +234,6 @@ static bool __rdt_get_mem_config_amd(struct rdt_resource *r)
 	r->data_width = 4;
 
 	r->alloc_capable = true;
-	r->alloc_enabled = true;
 
 	return true;
 }
@@ -255,7 +252,6 @@ static void rdt_get_cache_alloc_cfg(int idx, struct rdt_resource *r)
 	r->cache.shareable_bits = ebx & r->default_ctrl;
 	r->data_width = (r->cache.cbm_len + 3) / 4;
 	r->alloc_capable = true;
-	r->alloc_enabled = true;
 }
 
 static void rdt_get_cdp_config(int level)
