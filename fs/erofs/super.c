36217080b3fd2bccadd6068a6c3ccdb1d6c3953e
Jia Zhu <zhujia.zj@bytedance.com>
2022-09-18 12:34:56 +0800

    erofs: introduce 'domain_id' mount option
    commit 2ef164414123fcf574aff7a0be5f71f7e60a3fec upstream.
    
    Introduce 'domain_id' mount option to enable shared domain sementics.
    In which case, the related cookie is shared if two mountpoints in the
    same domain have the same data blob. Users could specify the name of
    domain by this mount option.
    
    Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>
    Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20220918043456.147-7-zhujia.zj@bytedance.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -320,6 +320,7 @@ enum {
 	Opt_cache_strategy,
 	Opt_device,
 	Opt_fsid,
+	Opt_domain_id,
 	Opt_err
 };
 
@@ -336,6 +337,7 @@ static const struct fs_parameter_spec erofs_param_specs[] = {
 	fsparam_enum("cache_strategy",	Opt_cache_strategy),
 	fsparam_string("device",	Opt_device),
 	fsparam_string("fsid",		Opt_fsid),
+	fsparam_string("domain_id",	Opt_domain_id),
 	{}
 };
 
@@ -420,6 +422,16 @@ static int erofs_fc_parse_param(struct fs_context *fc,
 #else
 		errorf(fc, "fsid option not supported");
 		return -EINVAL;
+#endif
+		break;
+	case Opt_domain_id:
+#ifdef CONFIG_EROFS_FS_ONDEMAND
+		kfree(ctx->opt.domain_id);
+		ctx->opt.domain_id = kstrdup(param->string, GFP_KERNEL);
+		if (!ctx->opt.domain_id)
+			return -ENOMEM;
+#else
+		errorf(fc, "domain_id option not supported");
 #endif
 		break;
 	default:
@@ -513,6 +525,7 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 	sb->s_fs_info = sbi;
 	sbi->opt = ctx->opt;
 	ctx->opt.fsid = NULL;
+	ctx->opt.domain_id = NULL;
 	sbi->devs = ctx->devs;
 	ctx->devs = NULL;
 
@@ -640,6 +653,7 @@ static void erofs_fc_free(struct fs_context *fc)
 
 	erofs_free_dev_context(ctx->devs);
 	kfree(ctx->opt.fsid);
+	kfree(ctx->opt.domain_id);
 	kfree(ctx);
 }
 
@@ -708,6 +722,7 @@ static void erofs_kill_sb(struct super_block *sb)
 	erofs_free_dev_context(sbi->devs);
 	erofs_fscache_unregister_fs(sb);
 	kfree(sbi->opt.fsid);
+	kfree(sbi->opt.domain_id);
 	kfree(sbi);
 	sb->s_fs_info = NULL;
 }
@@ -845,8 +860,10 @@ static int erofs_show_options(struct seq_file *seq, struct dentry *root)
 		seq_puts(seq, ",cache_strategy=readaround");
 #endif
 #ifdef CONFIG_EROFS_FS_ONDEMAND
-	if (sbi->opt.fsid)
-		seq_printf(seq, ",fsid=%s", sbi->opt.fsid);
+	if (opt->fsid)
+		seq_printf(seq, ",fsid=%s", opt->fsid);
+	if (opt->domain_id)
+		seq_printf(seq, ",domain_id=%s", opt->domain_id);
 #endif
 	return 0;
 }
