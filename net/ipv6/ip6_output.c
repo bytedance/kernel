85fbbe1f769730134afa7f8dca3a6d24eb0d2dfd
Pavel Begunkov <asml.silence@gmail.com>
2022-09-23 17:39:04 +0100

    net: shrink struct ubuf_info
    commit e7d2b51016 upstream
    fix conflict.
    
    We can benefit from a smaller struct ubuf_info, so leave only mandatory
    fields and let users to decide how they want to extend it. Convert
    MSG_ZEROCOPY to struct ubuf_info_msgzc and remove duplicated fields.
    This reduces the size from 48 bytes to just 16.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1595,7 +1595,7 @@ emsgsize:
 				paged = true;
 				zc = true;
 			} else {
-				uarg->zerocopy = 0;
+				uarg_to_msgzc(uarg)->zerocopy = 0;
 				skb_zcopy_set(skb, uarg, &extra_uref);
 			}
 		}
