edffaf538316a544fa93f1a6877f2fb605cb1cb4
Yu Kuai <yukuai3@huawei.com>
2022-10-12 17:40:32 +0800

    blk-iocost: disable writeback throttling
    commit 8796acbc9a0eceeddd99eaef833bdda1241d39b9 upstream.
    
    Commit b5dc5d4d1f4f ("block,bfq: Disable writeback throttling") disable
    wbt for bfq, because different write-throttling heuristics should not
    work together.
    
    For the same reason, wbt and iocost should not work together as well,
    unless admin really want to do that, dispite that performance is
    affected.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://lore.kernel.org/r/20221012094035.390056-2-yukuai1@huaweicloud.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Xin Yin <yinxin.x@bytedance.com>

--- block/blk-iocost.c
+++ block/blk-iocost.c
@@ -3195,6 +3195,7 @@ static ssize_t ioc_qos_write(struct kernfs_open_file *of, char *input,
 			     size_t nbytes, loff_t off)
 {
 	struct block_device *bdev;
+	struct gendisk *disk;
 	struct ioc *ioc;
 	u32 qos[NR_QOS_PARAMS];
 	bool enable, user;
@@ -3204,6 +3205,7 @@ static ssize_t ioc_qos_write(struct kernfs_open_file *of, char *input,
 	bdev = blkcg_conf_open_bdev(&input);
 	if (IS_ERR(bdev))
 		return PTR_ERR(bdev);
+	disk = bdev->bd_disk;
 
 	ioc = q_to_ioc(bdev_get_queue(bdev));
 	if (!ioc) {
@@ -3290,9 +3292,11 @@ static ssize_t ioc_qos_write(struct kernfs_open_file *of, char *input,
 		blk_stat_enable_accounting(ioc->rqos.q);
 		blk_queue_flag_set(QUEUE_FLAG_RQ_ALLOC_TIME, ioc->rqos.q);
 		ioc->enabled = true;
+		wbt_disable_default(disk->queue);
 	} else {
 		blk_queue_flag_clear(QUEUE_FLAG_RQ_ALLOC_TIME, ioc->rqos.q);
 		ioc->enabled = false;
+		wbt_enable_default(disk->queue);
 	}
 
 	if (user) {
