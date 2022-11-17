4e99c25bf63728a5f6db3c172a36761bfd2ccaba
Jithu Joseph <jithu.joseph@intel.com>
2022-11-16 19:59:26 -0800

    x86/microcode/intel: Reuse microcode_sanity_check()
    commit 514ee839c6d0750c1c4456502e6fa08599e57931 upstream.
    
    IFS test image carries the same microcode header as regular Intel
    microcode blobs.
    
    Reuse microcode_sanity_check() in the IFS driver to perform sanity check
    of the IFS test images too.
    
    Intel-SIG: commit 514ee839c6d0 x86/microcode/intel: Reuse microcode_sanity_check()
    Backport Intel In Field Scan(IFS) multi-blob images support.
    
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Ashok Raj <ashok.raj@intel.com>
    Reviewed-by: Sohil Mehta <sohil.mehta@intel.com>
    Link: https://lore.kernel.org/r/20221117035935.4136738-8-jithu.joseph@intel.com
    [ Aichun Shi: amend commit log ]
    Signed-off-by: Aichun Shi <aichun.shi@intel.com>
    Signed-off-by: Rui Qi <qirui.001@bytedance.com>

--- arch/x86/include/asm/cpu.h
+++ arch/x86/include/asm/cpu.h
@@ -92,5 +92,6 @@ static inline bool intel_cpu_signatures_match(unsigned int s1, unsigned int p1,
 }
 
 int intel_find_matching_signature(void *mc, unsigned int csig, int cpf);
+int intel_microcode_sanity_check(void *mc, bool print_err);
 
 #endif /* _ASM_X86_CPU_H */
