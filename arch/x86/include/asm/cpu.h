d7ed3e3ae9c4100bf156e0c047e67691479157d3
Jithu Joseph <jithu.joseph@intel.com>
2022-11-16 19:59:24 -0800

    x86/microcode/intel: Reuse find_matching_signature()
    commit 716f380275866350ee44447b3c7c999f39c3178d upstream.
    
    IFS uses test images provided by Intel that can be regarded as firmware.
    An IFS test image carries microcode header with an extended signature
    table.
    
    Reuse find_matching_signature() for verifying if the test image header
    or the extended signature table indicate whether that image is fit to
    run on a system.
    
    No functional changes.
    
    Intel-SIG: commit 716f38027586 x86/microcode/intel: Reuse find_matching_signature()
    Backport Intel In Field Scan(IFS) multi-blob images support.
    
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Ashok Raj <ashok.raj@intel.com>
    Reviewed-by: Sohil Mehta <sohil.mehta@intel.com>
    Link: https://lore.kernel.org/r/20221117035935.4136738-6-jithu.joseph@intel.com
    [ Aichun Shi: amend commit log ]
    Signed-off-by: Aichun Shi <aichun.shi@intel.com>
    Signed-off-by: Rui Qi <qirui.001@bytedance.com>

--- arch/x86/include/asm/cpu.h
+++ arch/x86/include/asm/cpu.h
@@ -91,4 +91,6 @@ static inline bool intel_cpu_signatures_match(unsigned int s1, unsigned int p1,
 	return p1 & p2;
 }
 
+int intel_find_matching_signature(void *mc, unsigned int csig, int cpf);
+
 #endif /* _ASM_X86_CPU_H */
