0f3c404227c54a71d597b3e741054b2b079f6399
Peter Xu <peterx@redhat.com>
2022-12-16 10:50:53 -0500

    mm/hugetlb: don't wait for migration entry during follow page
    commit bb373dce2c7b473023f9e69f041a22d81171b71a upstream.
    
    That's what the code does with !hugetlb pages, so we should logically do
    the same for hugetlb, so migration entry will also be treated as no page.
    
    This is probably also the last piece in follow_page code that may sleep,
    the last one should be removed in cf994dd8af27 ("mm/gup: remove
    FOLL_MIGRATION", 2022-11-16).
    
    Link: https://lkml.kernel.org/r/20221216155100.2043537-3-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: John Hubbard <jhubbard@nvidia.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: James Houghton <jthoughton@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Nadav Amit <nadav.amit@gmail.com>
    Cc: Rik van Riel <riel@surriel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5506,7 +5506,6 @@ struct page *hugetlb_follow_page_mask(struct vm_area_struct *vma,
 	if (WARN_ON_ONCE(flags & FOLL_PIN))
 		return NULL;
 
-retry:
 	pte = huge_pte_offset(mm, haddr, huge_page_size(h));
 	if (!pte)
 		return NULL;
@@ -5529,16 +5528,6 @@ retry:
 			page = NULL;
 			goto out;
 		}
-	} else {
-		if (is_hugetlb_entry_migration(entry)) {
-			spin_unlock(ptl);
-			__migration_entry_wait_huge(pte, ptl);
-			goto retry;
-		}
-		/*
-		 * hwpoisoned entry is treated as no_page_table in
-		 * follow_page_mask().
-		 */
 	}
 out:
 	spin_unlock(ptl);
