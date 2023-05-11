e3de34f428781d7eb719cafd2b48efef4a7488b6
Niklas Cassel <niklas.cassel@wdc.com>
2023-05-11 03:13:52 +0200

    scsi: ata: libata: Handle completion of CDL commands using policy 0xD
    commit 18bd7718b5c489b3161b6c2ab4685d57c1e2da3b upstream.
    
    A CDL timeout for policy 0xF is defined as a NCQ error, just with a CDL
    specific sk/asc/ascq in the sense data. Therefore, the existing code in
    libata does not need to be modified to handle a policy 0xF CDL timeout.
    
    For Command Duration Limits policy 0xD:
    
      The device shall complete the command without error with the additional
      sense code set to DATA CURRENTLY UNAVAILABLE.
    
    Since a CDL timeout for policy 0xD is not an error, we cannot use the NCQ
    Command Error log (10h).
    
    Instead, we need to read the Sense Data for Successful NCQ Commands log
    (0Fh).
    
    In the success case, just like in the error case, we cannot simply read a
    log page from the interrupt handler itself, since reading a log page
    involves sending a READ LOG DMA EXT or READ LOG EXT command.
    
    Therefore, we add a new EH action ATA_EH_GET_SUCCESS_SENSE.  When a command
    completes without error, and when the ATA_SENSE bit is set, this new action
    is set as pending, and EH is scheduled.
    
    This way, similar to the NCQ error case, the log page will be read from EH
    context.
    
    An alternative would have been to add a new kthread or workqueue to handle
    this. However, extending EH can be done with minimal changes and avoids the
    need to synchronize a new kthread/workqueue with EH.
    
    Co-developed-by: Damien Le Moal <dlemoal@kernel.org>
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Niklas Cassel <niklas.cassel@wdc.com>
    Link: https://lore.kernel.org/r/20230511011356.227789-20-nks@flawful.org
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Diangang Li <lidiangang@bytedance.com>

--- include/linux/ata.h
+++ include/linux/ata.h
@@ -326,6 +326,8 @@ enum {
 	ATA_LOG_CDL		= 0x18,
 	ATA_LOG_CDL_SIZE	= ATA_SECT_SIZE,
 	ATA_LOG_IDENTIFY_DEVICE	= 0x30,
+	ATA_LOG_SENSE_NCQ	= 0x0F,
+	ATA_LOG_SENSE_NCQ_SIZE	= ATA_SECT_SIZE * 2,
 
 	/* Identify device log pages: */
 	ATA_LOG_SUPPORTED_CAPABILITIES	= 0x03,
@@ -431,6 +433,7 @@ enum {
 	SATA_DEVSLP		= 0x09,	/* Device Sleep */
 
 	SETFEATURE_SENSE_DATA	= 0xC3, /* Sense Data Reporting feature */
+	SETFEATURE_SENSE_DATA_SUCC_NCQ = 0xC4, /* Sense Data for successful NCQ commands */
 
 	/* feature values for SET_MAX */
 	ATA_SET_MAX_ADDR	= 0x00,
