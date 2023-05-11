5e9882e76021d79b67280830bbfe638dc7473b6d
Damien Le Moal <dlemoal@kernel.org>
2023-05-11 03:13:42 +0200

    scsi: core: Allow enabling and disabling command duration limits
    commit 1b22cfb14142aba7742d307c4f8d7006f919308c upstream.
    
    Add the sysfs scsi_device attribute cdl_enable to allow a user to enable or
    disable a device command duration limits feature. CDL is disabled by
    default. This feature must be explicitly enabled by a user by setting the
    cdl_enable attribute to 1.
    
    The new function scsi_cdl_enable() does not do anything beside setting the
    cdl_enable field of struct scsi_device in the case of a (real) SCSI device
    (e.g. a SAS HDD). For ATA devices, the command duration limits feature
    needs to be enabled/disabled using the ATA feature sub-page of the control
    mode page. To do so, the scsi_cdl_enable() function checks if this mode
    page is supported using scsi_mode_sense(). If it is, scsi_mode_select() is
    used to enable and disable CDL.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Co-developed-by: Niklas Cassel <niklas.cassel@wdc.com>
    Signed-off-by: Niklas Cassel <niklas.cassel@wdc.com>
    Link: https://lore.kernel.org/r/20230511011356.227789-10-nks@flawful.org
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Diangang Li <lidiangang@bytedance.com>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -209,6 +209,7 @@ struct scsi_device {
 	unsigned silence_suspend:1;	/* Do not print runtime PM related messages */
 
 	unsigned cdl_supported:1;	/* Command duration limits supported */
+	unsigned cdl_enable:1;		/* Enable/disable Command duration limits */
 
 	bool offline_already;		/* Device offline message logged */
 
@@ -356,6 +357,7 @@ extern void scsi_remove_device(struct scsi_device *);
 extern int scsi_unregister_device_handler(struct scsi_device_handler *scsi_dh);
 void scsi_attach_vpd(struct scsi_device *sdev);
 void scsi_cdl_check(struct scsi_device *sdev);
+int scsi_cdl_enable(struct scsi_device *sdev, bool enable);
 
 extern struct scsi_device *scsi_device_from_queue(struct request_queue *q);
 extern int __must_check scsi_device_get(struct scsi_device *);
