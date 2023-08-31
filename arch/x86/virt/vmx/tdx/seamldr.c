2c68e92ebc10f8963a715aa84d645ed56369b628
Chao Gao <chao.gao@intel.com>
2023-08-31 10:18:03 +0800

    x86/virt/seamldr: Verify TDX module SHA384 hash
    P-SEAMLDR verifies the new TDX module SHA384 hash after overriding the old
    module with the new one. If the new TDX module is corrupted or doesn't
    match the sigstruct, P-SEAMLDR will reject the update, resulting in no
    TDX module in the system. This is undesired because it means, updates lead
    to TDX unusable.
    
    Calculate the hash and compare the result with the one in the provide
    sigstruct and reject TDX module updates if they mismatch.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>

--- arch/x86/virt/vmx/tdx/seamldr.c
+++ arch/x86/virt/vmx/tdx/seamldr.c
@@ -7,6 +7,7 @@
 
 #define pr_fmt(fmt)	"seamldr: " fmt
 
+#include <crypto/hash.h>
 #include <linux/cpu.h>
 #include <linux/device.h>
 #include <linux/gfp.h>
@@ -221,6 +222,37 @@ static bool pamt_can_reuse(const struct seam_sigstruct *sig)
 	return sysinfo.tdmr_info.pamt_entry_size[TDX_PS_4K] == sigstruct_pamt_entry_size(sig);
 }
 
+static int verify_hash(const void *module, int module_size,
+		       const void *expected_hash)
+{
+	SHASH_DESC_ON_STACK(shash, tfm);
+	struct crypto_shash *tfm;
+	u8 hash[48];
+	int ret;
+
+	tfm = crypto_alloc_shash("sha384", 0, CRYPTO_ALG_ASYNC);
+	if (IS_ERR(tfm)) {
+		pr_err("No tfm created\n");
+		return PTR_ERR(tfm);
+	}
+
+	shash->tfm = tfm;
+	ret = crypto_shash_digest(shash, module, module_size, hash);
+	if (ret) {
+		pr_err("cannot generate digest %d\n", ret);
+		crypto_free_shash(tfm);
+		return ret;
+	}
+
+	if (memcmp(hash, expected_hash, 48)) {
+		pr_err("Hash verification failed\n");
+		ret = -EINVAL;
+	}
+
+	crypto_free_shash(tfm);
+	return ret;
+}
+
 struct update_ctx {
 	struct seamldr_params *params;
 	const struct firmware *module, *sig;
@@ -272,6 +304,11 @@ static struct update_ctx *init_update_ctx(void)
 		ret = -EIO;
 		goto free;
 	}
+
+	ret = verify_hash(module->data, module->size,
+			  ((const struct seam_sigstruct *)(sig->data))->seamhash);
+	if (ret)
+		goto free;
 
 	params = alloc_seamldr_params(module->data, module->size,
 				      sig->data, sig->size);
