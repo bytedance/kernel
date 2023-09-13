8d4036d66cdb114838cd42950b4819e79e8aceeb
Usama Arif <usama.arif@bytedance.com>
2023-09-13 11:54:00 +0100

    memblock: introduce MEMBLOCK_RSRV_NOINIT flag
    commit 77e6c43e137c130138c3fbadc847351a83c4befe upstream.
    
    For reserved memory regions marked with this flag, reserve_bootmem_region
    is not called during memmap_init_reserved_pages.  This can be used to
    avoid struct page initialization for regions which won't need them, for
    e.g.  hugepages with Hugepage Vmemmap Optimization enabled.
    
    Link: https://lkml.kernel.org/r/20230913105401.519709-4-usama.arif@bytedance.com
    Signed-off-by: Usama Arif <usama.arif@bytedance.com>
    Acked-by: Muchun Song <songmuchun@bytedance.com>
    Reviewed-by: Mike Rapoport (IBM) <rppt@kernel.org>
    Cc: Fam Zheng <fam.zheng@bytedance.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Punit Agrawal <punit.agrawal@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Li Zhe <lizhe.67@bytedance.com>

--- mm/memblock.c
+++ mm/memblock.c
@@ -1039,6 +1039,24 @@ int __init_memblock memblock_clear_nomap(phys_addr_t base, phys_addr_t size)
 	return memblock_setclr_flag(&memblock.memory, base, size, 0, MEMBLOCK_NOMAP);
 }
 
+/**
+ * memblock_reserved_mark_noinit - Mark a reserved memory region with flag
+ * MEMBLOCK_RSRV_NOINIT which results in the struct pages not being initialized
+ * for this region.
+ * @base: the base phys addr of the region
+ * @size: the size of the region
+ *
+ * struct pages will not be initialized for reserved memory regions marked with
+ * %MEMBLOCK_RSRV_NOINIT.
+ *
+ * Return: 0 on success, -errno on failure.
+ */
+int __init_memblock memblock_reserved_mark_noinit(phys_addr_t base, phys_addr_t size)
+{
+	return memblock_setclr_flag(&memblock.reserved, base, size, 1,
+				    MEMBLOCK_RSRV_NOINIT);
+}
+
 static bool should_skip_region(struct memblock_type *type,
 			       struct memblock_region *m,
 			       int nid, int flags)
@@ -2155,16 +2173,21 @@ repeat:
 	if (max_reserved != memblock.reserved.max)
 		goto repeat;
 
-	/* initialize struct pages for the reserved regions */
+	/*
+	 * initialize struct pages for reserved regions that don't have
+	 * the MEMBLOCK_RSRV_NOINIT flag set
+	 */
 	for_each_reserved_mem_region(region) {
-		nid = memblock_get_region_node(region);
-		start = region->base;
-		end = start + region->size;
+		if (!memblock_is_reserved_noinit(region)) {
+			nid = memblock_get_region_node(region);
+			start = region->base;
+			end = start + region->size;
 
-		if (!numa_valid_node(nid))
-			nid = early_pfn_to_nid(PFN_DOWN(start));
+			if (!numa_valid_node(nid))
+				nid = early_pfn_to_nid(PFN_DOWN(start));
 
-		reserve_bootmem_region(start, end, nid);
+			reserve_bootmem_region(start, end, nid);
+		}
 	}
 }
 
