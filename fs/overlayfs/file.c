a43f1205e696fd729becc1116fe43f88f67c70d4
Amir Goldstein <amir73il@gmail.com>
2023-10-02 13:04:45 +0300

    ovl: fix file reference leak when submitting aio
    commit 8542f1712074f070ae90b64e6082d10d8e912e32 upstream.
    
    Commit 724768a39374 ("ovl: fix incorrect fdput() on aio completion")
    took a refcount on real file before submitting aio, but forgot to
    avoid clearing FDPUT_FPUT from real.flags stack variable.
    This can result in a file reference leak.
    
    Fixes: 724768a39374 ("ovl: fix incorrect fdput() on aio completion")
    Reported-by: Gil Lev <contact@levgil.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>

--- fs/overlayfs/file.c
+++ fs/overlayfs/file.c
@@ -321,7 +321,6 @@ static ssize_t ovl_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 		if (!aio_req)
 			goto out;
 
-		real.flags = 0;
 		aio_req->orig_iocb = iocb;
 		kiocb_clone(&aio_req->iocb, iocb, get_file(real.file));
 		aio_req->iocb.ki_complete = ovl_aio_rw_complete;
@@ -392,7 +391,6 @@ static ssize_t ovl_write_iter(struct kiocb *iocb, struct iov_iter *iter)
 		/* Pacify lockdep, same trick as done in aio_write() */
 		__sb_writers_release(file_inode(real.file)->i_sb,
 				     SB_FREEZE_WRITE);
-		real.flags = 0;
 		aio_req->orig_iocb = iocb;
 		kiocb_clone(&aio_req->iocb, iocb, get_file(real.file));
 		aio_req->iocb.ki_flags = ifl;
