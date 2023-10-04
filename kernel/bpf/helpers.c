1060ff4d7adfbad68825415574591599a5ed778f
David Vernet <void@manifault.com>
2023-10-04 11:23:38 -0500

    bpf: Add ability to pin bpf timer to calling CPU
    commit d6247ecb6c1e17d7a33317090627f5bfe563cbb2 upstream.
    
    BPF supports creating high resolution timers using bpf_timer_* helper
    functions. Currently, only the BPF_F_TIMER_ABS flag is supported, which
    specifies that the timeout should be interpreted as absolute time. It
    would also be useful to be able to pin that timer to a core. For
    example, if you wanted to make a subset of cores run without timer
    interrupts, and only have the timer be invoked on a single core.
    
    This patch adds support for this with a new BPF_F_TIMER_CPU_PIN flag.
    When specified, the HRTIMER_MODE_PINNED flag is passed to
    hrtimer_start(). A subsequent patch will update selftests to validate.
    
    Signed-off-by: David Vernet <void@manifault.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <song@kernel.org>
    Acked-by: Hou Tao <houtao1@huawei.com>
    Link: https://lore.kernel.org/bpf/20231004162339.200702-2-void@manifault.com
    (cherry picked from commit d6247ecb6c1e17d7a33317090627f5bfe563cbb2)
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- kernel/bpf/helpers.c
+++ kernel/bpf/helpers.c
@@ -1337,7 +1337,7 @@ BPF_CALL_3(bpf_timer_start, struct bpf_async_kern *, timer, u64, nsecs, u64, fla
 
 	if (in_nmi())
 		return -EOPNOTSUPP;
-	if (flags > BPF_F_TIMER_ABS)
+	if (flags & ~(BPF_F_TIMER_ABS | BPF_F_TIMER_CPU_PIN))
 		return -EINVAL;
 	__bpf_spin_lock_irqsave(&timer->lock);
 	t = timer->timer;
@@ -1350,6 +1350,9 @@ BPF_CALL_3(bpf_timer_start, struct bpf_async_kern *, timer, u64, nsecs, u64, fla
 		mode = HRTIMER_MODE_ABS_SOFT;
 	else
 		mode = HRTIMER_MODE_REL_SOFT;
+
+	if (flags & BPF_F_TIMER_CPU_PIN)
+		mode |= HRTIMER_MODE_PINNED;
 
 	hrtimer_start(&t->timer, ns_to_ktime(nsecs), mode);
 out:
