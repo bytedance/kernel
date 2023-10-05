370d1bdd0dd7bb372e77353e1461d4ce8e339327
Jim Mattson <jmattson@google.com>
2023-10-04 20:12:37 -0700

    x86: KVM: Add feature flag for CPUID.80000021H:EAX[bit 1]
    commit 329369caeccb1a0cb0dc5f864d6a671ed8190c11 upstream.
    
    Define an X86_FEATURE_* flag for CPUID.80000021H:EAX.[bit 1], and
    advertise the feature to userspace via KVM_GET_SUPPORTED_CPUID.
    
    Per AMD's "Processor Programming Reference (PPR) for AMD Family 19h
    Model 61h, Revision B1 Processors (56713-B1-PUB)," this CPUID bit
    indicates that a WRMSR to MSR_FS_BASE, MSR_GS_BASE, or
    MSR_KERNEL_GS_BASE is non-serializing. This is a change in previously
    architected behavior.
    
    Effectively, this CPUID bit is a "defeature" bit, or a reverse
    polarity feature bit. When this CPUID bit is clear, the feature
    (serialization on WRMSR to any of these three MSRs) is available. When
    this CPUID bit is set, the feature is not available.
    
    KVM_GET_SUPPORTED_CPUID must pass this bit through from the underlying
    hardware, if it is set. Leaving the bit clear claims that WRMSR to
    these three MSRs will be serializing in a guest running under
    KVM. That isn't true. Though KVM could emulate the feature by
    intercepting writes to the specified MSRs, it does not do so
    today. The guest is allowed direct read/write access to these MSRs
    without interception, so the innate hardware behavior is preserved
    under KVM.
    
    Signed-off-by: Jim Mattson <jmattson@google.com>
    Reviewed-by: Maxim Levitsky <mlevitsk@redhat.com>
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20231005031237.1652871-1-jmattson@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>
    Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -780,7 +780,8 @@ void kvm_set_cpu_caps(void)
 
 	kvm_cpu_cap_mask(CPUID_8000_0021_EAX,
 		F(NO_NESTED_DATA_BP) | F(LFENCE_RDTSC) | 0 /* SmmPgCfgLock */ |
-		F(NULL_SEL_CLR_BASE) | F(AUTOIBRS) | 0 /* PrefetchCtlMsr */
+		F(NULL_SEL_CLR_BASE) | F(AUTOIBRS) | 0 /* PrefetchCtlMsr */ |
+		F(WRMSR_XX_BASE_NS)
 	);
 
 	if (cpu_feature_enabled(X86_FEATURE_SRSO_NO))
