5d4d946413ddac668700c6b8773a0c402cc2d5a6
Haiyang Zhang <haiyangz@microsoft.com>
2023-10-11 13:30:44 -0700

    tcp: Set pingpong threshold via sysctl
    [ Upstream commit 562b1fdf061bff9394ccd884456ed1173c224fdc ]
    
    TCP pingpong threshold is 1 by default. But some applications, like SQL DB
    may prefer a higher pingpong threshold to activate delayed acks in quick
    ack mode for better performance.
    
    The pingpong threshold and related code were changed to 3 in the year
    2019 in:
      commit 4a41f453bedf ("tcp: change pingpong threshold to 3")
    And reverted to 1 in the year 2022 in:
      commit 4d8f24eeedc5 ("Revert "tcp: change pingpong threshold to 3"")
    
    There is no single value that fits all applications.
    Add net.ipv4.tcp_pingpong_thresh sysctl tunable, so it can be tuned for
    optimal performance based on the application needs.
    
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Link: https://lore.kernel.org/r/1697056244-21888-1-git-send-email-haiyangz@microsoft.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 87b08913a9ae ("inet: move icmp_global_{credit,stamp} to a separate cache line")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -1505,6 +1505,14 @@ static struct ctl_table ipv4_net_table[] = {
 		.mode           = 0644,
 		.proc_handler   = proc_dointvec_minmax,
 	},
+	{
+		.procname	= "tcp_pingpong_thresh",
+		.data		= &init_net.ipv4.sysctl_tcp_pingpong_thresh,
+		.maxlen		= sizeof(u8),
+		.mode		= 0644,
+		.proc_handler	= proc_dou8vec_minmax,
+		.extra1		= SYSCTL_ONE,
+	},
 	{ }
 };
 
