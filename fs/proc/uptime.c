38242fabab8c2bf639fc90c4803d263227d95aca
Zhang Yuchen <zhangyuchen.lcr@bytedance.com>
2023-10-12 16:31:32 +0800

    bytedance: uptime: override /proc/uptime in cgroup
    In the case of CONFIG_CGROUP_OVERRIDE_PROC, uptime displays the time from
    the creation time of the init task of the current pid namespace to the
    present time, and idle time only takes the idle time of the cpu in the
    cgroup.
    
    Signed-off-by: Zhang Yuchen <zhangyuchen.lcr@bytedance.com>

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -7,27 +7,44 @@
 #include <linux/time.h>
 #include <linux/time_namespace.h>
 #include <linux/kernel_stat.h>
+#include <linux/cgroup.h>
+#include <linux/sched/task.h>
 
 static int uptime_proc_show(struct seq_file *m, void *v)
 {
+	struct cpumask msk;
+	struct kernel_cpustat kcpustat;
+	struct task_struct *tsk;
 	struct timespec64 uptime;
 	struct timespec64 idle;
-	u64 idle_nsec;
+	u64 nsec = 0;
 	u32 rem;
 	int i;
-
-	idle_nsec = 0;
-	for_each_possible_cpu(i) {
-		struct kernel_cpustat kcs;
-
-		kcpustat_cpu_fetch(&kcs, i);
-		idle_nsec += get_idle_time(&kcs, i);
-	}
 
 	ktime_get_boottime_ts64(&uptime);
 	timens_add_boottime(&uptime);
 
-	idle.tv_sec = div_u64_rem(idle_nsec, NSEC_PER_SEC, &rem);
+	if (cgroup_override_proc()) {
+		tsk = cgroup_override_get_init_tsk();
+		uptime = timespec64_sub(uptime,
+					ns_to_timespec64(tsk->start_time));
+
+		cgroup_override_get_raw_cpuset(&msk);
+		for_each_cpu(i, &msk) {
+			if (cpuacct_get_kcpustat(tsk, i, &kcpustat))
+				nsec += (__force u64)kcpustat.cpustat[CPUTIME_IDLE];
+		}
+		put_task_struct(tsk);
+	} else {
+		for_each_possible_cpu(i) {
+			struct kernel_cpustat kcs;
+
+			kcpustat_cpu_fetch(&kcs, i);
+			nsec += get_idle_time(&kcs, i);
+		}
+	}
+
+	idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
 	idle.tv_nsec = rem;
 	seq_printf(m, "%lu.%02lu %lu.%02lu\n",
 			(unsigned long) uptime.tv_sec,
