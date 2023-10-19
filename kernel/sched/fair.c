d4f63c3bc46dbe8cca5f28a353ea4c9be8c9fc1c
Yicong Yang <yangyicong@hisilicon.com>
2023-10-19 11:33:23 +0800

    sched/fair: Use candidate prev/recent_used CPU if scanning failed for cluster wakeup
    commit 7fac96e4cfc5bd04b97d0e4dc20bae0a3d12c6ec upstream.
    
    Chen Yu reports a hackbench regression of cluster wakeup when
    hackbench threads equal to the CPU number [1]. Analysis shows
    it's because we wake up more on the target CPU even if the
    prev_cpu is a good wakeup candidate and leads to the decrease
    of the CPU utilization.
    
    Generally if the task's prev_cpu is idle we'll wake up the task
    on it without scanning. On cluster machines we'll try to wake up
    the task in the same cluster of the target for better cache
    affinity, so if the prev_cpu is idle but not sharing the same
    cluster with the target we'll still try to find an idle CPU within
    the cluster. This will improve the performance at low loads on
    cluster machines. But in the issue above, if the prev_cpu is idle
    but not in the cluster with the target CPU, we'll try to scan an
    idle one in the cluster. But since the system is busy, we're
    likely to fail the scanning and use target instead, even if
    the prev_cpu is idle. Then leads to the regression.
    
    This patch solves this in 2 steps:
    o record the prev_cpu/recent_used_cpu if they're good wakeup
      candidates but not sharing the cluster with the target.
    o on scanning failure use the prev_cpu/recent_used_cpu if
      they're recorded as idle
    
    [1] https://lore.kernel.org/all/ZGzDLuVaHR1PAYDt@chenyu5-mobl1/
    
    Closes: https://lore.kernel.org/all/ZGsLy83wPIpamy6x@chenyu5-mobl1/
    Reported-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Tested-and-reviewed-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20231019033323.54147-4-yangyicong@huawei.com
    Signed-off-by: huwentao <huwentao19@h-partners.com>
    Signed-off-by: Jinhui Guo <guojinhui.liam@bytedance.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6609,7 +6609,7 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 	bool has_idle_core = false;
 	struct sched_domain *sd;
 	unsigned long task_util, util_min, util_max;
-	int i, recent_used_cpu;
+	int i, recent_used_cpu, prev_aff = -1;
 
 	/*
 	 * On asymmetric system, update task utilization because we will check
@@ -6641,6 +6641,8 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 		if (!static_branch_unlikely(&sched_cluster_active) ||
 		    cpus_share_resources(prev, target))
 			return prev;
+
+		prev_aff = prev;
 	}
 
 	/*
@@ -6673,6 +6675,8 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 		    cpus_share_resources(recent_used_cpu, target))
 			return recent_used_cpu;
 
+	} else {
+		recent_used_cpu = -1;
 	}
 
 	/*
@@ -6712,6 +6716,17 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 	i = select_idle_cpu(p, sd, has_idle_core, target);
 	if ((unsigned)i < nr_cpumask_bits)
 		return i;
+
+	/*
+	 * For cluster machines which have lower sharing cache like L2 or
+	 * LLC Tag, we tend to find an idle CPU in the target's cluster
+	 * first. But prev_cpu or recent_used_cpu may also be a good candidate,
+	 * use them if possible when no idle CPU found in select_idle_cpu().
+	 */
+	if ((unsigned int)prev_aff < nr_cpumask_bits)
+		return prev_aff;
+	if ((unsigned int)recent_used_cpu < nr_cpumask_bits)
+		return recent_used_cpu;
 
 	return target;
 }
