af54f8c9ae671e6129fb1767f2f057d0e16fd29e
Yafang Shao <laoar.shao@gmail.com>
2023-10-29 06:14:30 +0000

    cgroup: Eliminate the need for cgroup_mutex in proc_cgroup_show()
    commit 9067d90006df089b9a1da0d74f0cad232a5d726a upstream.
    
    The cgroup root_list is already RCU-safe. Therefore, we can replace the
    cgroup_mutex with the RCU read lock in some particular paths. This change
    will be particularly beneficial for frequent operations, such as
    `cat /proc/self/cgroup`, in a cgroup1-based container environment.
    
    I did stress tests with this change, as outlined below
    (with CONFIG_PROVE_RCU_LIST enabled):
    
    - Continuously mounting and unmounting named cgroups in some tasks,
      for example:
    
      cgrp_name=$1
      while true
      do
          mount -t cgroup -o none,name=$cgrp_name none /$cgrp_name
          umount /$cgrp_name
      done
    
    - Continuously triggering proc_cgroup_show() in some tasks concurrently,
      for example:
      while true; do cat /proc/self/cgroup > /dev/null; done
    
    They can ran successfully after implementing this change, with no RCU
    warnings in dmesg.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Songtang Liu <liusongtang@bytedance.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -6108,7 +6108,7 @@ int proc_cgroup_show(struct seq_file *m, struct pid_namespace *ns,
 	if (!buf)
 		goto out;
 
-	mutex_lock(&cgroup_mutex);
+	rcu_read_lock();
 	spin_lock_irq(&css_set_lock);
 
 	for_each_root(root) {
@@ -6117,6 +6117,11 @@ int proc_cgroup_show(struct seq_file *m, struct pid_namespace *ns,
 		int ssid, count = 0;
 
 		if (root == &cgrp_dfl_root && !READ_ONCE(cgrp_dfl_visible))
+			continue;
+
+		cgrp = task_cgroup_from_root(tsk, root);
+		/* The root has already been unmounted. */
+		if (!cgrp)
 			continue;
 
 		seq_printf(m, "%d:", root->hierarchy_id);
@@ -6129,9 +6134,6 @@ int proc_cgroup_show(struct seq_file *m, struct pid_namespace *ns,
 			seq_printf(m, "%sname=%s", count ? "," : "",
 				   root->name);
 		seq_putc(m, ':');
-
-		cgrp = task_cgroup_from_root(tsk, root);
-
 		/*
 		 * On traditional hierarchies, all zombie tasks show up as
 		 * belonging to the root cgroup.  On the default hierarchy,
@@ -6163,7 +6165,7 @@ int proc_cgroup_show(struct seq_file *m, struct pid_namespace *ns,
 	retval = 0;
 out_unlock:
 	spin_unlock_irq(&css_set_lock);
-	mutex_unlock(&cgroup_mutex);
+	rcu_read_unlock();
 	kfree(buf);
 out:
 	return retval;
