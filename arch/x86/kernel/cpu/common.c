a21ad21491c0768b4186863dac6326613d6e76da
Kai Huang <kai.huang@intel.com>
2023-12-08 09:07:21 -0800

    x86/virt/tdx: Detect TDX during kernel boot
    commit 765a0542fdc7aad7cbc1da3bd19bed6297b54e2c upstream.
    
    Intel Trust Domain Extensions (TDX) protects guest VMs from malicious
    host and certain physical attacks.  A CPU-attested software module
    called 'the TDX module' runs inside a new isolated memory range as a
    trusted hypervisor to manage and run protected VMs.
    
    Pre-TDX Intel hardware has support for a memory encryption architecture
    called MKTME.  The memory encryption hardware underpinning MKTME is also
    used for Intel TDX.  TDX ends up "stealing" some of the physical address
    space from the MKTME architecture for crypto-protection to VMs.  The
    BIOS is responsible for partitioning the "KeyID" space between legacy
    MKTME and TDX.  The KeyIDs reserved for TDX are called 'TDX private
    KeyIDs' or 'TDX KeyIDs' for short.
    
    During machine boot, TDX microcode verifies that the BIOS programmed TDX
    private KeyIDs consistently and correctly programmed across all CPU
    packages.  The MSRs are locked in this state after verification.  This
    is why MSR_IA32_MKTME_KEYID_PARTITIONING gets used for TDX enumeration:
    it indicates not just that the hardware supports TDX, but that all the
    boot-time security checks passed.
    
    The TDX module is expected to be loaded by the BIOS when it enables TDX,
    but the kernel needs to properly initialize it before it can be used to
    create and run any TDX guests.  The TDX module will be initialized by
    the KVM subsystem when KVM wants to use TDX.
    
    Detect platform TDX support by detecting TDX private KeyIDs.
    
    The TDX module itself requires one TDX KeyID as the 'TDX global KeyID'
    to protect its metadata.  Each TDX guest also needs a TDX KeyID for its
    own protection.  Just use the first TDX KeyID as the global KeyID and
    leave the rest for TDX guests.  If no TDX KeyID is left for TDX guests,
    disable TDX as initializing the TDX module alone is useless.
    
    [ dhansen: add X86_FEATURE, replace helper function ]
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Link: https://lore.kernel.org/all/20231208170740.53979-1-dave.hansen%40intel.com
    Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -65,6 +65,7 @@
 #include <asm/set_memory.h>
 #include <asm/traps.h>
 #include <asm/sev.h>
+#include <asm/tdx.h>
 
 #include "cpu.h"
 
@@ -2044,6 +2045,7 @@ static __init void identify_boot_cpu(void)
 	setup_cr_pinning();
 
 	tsx_init();
+	tdx_init();
 	lkgs_init();
 }
 
