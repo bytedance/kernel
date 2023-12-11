06a15b5b0c83a8c7fed50d1abe2e9b5e3958af3c
James Clark <james.clark@arm.com>
2023-12-11 16:13:14 +0000

    arm: perf/kvm: Use GENMASK for ARMV8_PMU_PMCR_N
    commit 62e1f212e5fe7624249212813ee96202e0c31430 upstream.
    
    This is so that FIELD_GET and FIELD_PREP can be used and that the fields
    are in a consistent format to arm64/tools/sysreg
    
    Signed-off-by: James Clark <james.clark@arm.com>
    Link: https://lore.kernel.org/r/20231211161331.1277825-3-james.clark@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Junhao He <hejunhao3@huawei.com>
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Slim6882 <15605196882@163.com>
    Signed-off-by: chenyi <chenyi211@h-partners.com>
    Signed-off-by: Shi Yang <shiyang50@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -731,8 +731,7 @@ static u64 reset_pmu_reg(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 	if (!kvm_arm_support_pmu_v3())
 		return 0;
 
-	n = read_sysreg(pmcr_el0) >> ARMV8_PMU_PMCR_N_SHIFT;
-	n &= ARMV8_PMU_PMCR_N_MASK;
+	n = FIELD_GET(ARMV8_PMU_PMCR_N, read_sysreg(pmcr_el0));
 	if (n)
 		mask |= GENMASK(n - 1, 0);
 
@@ -779,7 +778,7 @@ static u64 reset_pmcr(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 		return 0;
 
 	/* Only preserve PMCR_EL0.N, and reset the rest to 0 */
-	pmcr = read_sysreg(pmcr_el0) & (ARMV8_PMU_PMCR_N_MASK << ARMV8_PMU_PMCR_N_SHIFT);
+	pmcr = read_sysreg(pmcr_el0) & ARMV8_PMU_PMCR_N;
 	if (!kvm_supports_32bit_el0())
 		pmcr |= ARMV8_PMU_PMCR_LC;
 
@@ -890,7 +889,7 @@ static bool pmu_counter_idx_valid(struct kvm_vcpu *vcpu, u64 idx)
 	u64 pmcr, val;
 
 	pmcr = kvm_vcpu_read_pmcr(vcpu);
-	val = (pmcr >> ARMV8_PMU_PMCR_N_SHIFT) & ARMV8_PMU_PMCR_N_MASK;
+	val = FIELD_GET(ARMV8_PMU_PMCR_N, pmcr);
 	if (idx >= val && idx != ARMV8_PMU_CYCLE_IDX) {
 		kvm_inject_undefined(vcpu);
 		return false;
