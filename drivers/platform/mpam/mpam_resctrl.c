c5de19b65df5cb6439069efa01443df8196c815d
James Morse <james.morse@arm.com>
2024-01-22 14:58:25 +0800

    downstream: arm_mpam: resctrl: Tell resctrl about cpu/domain online/offline
    Now that mpam links against resctrl, call the cpu and domain
    online/offline calls at the appropriate point.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/platform/mpam/mpam_resctrl.c
+++ drivers/platform/mpam/mpam_resctrl.c
@@ -823,7 +823,7 @@ int mpam_resctrl_setup(void)
 			pr_warn("Number of PMG is not a power of 2! resctrl may misbehave");
 		}
 
-		/* TODO: call resctrl_init() */
+		err = resctrl_init();
 	}
 
 	return err;
@@ -1077,7 +1077,7 @@ struct rdt_domain *resctrl_arch_find_domain(struct rdt_resource *r, int id)
 
 int mpam_resctrl_online_cpu(unsigned int cpu)
 {
-	int i;
+	int i, err;
 	struct mpam_resctrl_dom *dom;
 	struct mpam_resctrl_res *res;
 
@@ -1096,8 +1096,12 @@ int mpam_resctrl_online_cpu(unsigned int cpu)
 		dom = mpam_resctrl_alloc_domain(cpu, res);
 		if (IS_ERR(dom))
 			return PTR_ERR(dom);
+		err = resctrl_online_domain(&res->resctrl_res, &dom->resctrl_dom);
+		if (err)
+			return err;
 	}
 
+	resctrl_online_cpu(cpu);
 	return 0;
 }
 
@@ -1107,6 +1111,8 @@ int mpam_resctrl_offline_cpu(unsigned int cpu)
 	struct rdt_domain *d;
 	struct mpam_resctrl_res *res;
 	struct mpam_resctrl_dom *dom;
+
+	resctrl_offline_cpu(cpu);
 
 	for (i = 0; i < RDT_NUM_RESOURCES; i++) {
 		res = &mpam_resctrl_exports[i];
@@ -1126,6 +1132,7 @@ int mpam_resctrl_offline_cpu(unsigned int cpu)
 		if (!cpumask_empty(&d->cpu_mask))
 			continue;
 
+		resctrl_offline_domain(&res->resctrl_res, &dom->resctrl_dom);
 		list_del(&d->list);
 		kfree(dom);
 	}
