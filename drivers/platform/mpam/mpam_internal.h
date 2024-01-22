4742da84d50b2d23802a51b4a6963ae86dfd750f
James Morse <james.morse@arm.com>
2024-01-22 14:57:57 +0800

    downstream: arm_mpam: Use the arch static key to indicate when mpam is enabled
    Once all the MSC have been probed, the system wide usable number of
    PARTID is known and the configuration arrays can be allocated.
    
    After this point, checking all the MSC have been probed is pointless,
    and the cpuhp callbacks should restore the configuration, instead of
    just resetting the MSC.
    
    Enable the architecture's static key that indicates whether mpam is
    enabled and use this to skip the discovery work on cpu hotplug.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/platform/mpam/mpam_internal.h
+++ drivers/platform/mpam/mpam_internal.h
@@ -8,11 +8,19 @@
 #include <linux/atomic.h>
 #include <linux/cpumask.h>
 #include <linux/io.h>
+#include <linux/jump_label.h>
 #include <linux/mailbox_client.h>
 #include <linux/mutex.h>
 #include <linux/resctrl.h>
 #include <linux/sizes.h>
 #include <linux/srcu.h>
+
+DECLARE_STATIC_KEY_FALSE(mpam_enabled);
+
+static inline bool mpam_is_enabled(void)
+{
+	return static_branch_likely(&mpam_enabled);
+}
 
 struct mpam_msc {
 	/* member of mpam_all_msc */
