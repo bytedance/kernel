c8862ce467fd4016c78406a95dc24ebb194e8440
James Morse <james.morse@arm.com>
2024-01-22 14:57:39 +0800

    downstream: ACPI / PPTT: Add a helper to fill a cpumask from a cache_id
    MPAM identifies CPUs by the cache_id in the PPTT cache structure.
    
    The driver needs to know which CPUs are associated with the cache,
    the CPUs may not all be online, so cacheinfo does not have the
    information.
    
    Add a helper to pull this information out of the PPTT.
    
    CC: Rohit Mathew <Rohit.Mathew@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1500,6 +1500,7 @@ int find_acpi_cpu_topology_package(unsigned int cpu);
 int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
 int find_acpi_cache_level_from_id(u32 cache_id);
 int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id, cpumask_t *cpus);
+int acpi_pptt_get_cpumask_from_cache_id(u32 cache_id, cpumask_t *cpus);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
 {
@@ -1527,6 +1528,11 @@ static inline int find_acpi_cache_level_from_id(u32 cache_id)
 }
 static inline int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id,
 						    cpumask_t *cpus)
+{
+	return -EINVAL;
+}
+static inline int acpi_pptt_get_cpumask_from_cache_id(u32 cache_id,
+						      cpumask_t *cpus)
 {
 	return -EINVAL;
 }
