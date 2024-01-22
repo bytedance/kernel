09297bb294e3f62b7015b370ad643c0fe702de44
Rohit Mathew <rohit.mathew@arm.com>
2024-01-22 14:58:04 +0800

    downstream: arm_mpam: Use long MBWU counters if supported
    If the 44 bit (long) or 63 bit (LWD) counters are detected on probing
    the RIS, use long/LWD counter instead of the regular 31 bit mbwu
    counter.
    
    Only 32bit accesses to the MSC are required to be supported by the
    spec, but these registers are 64bits. The lower half may overflow
    into the higher half between two 32bit reads. To avoid this, use
    a helper that reads the top half twice to check for overflow.
    
    Signed-off-by: Rohit Mathew <rohit.mathew@arm.com>
    [morse: merged multiple patches from Rohit]
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/platform/mpam/mpam_internal.h
+++ drivers/platform/mpam/mpam_internal.h
@@ -326,6 +326,8 @@ int mpam_msmon_read(struct mpam_component *comp, struct mon_cfg *ctx,
 #define MSMON_CSU_CAPTURE       0x0848  /* last cache-usage value captured */
 #define MSMON_MBWU              0x0860  /* current mem-bw usage value */
 #define MSMON_MBWU_CAPTURE      0x0868  /* last mem-bw value captured */
+#define MSMON_MBWU_L		0x0880  /* current long mem-bw usage value */
+#define MSMON_MBWU_CAPTURE_L	0x0890  /* last long mem-bw value captured */
 #define MSMON_CAPT_EVNT         0x0808  /* signal a capture event */
 #define MPAMF_ESR               0x00F8  /* error status register */
 #define MPAMF_ECR               0x00F0  /* error control register */
@@ -529,7 +531,10 @@ int mpam_msmon_read(struct mpam_component *comp, struct mon_cfg *ctx,
  */
 #define MSMON___VALUE          GENMASK(30, 0)
 #define MSMON___NRDY           BIT(31)
-#define MSMON_MBWU_L_VALUE     GENMASK(62, 0)
+#define MSMON___NRDY_L		BIT(63)
+#define MSMON___L_VALUE		GENMASK(43, 0)
+#define MSMON___LWD_VALUE	GENMASK(62, 0)
+
 /*
  * MSMON_CAPT_EVNT - Memory system performance monitoring capture event
  *                  generation register
