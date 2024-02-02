3a1fb38ec3aca60a270543364f889c9ba6919bb7
Hou Tao <houtao1@huawei.com>
2024-02-02 18:39:33 +0800

    x86/mm: Move is_vsyscall_vaddr() into asm/vsyscall.h
    commit ee0e39a63b78849f8abbef268b13e4838569f646 upstream.
    
    Move is_vsyscall_vaddr() into asm/vsyscall.h to make it available for
    copy_from_kernel_nofault_allowed() in arch/x86/mm/maccess.c.
    
    Reviewed-by: Sohil Mehta <sohil.mehta@intel.com>
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Link: https://lore.kernel.org/r/20240202103935.3154011-2-houtao@huaweicloud.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Zhang Yuchen <zhangyuchen.lcr@bytedance.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -789,15 +789,6 @@ show_signal_msg(struct pt_regs *regs, unsigned long error_code,
 	trace_segfault(address, regs, error_code, tsk);
 }
 
-/*
- * The (legacy) vsyscall page is the long page in the kernel portion
- * of the address space that has user-accessible permissions.
- */
-static bool is_vsyscall_vaddr(unsigned long vaddr)
-{
-	return unlikely((vaddr & PAGE_MASK) == VSYSCALL_ADDR);
-}
-
 static void
 __bad_area_nosemaphore(struct pt_regs *regs, unsigned long error_code,
 		       unsigned long address, u32 pkey, int si_code)
