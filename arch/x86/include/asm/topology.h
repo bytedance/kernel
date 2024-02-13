1207a10aed08d36ac1de59b71c2e97381b6de331
Thomas Gleixner <tglx@linutronix.de>
2024-02-13 22:04:10 +0100

    x86/cpu/amd: Provide a separate accessor for Node ID
    commit 7e3ec6286753b404666af9a58d283690302c9321 upstream.
    
    AMD (ab)uses topology_die_id() to store the Node ID information and
    topology_max_dies_per_pkg to store the number of nodes per package.
    
    This collides with the proper processor die level enumeration which is
    coming on AMD with CPUID 8000_0026, unless there is a correlation between
    the two. There is zero documentation about that.
    
    So provide new storage and new accessors which for now still access die_id
    and topology_max_die_per_pkg(). Will be mopped up after AMD and HYGON are
    converted over.
    
    tweak: topology_amd_nodes_per_pkg declaration which fix by
    b0402403e54ae9eb94ce1cbb53c7def776e97426.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Juergen Gross <jgross@suse.com>
    Tested-by: Sohil Mehta <sohil.mehta@intel.com>
    Tested-by: Michael Kelley <mhklinux@outlook.com>
    Tested-by: Zhang Rui <rui.zhang@intel.com>
    Tested-by: Wang Wendy <wendy.wang@intel.com>
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://lore.kernel.org/r/20240212153624.956116738@linutronix.de
    Signed-off-by: Malathi <amalathi@amd.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>
    Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>

--- arch/x86/include/asm/topology.h
+++ arch/x86/include/asm/topology.h
@@ -112,6 +112,8 @@ extern const struct cpumask *cpu_clustergroup_mask(int cpu);
 #define topology_core_id(cpu)			(cpu_data(cpu).cpu_core_id)
 #define topology_ppin(cpu)			(cpu_data(cpu).ppin)
 
+#define topology_amd_node_id(cpu)		(cpu_data(cpu).topo.die_id)
+
 extern unsigned int __max_die_per_package;
 
 #ifdef CONFIG_SMP
@@ -143,6 +145,11 @@ int topology_update_package_map(unsigned int apicid, unsigned int cpu);
 int topology_update_die_map(unsigned int dieid, unsigned int cpu);
 int topology_phys_to_logical_pkg(unsigned int pkg);
 
+static inline unsigned int topology_amd_nodes_per_pkg(void)
+{
+	return __max_die_per_package;
+}
+
 extern struct cpumask __cpu_primary_thread_mask;
 #define cpu_primary_thread_mask ((const struct cpumask *)&__cpu_primary_thread_mask)
 
@@ -165,6 +172,7 @@ static inline int topology_phys_to_logical_pkg(unsigned int pkg) { return 0; }
 static inline int topology_max_die_per_package(void) { return 1; }
 static inline int topology_max_smt_threads(void) { return 1; }
 static inline bool topology_is_primary_thread(unsigned int cpu) { return true; }
+static inline unsigned int topology_amd_nodes_per_pkg(void) { return 1; }
 #endif /* !CONFIG_SMP */
 
 static inline void arch_fix_phys_package_id(int num, u32 slot)
