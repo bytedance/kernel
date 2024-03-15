39f6dd6e9d765af700487f5bf89a57bd01d46bfd
Matthew Wilcox (Oracle) <willy@infradead.org>
2024-03-15 14:08:21 +0000

    mm: increase folio batch size
    commit 9cecde80aae0fb0aa44425575d5aca71bc646d89 upstream.
    
    On a 104 thread, 2 socket Skylake system, Intel report a 4.7% performance
    reduction with will-it-scale page_fault2.  This was due to reducing the
    size of the batch from 32 to 15.  Increasing the folio batch size from 15
    to 31 gives a performance increase of 12.5% relative to the original, or
    17.2% relative to the reduced performance commit.
    
    The penalty of this commit is an additional 128 bytes of stack usage.  Six
    folio_batches are also allocated from percpu memory in cpu_fbatches so
    that will be an additional 768 bytes of percpu memory (per CPU).  Tim Chen
    originally submitted a patch like this in 2020:
    https://lore.kernel.org/linux-mm/d1cc9f12a8ad6c2a52cb600d93b06b064f2bbc57.1593205965.git.tim.c.chen@linux.intel.com/
    
    pagevec must match folio_batch on upstream.
    
    Link: https://lkml.kernel.org/r/20240315140823.2478146-1-willy@infradead.org
    Fixes: 99fbb6bfc16f ("mm: make folios_put() the basis of release_pages()")
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Tested-by: Yujie Liu <yujie.liu@intel.com>
    Reported-by: kernel test robot <oliver.sang@intel.com>
    Closes: https://lore.kernel.org/oe-lkp/202403151058.7048f6a8-oliver.sang@intel.com
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Songtang Liu <liusongtang@bytedance.com>

--- include/linux/pagevec.h
+++ include/linux/pagevec.h
@@ -11,8 +11,8 @@
 
 #include <linux/xarray.h>
 
-/* 15 pointers + header align the pagevec structure to a power of two */
-#define PAGEVEC_SIZE	15
+/* 31 pointers + header align the pagevec structure to a power of two */
+#define PAGEVEC_SIZE	31
 
 struct page;
 struct address_space;
