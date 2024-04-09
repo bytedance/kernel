18d1283478b912be0f0032a5b78002de9dc0e5b2
Baoquan He <bhe@redhat.com>
2024-04-09 12:22:38 +0800

    kexec: fix the unexpected kexec_dprintk() macro
    commit f4af41bf177add167e39e4b0203460b1d0b531f6 upstream.
    
    Jiri reported that the current kexec_dprintk() always prints out debugging
    message whenever kexec/kdmmp loading is triggered.  That is not wanted.
    The debugging message is supposed to be printed out when 'kexec -s -d' is
    specified for kexec/kdump loading.
    
    After investigating, the reason is the current kexec_dprintk() takes
    printk(KERN_INFO) or printk(KERN_DEBUG) depending on whether '-d' is
    specified.  However, distros usually have defaulg log level like below:
    
     [~]# cat /proc/sys/kernel/printk
     7       4      1       7
    
    So, even though '-d' is not specified, printk(KERN_DEBUG) also always
    prints out.  I thought printk(KERN_DEBUG) is equal to pr_debug(), it's
    not.
    
    Fix it by changing to use pr_info() instead which are expected to work.
    
    Link: https://lkml.kernel.org/r/20240409042238.1240462-1-bhe@redhat.com
    Fixes: cbc2fe9d9cb2 ("kexec_file: add kexec_file flag to control debug printing")
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Reported-by: Jiri Slaby <jirislaby@kernel.org>
    Closes: https://lore.kernel.org/all/4c775fca-5def-4a2d-8437-7130b02722a2@kernel.org
    Reviewed-by: Dave Young <dyoung@redhat.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Rui Qi <qirui.001@bytedance.com>

--- include/linux/kexec.h
+++ include/linux/kexec.h
@@ -506,10 +506,8 @@ static inline unsigned int crash_get_elfcorehdr_size(void) { return 0; }
 
 extern bool kexec_file_dbg_print;
 
-#define kexec_dprintk(fmt, ...)					\
-	printk("%s" fmt,					\
-	       kexec_file_dbg_print ? KERN_INFO : KERN_DEBUG,	\
-	       ##__VA_ARGS__)
+#define kexec_dprintk(fmt, arg...) \
+        do { if (kexec_file_dbg_print) pr_info(fmt, ##arg); } while (0)
 
 #else /* !CONFIG_KEXEC_CORE */
 struct pt_regs;
