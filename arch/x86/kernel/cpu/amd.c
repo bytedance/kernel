158605f189a647c8526152854579280c8b12edf1
Thomas Gleixner <tglx@linutronix.de>
2024-04-11 18:55:38 +0200

    x86/cpu/amd: Move TOPOEXT enablement into the topology parser
    commit 7211274fe0ee352332255e41ab5e628b86e83994 upstream.
    
    The topology rework missed that early_init_amd() tries to re-enable the
    Topology Extensions when the BIOS disabled them.
    
    The new parser is invoked before early_init_amd() so the re-enable attempt
    happens too late.
    
    Move it into the AMD specific topology parser code where it belongs.
    
    Fixes: f7fb3b2dd92c ("x86/cpu: Provide an AMD/HYGON specific topology parser")
    Intel-SIG: commit 7211274fe0ee x86/cpu/amd: Move TOPOEXT enablement into the topology parser.
    x86/cpu: Rework the topology evaluation - part2
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/878r1j260l.ffs@tglx
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>
    Signed-off-by: Peng Zhang <zhangpeng.00@bytedance.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -738,7 +738,6 @@ clear_sev:
 
 static void early_init_amd(struct cpuinfo_x86 *c)
 {
-	u64 value;
 	u32 dummy;
 
 	early_init_amd_mc(c);
@@ -807,20 +806,6 @@ static void early_init_amd(struct cpuinfo_x86 *c)
 		msr_set_bit(MSR_AMD64_LS_CFG, 15);
 
 	early_detect_mem_encrypt(c);
-
-	/* Re-enable TopologyExtensions if switched off by BIOS */
-	if (c->x86 == 0x15 &&
-	    (c->x86_model >= 0x10 && c->x86_model <= 0x6f) &&
-	    !cpu_has(c, X86_FEATURE_TOPOEXT)) {
-
-		if (msr_set_bit(0xc0011005, 54) > 0) {
-			rdmsrl(0xc0011005, value);
-			if (value & BIT_64(54)) {
-				set_cpu_cap(c, X86_FEATURE_TOPOEXT);
-				pr_info_once(FW_INFO "CPU: Re-enabling disabled Topology Extensions Support.\n");
-			}
-		}
-	}
 
 	if (cpu_has(c, X86_FEATURE_TOPOEXT))
 		smp_num_siblings = ((cpuid_ebx(0x8000001e) >> 8) & 0xff) + 1;
