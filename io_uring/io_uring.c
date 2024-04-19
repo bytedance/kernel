70362facbe948a71e0d4568a16cdd341a53de610
Fengnan Chang <changfengnan@bytedance.com>
2024-04-19 14:58:28 +0800

    Revert "io_uring: hold uring mutex around poll removal"
    This reverts commit 0e388fce7aec40992eadee654193cad345d62663.
    
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -6114,8 +6114,6 @@ static int io_poll_update(struct io_kiocb *req, unsigned int issue_flags)
 	struct io_kiocb *preq;
 	int ret2, ret = 0;
 
-	io_ring_submit_lock(ctx, !(issue_flags & IO_URING_F_NONBLOCK));
-
 	spin_lock(&ctx->completion_lock);
 	preq = io_poll_find(ctx, req->poll_update.old_user_data, true);
 	if (!preq || !io_poll_disarm(preq)) {
@@ -6147,7 +6145,6 @@ out:
 		req_set_fail(req);
 	/* complete update request, we're done with it */
 	io_req_complete(req, ret);
-	io_ring_submit_unlock(ctx, !(issue_flags & IO_URING_F_NONBLOCK));
 	return 0;
 }
 
