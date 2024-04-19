abeb0664f7c0655ebf07ecf301a67e9c27f1f2f7
Fengnan Chang <changfengnan@bytedance.com>
2024-04-19 14:58:32 +0800

    Revert "io_uring: remove MSG_NOSIGNAL from recvmsg"
    This reverts commit 7e8cd208e9c28ce79d6725592327440c4f76b3de.
    
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -5144,7 +5144,7 @@ static int io_recvmsg_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 	sr->umsg = u64_to_user_ptr(READ_ONCE(sqe->addr));
 	sr->len = READ_ONCE(sqe->len);
 	sr->bgid = READ_ONCE(sqe->buf_group);
-	sr->msg_flags = READ_ONCE(sqe->msg_flags);
+	sr->msg_flags = READ_ONCE(sqe->msg_flags) | MSG_NOSIGNAL;
 	if (sr->msg_flags & MSG_DONTWAIT)
 		req->flags |= REQ_F_NOWAIT;
 
