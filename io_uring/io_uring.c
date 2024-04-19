8ded433858dfeac05e8a6fb4988f2635c6210eb4
Fengnan Chang <changfengnan@bytedance.com>
2024-04-19 15:00:55 +0800

    Revert "io_uring: allow re-poll if we made progress"
    This reverts commit a79b13f24967b68b49aa23eda5fa9eb14d07b3a4.
    
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -5856,7 +5856,7 @@ static int io_arm_poll_handler(struct io_kiocb *req)
 
 	if (!req->file || !file_can_poll(req->file))
 		return IO_APOLL_ABORTED;
-	if ((req->flags & (REQ_F_POLLED|REQ_F_PARTIAL_IO)) == REQ_F_POLLED)
+	if (req->flags & REQ_F_POLLED)
 		return IO_APOLL_ABORTED;
 	if (!def->pollin && !def->pollout)
 		return IO_APOLL_ABORTED;
@@ -5872,10 +5872,7 @@ static int io_arm_poll_handler(struct io_kiocb *req)
 		mask |= POLLOUT | POLLWRNORM;
 	}
 
-	if (req->flags & REQ_F_POLLED)
-		apoll = req->apoll;
-	else
-		apoll = kmalloc(sizeof(*apoll), GFP_ATOMIC);
+	apoll = kmalloc(sizeof(*apoll), GFP_ATOMIC);
 	if (unlikely(!apoll))
 		return IO_APOLL_ABORTED;
 	apoll->double_poll = NULL;
