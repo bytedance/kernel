f8a9cbc1a3d388e451ea0b3a1615aa01cd8c2be1
Fengnan Chang <changfengnan@bytedance.com>
2024-04-23 15:26:29 +0800

    Revert "io_uring: ensure that send/sendmsg and recv/recvmsg check sqe->ioprio"
    This reverts commit e5179ea01b114b76f08a4e7a83555df981ff71d2.

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -4791,8 +4791,6 @@ static int io_sendmsg_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 		return -EINVAL;
 	if (unlikely(sqe->addr2 || sqe->file_index))
 		return -EINVAL;
-	if (unlikely(sqe->addr2 || sqe->file_index || sqe->ioprio))
-		return -EINVAL;
 
 	sr->umsg = u64_to_user_ptr(READ_ONCE(sqe->addr));
 	sr->len = READ_ONCE(sqe->len);
@@ -5015,8 +5013,6 @@ static int io_recvmsg_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 	if (unlikely(req->ctx->flags & IORING_SETUP_IOPOLL))
 		return -EINVAL;
 	if (unlikely(sqe->addr2 || sqe->file_index))
-		return -EINVAL;
-	if (unlikely(sqe->addr2 || sqe->file_index || sqe->ioprio))
 		return -EINVAL;
 
 	sr->umsg = u64_to_user_ptr(READ_ONCE(sqe->addr));
