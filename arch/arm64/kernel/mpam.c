a25dea643e5d1ab743ce03607f84efd3e218a5b7
Zeng Heng <zengheng4@huawei.com>
2024-06-01 14:38:29 +0800

    downstream: arm64/mpam: skip mpam initialize under kdump kernel
    Skip MPAM initialize when running under kdump kernel. We put the judgement
    at the front of MPAM initialization to avoid meaningless initialization as
    much as possible.
    
    Fixes: 9cb26074545b ("arm_mpam: Probe MSCs to find the supported partid/pmg values")
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- arch/arm64/kernel/mpam.c
+++ arch/arm64/kernel/mpam.c
@@ -6,6 +6,7 @@
 #include <linux/arm_mpam.h>
 #include <linux/jump_label.h>
 #include <linux/percpu.h>
+#include <linux/crash_dump.h>
 
 DEFINE_STATIC_KEY_FALSE(arm64_mpam_has_hcr);
 DEFINE_STATIC_KEY_FALSE(mpam_enabled);
@@ -14,6 +15,9 @@ DEFINE_PER_CPU(u64, arm64_mpam_current);
 
 static int __init arm64_mpam_register_cpus(void)
 {
+	if (is_kdump_kernel())
+		return 0;
+
 	u64 mpamidr = read_sanitised_ftr_reg(SYS_MPAMIDR_EL1);
 	u16 partid_max = FIELD_GET(MPAMIDR_PARTID_MAX, mpamidr);
 	u8 pmg_max = FIELD_GET(MPAMIDR_PMG_MAX, mpamidr);
