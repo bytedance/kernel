b3606665171c80b6a371e5c3230d6166555b630a
Ian Rogers <irogers@google.com>
2024-06-06 23:53:43 -0700

    perf arm: Workaround ARM PMUs cpu maps having offline cpus
    commit 5518063fcb2e022411c1112b1b9b069e68380bbb upstream.
    
    When PMUs have a cpu map in the 'cpus' or 'cpumask' file, perf will
    try to open events on those CPUs. ARM doesn't remove offline CPUs
    meaning taking a CPU offline will cause perf commands to fail unless a
    CPU map is passed on the command line.
    
    More context in:
    https://lore.kernel.org/lkml/20240603092812.46616-1-yangyicong@huawei.com/
    
    Reported-by: Yicong Yang <yangyicong@huawei.com>
    Closes: https://lore.kernel.org/lkml/20240603092812.46616-2-yangyicong@huawei.com/
    Signed-off-by: Ian Rogers <irogers@google.com>
    Tested-by: Yicong Yang <yangyicong@hisilicon.com>
    Tested-by: Leo Yan <leo.yan@arm.com>
    Cc: James Clark <james.clark@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Mike Leach <mike.leach@linaro.org>
    Cc: Leo Yan <leo.yan@linux.dev>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: coresight@lists.linaro.org
    Cc: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Qizhi Zhang <zhangqizhi3@h-partners.com>
    Link: https://lore.kernel.org/r/20240607065343.695369-1-irogers@google.com
    Signed-off-by: Slim6882 <15605196882@163.com>
    Signed-off-by: chenyi <chenyi211@h-partners.com>
    Signed-off-by: slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>
    (cherry picked from commit 4ac2a5038cc53b3cee7090bd6feb7447834088a7)

--- tools/perf/arch/arm/util/pmu.c
+++ tools/perf/arch/arm/util/pmu.c
@@ -11,11 +11,14 @@
 
 #include "arm-spe.h"
 #include "hisi-ptt.h"
+#include "../../../util/cpumap.h"
 #include "../../../util/pmu.h"
 
 struct perf_event_attr
-*perf_pmu__get_default_config(struct perf_pmu *pmu __maybe_unused)
+*perf_pmu__get_default_config(struct perf_pmu *pmu)
 {
+	struct perf_cpu_map *intersect;
+
 #ifdef HAVE_AUXTRACE_SUPPORT
 	if (!strcmp(pmu->name, CORESIGHT_ETM_PMU_NAME)) {
 		/* add ETM default config here */
@@ -29,5 +32,10 @@ struct perf_event_attr
 	}
 
 #endif
+	/* Workaround some ARM PMU's failing to correctly set CPU maps for online processors. */
+	intersect = perf_cpu_map__intersect(cpu_map__online(), pmu->cpus);
+	perf_cpu_map__put(pmu->cpus);
+	pmu->cpus = intersect;
+
 	return NULL;
 }
