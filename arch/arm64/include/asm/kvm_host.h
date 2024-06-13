bf1ec4762de4a88a6fb456a085cbb7176fb6eb2b
Anshuman Khandual <anshuman.khandual@arm.com>
2024-06-13 11:47:28 +0530

    downstream: KVM: arm64: nvhe: Disable branch generation in nVHE guests
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Oliver Upton <oliver.upton@linux.dev>
    Cc: James Morse <james.morse@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: kvmarm@lists.linux.dev
    Cc: linux-arm-kernel@lists.infradead.org
    CC: linux-kernel@vger.kernel.org
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Junhao He <hejunhao3@huawei.com>
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Slim6882 <15605196882@163.com>
    Signed-off-by: chenyi <chenyi211@h-partners.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -465,6 +465,7 @@ struct kvm_host_data {
 		u64 trfcr_el1;
 		/* Values of trap registers for the host before guest entry. */
 		u64 mdcr_el2;
+		u64 brbcr_el1;
 	} host_debug_state;
 };
 
@@ -730,6 +731,8 @@ struct kvm_vcpu_arch {
 #define DEBUG_STATE_SAVE_SPE	__vcpu_single_flag(iflags, BIT(5))
 /* Save TRBE context if active  */
 #define DEBUG_STATE_SAVE_TRBE	__vcpu_single_flag(iflags, BIT(6))
+/* Save BRBE context if active  */
+#define DEBUG_STATE_SAVE_BRBE	__vcpu_single_flag(iflags, BIT(7))
 
 /* Physical CPU not in supported_cpus */
 #define ON_UNSUPPORTED_CPU	__vcpu_single_flag(sflags, BIT(2))
