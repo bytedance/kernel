ff098d0eae899a4f22ce50f1db6e3d7030e4ff26
Mateusz Guzik <mjguzik@gmail.com>
2024-06-13 10:26:37 +0200

    x86/CPU/AMD: Always inline amd_clear_divider()
    commit 501bd734f933f4eb5c080b87936e9d43f471d723 upstream.
    
    The routine is used on syscall exit and on non-AMD CPUs is guaranteed to
    be empty.
    
    It probably does not need to be a function call even on CPUs which do need the
    mitigation.
    
      [ bp: Make sure it is always inlined so that noinstr marking works. ]
    
    Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20240613082637.659133-1-mjguzik@gmail.com
    Signed-off-by: TauqirAzam <mdtauqir.azamkausar@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -1281,14 +1281,3 @@ void amd_check_microcode(void)
 	if (cpu_feature_enabled(X86_FEATURE_ZEN2))
 		on_each_cpu(zenbleed_check_cpu, NULL, 1);
 }
-
-/*
- * Issue a DIV 0/1 insn to clear any division data from previous DIV
- * operations.
- */
-void noinstr amd_clear_divider(void)
-{
-	asm volatile(ALTERNATIVE("", "div %2\n\t", X86_BUG_DIV0)
-		     :: "a" (0), "d" (0), "r" (1));
-}
-EXPORT_SYMBOL_GPL(amd_clear_divider);
