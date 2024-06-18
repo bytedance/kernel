16e8bf75455b272b812581f4aad85d2ba88d783d
Tejun Heo <tj@kernel.org>
2024-06-18 10:09:20 -1000

    sched_ext: Implement sched_ext_ops.cpu_online/offline()
    commit 60c27fb59f6cffa73fc8c60e3a22323c78044576 upstream.
    
    Add ops.cpu_online/offline() which are invoked when CPUs come online and
    offline respectively. As the enqueue path already automatically bypasses
    tasks to the local dsq on a deactivated CPU, BPF schedulers are guaranteed
    to see tasks only on CPUs which are between online() and offline().
    
    If the BPF scheduler doesn't implement ops.cpu_online/offline(), the
    scheduler is automatically exited with SCX_ECODE_RESTART |
    SCX_ECODE_RSN_HOTPLUG. Userspace can implement CPU hotpplug support
    trivially by simply reinitializing and reloading the scheduler.
    
    scx_qmap is updated to print out online CPUs on hotplug events. Other
    schedulers are updated to restart based on ecode.
    
    v3: - The previous implementation added @reason to
          sched_class.rq_on/offline() to distinguish between CPU hotplug events
          and topology updates. This was buggy and fragile as the methods are
          skipped if the current state equals the target state. Instead, add
          scx_rq_[de]activate() which are directly called from
          sched_cpu_de/activate(). This also allows ops.cpu_on/offline() to
          sleep which can be useful.
    
        - ops.dispatch() could be called on a CPU that the BPF scheduler was
          told to be offline. The dispatch patch is updated to bypass in such
          cases.
    
    v2: - To accommodate lock ordering change between scx_cgroup_rwsem and
          cpus_read_lock(), CPU hotplug operations are put into its own SCX_OPI
          block and enabled eariler during scx_ope_enable() so that
          cpus_read_lock() can be dropped before acquiring scx_cgroup_rwsem.
    
        - Auto exit with ECODE added.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Acked-by: Josh Don <joshdon@google.com>
    Acked-by: Hao Luo <haoluo@google.com>
    Acked-by: Barret Rhoden <brho@google.com>
    (cherry picked from commit 60c27fb59f6cffa73fc8c60e3a22323c78044576)
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9801,6 +9801,8 @@ int sched_cpu_activate(unsigned int cpu)
 		cpuset_cpu_active();
 	}
 
+	scx_rq_activate(rq);
+
 	/*
 	 * Put the rq online, if not already. This happens:
 	 *
@@ -9849,6 +9851,8 @@ int sched_cpu_deactivate(unsigned int cpu)
 	synchronize_rcu();
 
 	sched_set_rq_offline(rq, cpu);
+
+	scx_rq_deactivate(rq);
 
 	/*
 	 * When going down, decrement the number of cores with SMT present.
