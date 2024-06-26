6c4dcb34eebc71c74f8fe979593952d68f2f430f
Kan Liang <kan.liang@linux.intel.com>
2024-06-26 07:35:37 -0700

    perf/x86/intel: Support new data source for Lunar Lake
    commit 608f6976c309793ceea37292c54b057dab091944 upstream.
    
    A new PEBS data source format is introduced for the p-core of Lunar
    Lake. The data source field is extended to 8 bits with new encodings.
    
    A new layout is introduced into the union intel_x86_pebs_dse.
    Introduce the lnl_latency_data() to parse the new format.
    Enlarge the pebs_data_source[] accordingly to include new encodings.
    
    Only the mem load and the mem store events can generate the data source.
    Introduce INTEL_HYBRID_LDLAT_CONSTRAINT and
    INTEL_HYBRID_STLAT_CONSTRAINT to mark them.
    
    Add two new bits for the new cache-related data src, L2_MHB and MSC.
    The L2_MHB is short for L2 Miss Handling Buffer, which is similar to
    LFB (Line Fill Buffer), but to track the L2 Cache misses.
    The MSC stands for the memory-side cache.
    
    Intel-SIG: commit 608f6976c309 perf/x86/intel: Support new data source for Lunar Lake
    Backport CWF PMU support and dependency
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Ian Rogers <irogers@google.com>
    Link: https://lkml.kernel.org/r/20240626143545.480761-6-kan.liang@linux.intel.com
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Signed-off-by: Liangyan <liangyan.peng@bytedance.com>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -7100,6 +7100,7 @@ __init int intel_pmu_init(void)
 	case INTEL_FAM6_ARROWLAKE:
 		intel_pmu_init_hybrid(hybrid_big_small);
 
+		x86_pmu.pebs_latency_data = lnl_latency_data;
 		x86_pmu.get_event_constraints = mtl_get_event_constraints;
 		x86_pmu.hw_config = adl_hw_config;
 
@@ -7117,6 +7118,7 @@ __init int intel_pmu_init(void)
 		pmu = &x86_pmu.hybrid_pmu[X86_HYBRID_PMU_ATOM_IDX];
 		intel_pmu_init_skt(&pmu->pmu);
 
+		intel_pmu_pebs_data_source_lnl();
 		pr_cont("Lunarlake Hybrid events, ");
 		name = "lunarlake_hybrid";
 		break;
