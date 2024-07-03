be503fe1d60f8ef51a247012cf525ca6e69b446c
Alison Schofield <alison.schofield@intel.com>
2024-07-02 22:29:49 -0700

    cxl/core: Fold cxl_trace_hpa() into cxl_dpa_to_hpa()
    commit 9aa5f6235e16ac6fceed789a30e72addf1abd7d8 upstream.
    
    Although cxl_trace_hpa() is used to populate TRACE EVENTs with HPA
    addresses the work it performs is a DPA to HPA translation not a
    trace. Tidy up this naming by moving the minimal work done in
    cxl_trace_hpa() into cxl_dpa_to_hpa() and use cxl_dpa_to_hpa()
    for trace event callbacks.
    
    Suggested-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Alison Schofield <alison.schofield@intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Reviewed-by: Robert Richter <rrichter@amd.com>
    Link: https://patch.msgid.link/452a9b0c525b774c72d9d5851515ffa928750132.1719980933.git.alison.schofield@intel.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Signed-off-by: Bo Li <libo.gcs85@bytedance.com>

--- drivers/cxl/core/trace.h
+++ drivers/cxl/core/trace.h
@@ -715,8 +715,8 @@ TRACE_EVENT(cxl_poison,
 		if (cxlr) {
 			__assign_str(region, dev_name(&cxlr->dev));
 			memcpy(__entry->uuid, &cxlr->params.uuid, 16);
-			__entry->hpa = cxl_trace_hpa(cxlr, cxlmd,
-						     __entry->dpa);
+			__entry->hpa = cxl_dpa_to_hpa(cxlr, cxlmd,
+						      __entry->dpa);
 		} else {
 			__assign_str(region, "");
 			memset(__entry->uuid, 0, 16);
