7878e2e4c71a70da22287854569d7cbee78e3400
Lizhe <sensor1010@163.com>
2024-07-04 12:23:55 +0530

    cpufreq: Make cpufreq_driver->exit() return void
    commit b4b1ddc9dfe997a5f492fa3a36487f8e7a5de30d upstream.
    
    The cpufreq core doesn't check the return type of the exit() callback
    and there is not much the core can do on failures at that point. Just
    drop the returned value and make it return void.
    
    Signed-off-by: Lizhe <sensor1010@163.com>
    [ Viresh: Reworked the patches to fix all missing changes together. ]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com> # Mediatek
    Acked-by: Sudeep Holla <sudeep.holla@arm.com> # scpi, scmi, vexpress
    Acked-by: Mario Limonciello <mario.limonciello@amd.com> # amd
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com> # bmips
    Acked-by: Rafael J. Wysocki <rafael@kernel.org>
    Acked-by: Kevin Hilman <khilman@baylibre.com> # omap
    Signed-off-by: Rahul Kumar <Kumar.Rahul2@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/scpi-cpufreq.c
+++ drivers/cpufreq/scpi-cpufreq.c
@@ -175,7 +175,7 @@ out_free_opp:
 	return ret;
 }
 
-static int scpi_cpufreq_exit(struct cpufreq_policy *policy)
+static void scpi_cpufreq_exit(struct cpufreq_policy *policy)
 {
 	struct scpi_data *priv = policy->driver_data;
 
@@ -183,8 +183,6 @@ static int scpi_cpufreq_exit(struct cpufreq_policy *policy)
 	dev_pm_opp_free_cpufreq_table(priv->cpu_dev, &policy->freq_table);
 	dev_pm_opp_remove_all_dynamic(priv->cpu_dev);
 	kfree(priv);
-
-	return 0;
 }
 
 static struct cpufreq_driver scpi_cpufreq_driver = {
