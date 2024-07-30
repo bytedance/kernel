daf55a83898f91103b061fe62742433da82b3ff6
Ben Gainey <ben.gainey@arm.com>
2024-07-30 09:44:14 +0100

    perf: Rename perf_event_context.nr_pending to nr_no_switch_fast.
    commit 79bd233010859463e46a0a4b3926eaaba25a6110 upstream.
    
    nr_pending counts the number of events in the context that
    either pending_sigtrap or pending_work, but it is used
    to prevent taking the fast path in perf_event_context_sched_out.
    
    Renamed to reflect what it is used for, rather than what it
    counts. This change allows using the field to track other
    event properties that also require skipping the fast path
    without possible confusion over the name.
    
    Signed-off-by: Ben Gainey <ben.gainey@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20240730084417.7693-2-ben.gainey@arm.com
    Signed-off-by: Songtang Liu <liusongtang@bytedance.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -923,12 +923,13 @@ struct perf_event_context {
 	struct rcu_head			rcu_head;
 
 	/*
-	 * Sum (event->pending_work + event->pending_work)
+	 * The count of events for which using the switch-out fast path
+	 * should be avoided.
 	 *
 	 * The SIGTRAP is targeted at ctx->task, as such it won't do changing
 	 * that until the signal is delivered.
 	 */
-	local_t				nr_pending;
+	local_t				nr_no_switch_fast;
 };
 
 /*
