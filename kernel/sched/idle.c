ad2cc60dabc37bb4840fb7608c5a620afd02f457
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:53 +0200

    sched: Rework pick_next_task()
    commit fd03c5b8585562d60f8b597b4332d28f48abfe7d upstream.
    
    The current rule is that:
    
      pick_next_task() := pick_task() + set_next_task(.first = true)
    
    And many classes implement it directly as such. Change things around
    to make pick_next_task() optional while also changing the definition to:
    
      pick_next_task(prev) := pick_task() + put_prev_task() + set_next_task(.first = true)
    
    The reason is that sched_ext would like to have a 'final' call that
    knows the next task. By placing put_prev_task() right next to
    set_next_task() (as it already is for sched_core) this becomes
    trivial.
    
    As a bonus, this is a nice cleanup on its own.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224016.051225657@infradead.org
    (cherry picked from commit fd03c5b8585562d60f8b597b4332d28f48abfe7d)
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -418,20 +418,9 @@ static void set_next_task_idle(struct rq *rq, struct task_struct *next, bool fir
 	schedstat_inc(rq->sched_goidle);
 }
 
-#ifdef CONFIG_SMP
-static struct task_struct *pick_task_idle(struct rq *rq)
+struct task_struct *pick_task_idle(struct rq *rq)
 {
 	return rq->idle;
-}
-#endif
-
-struct task_struct *pick_next_task_idle(struct rq *rq)
-{
-	struct task_struct *next = rq->idle;
-
-	set_next_task_idle(rq, next, true);
-
-	return next;
 }
 
 /*
@@ -486,13 +475,12 @@ DEFINE_SCHED_CLASS(idle) = {
 
 	.wakeup_preempt		= wakeup_preempt_idle,
 
-	.pick_next_task		= pick_next_task_idle,
+	.pick_task		= pick_task_idle,
 	.put_prev_task		= put_prev_task_idle,
 	.set_next_task          = set_next_task_idle,
 
 #ifdef CONFIG_SMP
 	.balance		= balance_idle,
-	.pick_task		= pick_task_idle,
 	.select_task_rq		= select_task_rq_idle,
 	.set_cpus_allowed	= set_cpus_allowed_common,
 #endif
