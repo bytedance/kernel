d8f11a519df6cca1be9b292747f88b1ec8e46120
Dan Carpenter <dan.carpenter@linaro.org>
2024-08-13 15:21:13 +0530

    cpufreq: amd-pstate: Fix uninitialized variable in amd_pstate_cpu_boost_update()
    commit 67d95303c84732c2e1de5730756281f648dbefaf upstream.
    
    Smatch complains that "ret" could be uninitialized:
    
      drivers/cpufreq/amd-pstate.c:734 amd_pstate_cpu_boost_update()
      error: uninitialized symbol 'ret'.
    
    This seems like it probably is a real issue.  Initialize "ret" to zero to
    be safe.
    
    Fixes: c8c68c38b56f ("cpufreq: amd-pstate: initialize core precision boost state")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>
    Reviewed-by: Perry Yuan <perry.yuan@amd.com>
    Acked-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Link: https://lore.kernel.org/lkml/7ff53543-6c04-48a0-8d99-7dc010b93b3a@stanley.mountain/T/
    Signed-off-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: TauqirAzam <mdtauqir.azamkausar@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -320,7 +320,7 @@ static inline int pstate_enable(bool enable)
 		return 0;
 
 	for_each_present_cpu(cpu) {
-		unsigned long logical_id = topology_logical_die_id(cpu);
+		unsigned long logical_id = topology_logical_package_id(cpu);
 
 		if (test_bit(logical_id, &logical_proc_id_mask))
 			continue;
@@ -696,7 +696,7 @@ static int amd_pstate_cpu_boost_update(struct cpufreq_policy *policy, bool on)
 	struct amd_cpudata *cpudata = policy->driver_data;
 	struct cppc_perf_ctrls perf_ctrls;
 	u32 highest_perf, nominal_perf, nominal_freq, max_freq;
-	int ret;
+	int ret = 0;
 
 	highest_perf = READ_ONCE(cpudata->highest_perf);
 	nominal_perf = READ_ONCE(cpudata->nominal_perf);
