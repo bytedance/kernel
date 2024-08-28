105bc05ebd834800840a4aa9802ea1160c65e0ff
Damien Le Moal <dlemoal@kernel.org>
2024-08-28 11:07:43 +0900

    ata: libata: Move sector_buf from struct ata_port to struct ata_device
    commit da65bbdd3bc1e8d2193e01167a413d90d9988c04 upstream.
    
    Fix Conflict.
    
    The 512B buffer sector_buf field of struct ata_port is used for scanning
    devices as well as during error recovery with ata EH. This buffer is
    thus useless if a port does not have a device connected to it.
    And also given that commands using this buffer are issued to devices,
    and not to ports, move this buffer definition from struct ata_port to
    struct ata_device.
    
    This change slightly increases system memory usage for systems using a
    port-multiplier as in that case we do not need a per-device buffer for
    scanning devices (PMP does not allow parallel scanning) nor for EH (as
    when entering EH we are guaranteed that all commands to all devices
    connected to the PMP have completed or have been aborted). However,
    this change reduces memory usage on systems that have many ports with
    only few devices rives connected, which is a much more common use case
    than the PMP use case.
    
    Suggested-by: Niklas Cassel <cassel@kernel.org>
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Reviewed-by: Niklas Cassel <cassel@kernel.org>
    Signed-off-by: Diangang Li <lidiangang@bytedance.com>

--- include/linux/libata.h
+++ include/linux/libata.h
@@ -757,6 +757,9 @@ struct ata_device {
 	int			spdn_cnt;
 	/* ering is CLEAR_END, read comment above CLEAR_END */
 	struct ata_ering	ering;
+
+	/* For EH */
+	u8			sector_buf[ATA_SECT_SIZE] ____cacheline_aligned;
 };
 
 /* Fields between ATA_DEVICE_CLEAR_BEGIN and ATA_DEVICE_CLEAR_END are
@@ -908,7 +911,6 @@ struct ata_port {
 #endif
 	/* owned by EH */
 	u8			*ncq_sense_buf;
-	u8			sector_buf[ATA_SECT_SIZE] ____cacheline_aligned;
 };
 
 /* The following initializer overrides a method to NULL whether one of
