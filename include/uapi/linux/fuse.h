77659c186508698fac0c8fe274289298f7d1aafa
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2024-09-03 17:16:25 +0200

    fuse: allow idmapped mounts
    commit 16e1503eaf329129170e4e7a078aee17686967a5 upstream.
    
    Now we have everything in place and we can allow idmapped mounts
    by setting the FS_ALLOW_IDMAP flag. Notice that real availability
    of idmapped mounts will depend on the fuse daemon. Fuse daemon
    have to set FUSE_ALLOW_IDMAP flag in the FUSE_INIT reply.
    
    To discuss:
    - we enable idmapped mounts support only if "default_permissions" mode is
    enabled, because otherwise we would need to deal with UID/GID mappings in
    the userspace side OR provide the userspace with idmapped
    req->in.h.uid/req->in.h.gid values which is not something that we probably
    want to. Idmapped mounts philosophy is not about faking caller uid/gid.
    
    Some extra links and examples:
    
    - libfuse support
    https://github.com/mihalicyn/libfuse/commits/idmap_support
    
    - fuse-overlayfs support:
    https://github.com/mihalicyn/fuse-overlayfs/commits/idmap_support
    
    - cephfs-fuse conversion example
    https://github.com/mihalicyn/ceph/commits/fuse_idmap
    
    - glusterfs conversion example
    https://github.com/mihalicyn/glusterfs/commits/fuse_idmap
    
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Xin Yin <yinxin.x@bytedance.com>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -412,6 +412,7 @@ struct fuse_file_lock {
  * FUSE_DIRECT_IO_ALLOW_MMAP: allow shared mmap in FOPEN_DIRECT_IO mode.
  * FUSE_HAS_RESEND: kernel supports resending pending requests, and the high bit
  *		    of the request ID indicates resend requests
+ * FUSE_ALLOW_IDMAP: allow creation of idmapped mounts
  */
 #define FUSE_ASYNC_READ		(1 << 0)
 #define FUSE_POSIX_LOCKS	(1 << 1)
@@ -456,6 +457,7 @@ struct fuse_file_lock {
 
 /* Obsolete alias for FUSE_DIRECT_IO_ALLOW_MMAP */
 #define FUSE_DIRECT_IO_RELAX	FUSE_DIRECT_IO_ALLOW_MMAP
+#define FUSE_ALLOW_IDMAP	(1ULL << 40)
 
 /**
  * CUSE INIT request/reply flags
@@ -973,6 +975,19 @@ struct fuse_fallocate_in {
  */
 #define FUSE_UNIQUE_RESEND (1ULL << 63)
 
+/**
+ * This value will be set by the kernel to
+ * (struct fuse_in_header).{uid,gid} fields in
+ * case when:
+ * - fuse daemon enabled FUSE_ALLOW_IDMAP
+ * - idmapping information is not available and uid/gid
+ *   can not be mapped in accordance with an idmapping.
+ *
+ * Note: an idmapping information always available
+ * for inode creation operations like:
+ * FUSE_MKNOD, FUSE_SYMLINK, FUSE_MKDIR, FUSE_TMPFILE,
+ * FUSE_CREATE and FUSE_RENAME2 (with RENAME_WHITEOUT).
+ */
 #define FUSE_INVALID_UIDGID ((uint32_t)(-1))
 
 struct fuse_in_header {
