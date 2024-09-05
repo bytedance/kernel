44a83fcc2303d719e6cdc32fc74a0afdf9094df1
Dave Jiang <dave.jiang@intel.com>
2024-09-05 15:35:45 -0700

    cxl: move cxl headers to new include/cxl/ directory
    commit 40a895fd9a358eea16901026360bd2cd3ca691a7 upstream.
    
    Group all cxl related kernel headers into include/cxl/ directory.
    
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Link: https://patch.msgid.link/20240905223711.1990186-2-dave.jiang@intel.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Signed-off-by: Bo Li <libo.gcs85@bytedance.com>

--- drivers/cxl/cxlmem.h
+++ drivers/cxl/cxlmem.h
@@ -7,7 +7,7 @@
 #include <linux/uuid.h>
 #include <linux/rcuwait.h>
 #include <linux/node.h>
-#include <linux/cxl-event.h>
+#include <cxl/event.h>
 #include "cxl.h"
 
 /* CXL 2.0 8.2.8.5.1.1 Memory Device Status Register */
