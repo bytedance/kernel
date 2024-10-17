2ebedec8c52d3139c84a7104d3038333238c5d5c
Dhananjay Ugwekar <Dhananjay.Ugwekar@amd.com>
2024-10-17 10:05:33 +0000

    cpufreq/amd-pstate: Remove the redundant amd_pstate_set_driver() call
    commit b427ac4084753d7c62ef797bed7f30203f0ef5fb upstream.
    
    amd_pstate_set_driver() is called twice, once in amd_pstate_init() and once
    as part of amd_pstate_register_driver(). Move around code and eliminate
    the redundancy.
    
    Signed-off-by: Dhananjay Ugwekar <Dhananjay.Ugwekar@amd.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Link: https://lore.kernel.org/r/20241017100528.300143-5-Dhananjay.Ugwekar@amd.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Mukesh Ogare <OgreKumar.Mukesh@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -1840,9 +1840,11 @@ static int __init amd_pstate_init(void)
 		return -ENODEV;
 	}
 
-	ret = amd_pstate_set_driver(cppc_state);
-	if (ret)
+	ret = amd_pstate_register_driver(cppc_state);
+	if (ret) {
+		pr_err("failed to register with return %d\n", ret);
 		return ret;
+	}
 
 	/* capability check */
 	if (cpu_feature_enabled(X86_FEATURE_CPPC)) {
@@ -1860,12 +1862,6 @@ static int __init amd_pstate_init(void)
 		ret = amd_detect_prefcore(&amd_pstate_prefcore);
 		if (ret)
 			return ret;
-	}
-
-	ret = amd_pstate_register_driver(cppc_state);
-	if (ret) {
-		pr_err("failed to register with return %d\n", ret);
-		return ret;
 	}
 
 	dev_root = bus_get_dev_root(&cpu_subsys);
