9f706a2e9068c366c5fd9e378e6174f413c27e0d
Benjamin Segall <bsegall@google.com>
2024-10-25 18:35:35 -0700

    posix-cpu-timers: Clear TICK_DEP_BIT_POSIX_TIMER on clone
    commit b5413156bad91dc2995a5c4eab1b05e56914638a upstream.
    
    When cloning a new thread, its posix_cputimers are not inherited, and
    are cleared by posix_cputimers_init(). However, this does not clear the
    tick dependency it creates in tsk->tick_dep_mask, and the handler does
    not reach the code to clear the dependency if there were no timers to
    begin with.
    
    Thus if a thread has a cputimer running before clone/fork, all
    descendants will prevent nohz_full unless they create a cputimer of
    their own.
    
    Fix this by entirely clearing the tick_dep_mask in copy_process().
    (There is currently no inherited state that needs a tick dependency)
    
    Process-wide timers do not have this problem because fork does not copy
    signal_struct as a baseline, it creates one from scratch.
    
    Fixes: b78783000d5c ("posix-cpu-timers: Migrate to use new tick dependency mask model")
    Signed-off-by: Ben Segall <bsegall@google.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/all/xm26o737bq8o.fsf@google.com
    
    [ ziqianlu: adjust #include <linux/tick.h> position ]
    Signed-off-by: Ziqian Lu <ziqianlu@bytedance.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -98,6 +98,7 @@
 #include <linux/bpf.h>
 #include <linux/sched/mm.h>
 #include <linux/async_fork.h>
+#include <linux/tick.h>
 
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2138,6 +2139,7 @@ static __latent_entropy struct task_struct *copy_process(
 	acct_clear_integrals(p);
 
 	posix_cputimers_init(&p->posix_cputimers);
+	tick_dep_init_task(p);
 
 	p->io_context = NULL;
 	audit_set_context(p, NULL);
