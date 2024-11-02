7d1647f7fff3d028742a95d78e34225764f6c76d
Yicong Yang <yangyicong@hisilicon.com>
2024-11-02 18:42:33 +0800

    arm64: Add support for FEAT_HAFT
    commit efe72541355d4d40a4f076af453f6533e98e058c upstream.
    
    Armv8.9/v9.4 introduces the feature Hardware managed Access Flag
    for Table descriptors (FEAT_HAFT). The feature is indicated by
    ID_AA64MMFR1_EL1.HAFDBS == 0b0011 and can be enabled by
    TCR2_EL1.HAFT so it has a dependency on FEAT_TCR2.
    
    Adds the Kconfig for FEAT_HAFT and support detecting and enabling
    the feature. The feature is enabled in __cpu_setup() before MMU on
    just like HA. A CPU capability is added to notify the user of the
    feature.
    
    Add definition of P{G,4,U,M}D_TABLE_AF bit and set the AF bit
    when creating the page table, which will save the hardware
    from having to update them at runtime. This will be ignored if
    FEAT_HAFT is not enabled.
    
    The AF bit of table descriptors cannot be managed by the software
    per spec, unlike the HA. So this should be used only if it's supported
    system wide by system_supports_haft().
    
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Link: https://lore.kernel.org/r/20241102104235.62560-4-yangyicong@huawei.com
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    [catalin.marinas@arm.com: added the ID check back to __cpu_setup in case of future CPU errata]
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Conflicts:
    	arch/arm64/Kconfig
    	arch/arm64/include/asm/cpufeature.h
    	arch/arm64/kernel/cpufeature.c
    	arch/arm64/mm/mmu.c
    	arch/arm64/tools/cpucaps
    [Context conflicts]
    Signed-off-by: JiangShui Yang <yangjiangshui@h-partners.com>
    Signed-off-by: Slim6882 <15605196882@163.com>
    Signed-off-by: chenyi <chenyi211@h-partners.com>
    Signed-off-by: slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -204,7 +204,7 @@ static void alloc_init_cont_pte(pmd_t *pmdp, unsigned long addr,
 
 	BUG_ON(pmd_sect(pmd));
 	if (pmd_none(pmd)) {
-		pmdval_t pmdval = PMD_TYPE_TABLE | PMD_TABLE_UXN;
+		pmdval_t pmdval = PMD_TYPE_TABLE | PMD_TABLE_UXN | PMD_TABLE_AF;
 		phys_addr_t pte_phys;
 
 		if (flags & NO_EXEC_MAPPINGS)
@@ -282,7 +282,7 @@ static void alloc_init_cont_pmd(pud_t *pudp, unsigned long addr,
 	 */
 	BUG_ON(pud_sect(pud));
 	if (pud_none(pud)) {
-		pudval_t pudval = PUD_TYPE_TABLE | PUD_TABLE_UXN;
+		pudval_t pudval = PUD_TYPE_TABLE | PUD_TABLE_UXN | PUD_TABLE_AF;
 		phys_addr_t pmd_phys;
 
 		if (flags & NO_EXEC_MAPPINGS)
@@ -321,7 +321,7 @@ static void alloc_init_pud(pgd_t *pgdp, unsigned long addr, unsigned long end,
 	p4d_t p4d = READ_ONCE(*p4dp);
 
 	if (p4d_none(p4d)) {
-		p4dval_t p4dval = P4D_TYPE_TABLE | P4D_TABLE_UXN;
+		p4dval_t p4dval = P4D_TYPE_TABLE | P4D_TABLE_UXN | P4D_TABLE_AF;
 		phys_addr_t pud_phys;
 
 		if (flags & NO_EXEC_MAPPINGS)
