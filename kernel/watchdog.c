a5a91cc426bb27b513f0a83fe8b54c40211f23e5
Tejun Heo <tj@kernel.org>
2024-11-05 11:49:04 -1000

    sched_ext: Enable the ops breather and eject BPF scheduler on softlockup
    commit e32c260195e6ff72940ab7826e38e0a0066fc58f upstream.
    
    On 2 x Intel Sapphire Rapids machines with 224 logical CPUs, a poorly
    behaving BPF scheduler can live-lock the system by making multiple CPUs bang
    on the same DSQ to the point where soft-lockup detection triggers before
    SCX's own watchdog can take action. It also seems possible that the machine
    can be live-locked enough to prevent scx_ops_helper, which is an RT task,
    from running in a timely manner.
    
    Implement scx_softlockup() which is called when three quarters of
    soft-lockup threshold has passed. The function immediately enables the ops
    breather and triggers an ops error to initiate ejection of the BPF
    scheduler.
    
    The previous and this patch combined enable the kernel to reliably recover
    the system from live-lock conditions that can be triggered by a poorly
    behaving BPF scheduler on Intel dual socket systems.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Douglas Anderson <dianders@chromium.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit e32c260195e6ff72940ab7826e38e0a0066fc58f)
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -415,6 +415,14 @@ static int is_softlockup(unsigned long touch_ts,
 			 unsigned long now)
 {
 	if ((watchdog_enabled & WATCHDOG_SOFTOCKUP_ENABLED) && watchdog_thresh) {
+		/*
+		 * A poorly behaving BPF scheduler can live-lock the system into
+		 * soft lockups. Tell sched_ext to try ejecting the BPF
+		 * scheduler when close to a soft lockup.
+		 */
+		if (time_after_eq(now, period_ts + get_softlockup_thresh() * 3 / 4))
+			scx_softlockup(now - touch_ts);
+
 		/* Warn about unreasonable delays. */
 		if (time_after(now, period_ts + get_softlockup_thresh()))
 			return now - touch_ts;
