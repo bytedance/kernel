1ecc2f103036755a69f88de1f1f9100988cee958
Mario Limonciello <mario.limonciello@amd.com>
2024-12-11 14:57:57 -0600

    cpufreq/amd-pstate: Show a warning when a CPU fails to setup
    commit a9ba0fd452d82ca0da170eb6291aac01075a17d5 upstream.
    
    I came across a system that MSR_AMD_CPPC_CAP1 for some CPUs isn't
    populated.  This is an unexpected behavior that is most likely a
    BIOS bug. In the event it happens I'd like users to report bugs
    to properly root cause and get this fixed.
    
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Reviewed-by: Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Mukesh Ogare <OgreKumar.Mukesh@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -1024,6 +1024,7 @@ static int amd_pstate_cpu_init(struct cpufreq_policy *policy)
 free_cpudata2:
 	freq_qos_remove_request(&cpudata->req[0]);
 free_cpudata1:
+	pr_warn("Failed to initialize CPU %d: %d\n", policy->cpu, ret);
 	kfree(cpudata);
 	return ret;
 }
@@ -1517,6 +1518,7 @@ static int amd_pstate_epp_cpu_init(struct cpufreq_policy *policy)
 	return 0;
 
 free_cpudata1:
+	pr_warn("Failed to initialize CPU %d: %d\n", policy->cpu, ret);
 	kfree(cpudata);
 	return ret;
 }
