20f90db5bcb3b29f1c824f65b0d128503487462d
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2024-12-12 22:00:57 +0800

    x86/mce: Make several functions return bool
    commit c845cb8dbd2e1a804babfd13648026c3a7cfbc0b upstream.
    
    Make several functions that return 0 or 1 return a boolean value for
    better readability.
    
    No functional changes are intended.
    
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Nikolay Borisov <nik.borisov@suse.com>
    Reviewed-by: Sohil Mehta <sohil.mehta@intel.com>
    Reviewed-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Link: https://lore.kernel.org/r/20241212140103.66964-2-qiuxu.zhuo@intel.com
    Signed-off-by: Rahul Kumar <Kumar.Rahul2@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>
    Signed-off-by: Bo Li <libo.gcs85@bytedance.com>

--- arch/x86/kernel/cpu/mce/intel.c
+++ arch/x86/kernel/cpu/mce/intel.c
@@ -56,12 +56,12 @@ static DEFINE_SPINLOCK(cmci_poll_lock);
 
 #define CMCI_THRESHOLD		1
 
-static int cmci_supported(int *banks)
+static bool cmci_supported(int *banks)
 {
 	u64 cap;
 
 	if (mca_cfg.cmci_disabled || mca_cfg.ignore_ce)
-		return 0;
+		return false;
 
 	/*
 	 * Vendor check is not strictly needed, but the initial
@@ -70,10 +70,11 @@ static int cmci_supported(int *banks)
 	 */
 	if (boot_cpu_data.x86_vendor != X86_VENDOR_INTEL &&
 	    boot_cpu_data.x86_vendor != X86_VENDOR_ZHAOXIN)
-		return 0;
+		return false;
 
 	if (!boot_cpu_has(X86_FEATURE_APIC) || lapic_get_maxlvt() < 6)
-		return 0;
+		return false;
+
 	rdmsrl(MSR_IA32_MCG_CAP, cap);
 	*banks = min_t(unsigned, MAX_NR_BANKS, cap & 0xff);
 	return !!(cap & MCG_CMCI_P);
