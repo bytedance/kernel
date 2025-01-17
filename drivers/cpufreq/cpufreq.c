854e223ab3d5aaa91977ee68ebc78a1a2317bba7
Lifeng Zheng <zhenglifeng1@huawei.com>
2025-01-17 18:14:55 +0800

    cpufreq: Introduce a more generic way to set default per-policy boost flag
    commit dd016f379ebc2d43a9405742d1a6066577509bd7 upstream.
    
    In cpufreq_online() of cpufreq.c, the per-policy boost flag is already
    set to mirror the cpufreq_driver boost during init but using freq_table
    to judge if the policy has boost frequency. There are two drawbacks to
    this approach:
    
     1. It doesn't work for the cpufreq drivers that do not use a frequency
        table. For now, acpi-cpufreq and amd-pstate have to enable boost in
        policy initialization. And cppc_cpufreq never set policy to boost
        when going online no matter what the cpufreq_driver boost flag is.
    
     2. If the CPU goes offline when cpufreq_driver boost is enabled and
        then goes online when cpufreq_driver boost is disabled, the
        per-policy boost flag will incorrectly remain true.
    
    Running set_boost at the end of the online process is a more generic way
    for all cpufreq drivers.
    
    Signed-off-by: Lifeng Zheng <zhenglifeng1@huawei.com>
    Link: https://patch.msgid.link/20250117101457.1530653-3-zhenglifeng1@huawei.com
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    [ rjw: Changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Xinghai Cen <cenxinghai@h-partners.com>
    Signed-off-by: Qi Xi <xiqi2@huawei.com>
    Signed-off-by: slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1414,10 +1414,6 @@ static int cpufreq_online(unsigned int cpu)
 			goto out_free_policy;
 		}
 
-		/* Let the per-policy boost flag mirror the cpufreq_driver boost during init */
-		if (cpufreq_boost_enabled() && policy_has_boost_freq(policy))
-			policy->boost_enabled = true;
-
 		/*
 		 * The initialization has succeeded and the policy is online.
 		 * If there is a problem with its frequency table, take it
@@ -1569,6 +1565,18 @@ static int cpufreq_online(unsigned int cpu)
 
 	if (cpufreq_thermal_control_enabled(cpufreq_driver))
 		policy->cdev = of_cpufreq_cooling_register(policy);
+
+	/* Let the per-policy boost flag mirror the cpufreq_driver boost during init */
+	if (policy->boost_enabled != cpufreq_boost_enabled()) {
+		policy->boost_enabled = cpufreq_boost_enabled();
+		ret = cpufreq_driver->set_boost(policy, policy->boost_enabled);
+		if (ret) {
+			/* If the set_boost fails, the online operation is not affected */
+			pr_info("%s: CPU%d: Cannot %s BOOST\n", __func__, policy->cpu,
+				policy->boost_enabled ? "enable" : "disable");
+			policy->boost_enabled = !policy->boost_enabled;
+		}
+	}
 
 	pr_debug("initialization complete\n");
 
