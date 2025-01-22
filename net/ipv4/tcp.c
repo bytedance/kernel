30ed9ca6ea12a263dfa1820764daf3fd54d4c265
Jiayuan Chen <mrpre@163.com>
2025-01-22 18:09:14 +0800

    bpf: Fix wrong copied_seq calculation
    commit 36b62df5683c315ba58c950f1a9c771c796c30ec upstream.
    
    'sk->copied_seq' was updated in the tcp_eat_skb() function when the action
    of a BPF program was SK_REDIRECT. For other actions, like SK_PASS, the
    update logic for 'sk->copied_seq' was moved to tcp_bpf_recvmsg_parser()
    to ensure the accuracy of the 'fionread' feature.
    
    It works for a single stream_verdict scenario, as it also modified
    sk_data_ready->sk_psock_verdict_data_ready->tcp_read_skb
    to remove updating 'sk->copied_seq'.
    
    However, for programs where both stream_parser and stream_verdict are
    active (strparser purpose), tcp_read_sock() was used instead of
    tcp_read_skb() (sk_data_ready->strp_data_ready->tcp_read_sock).
    tcp_read_sock() now still updates 'sk->copied_seq', leading to duplicate
    updates.
    
    In summary, for strparser + SK_PASS, copied_seq is redundantly calculated
    in both tcp_read_sock() and tcp_bpf_recvmsg_parser().
    
    The issue causes incorrect copied_seq calculations, which prevent
    correct data reads from the recv() interface in user-land.
    
    We do not want to add new proto_ops to implement a new version of
    tcp_read_sock, as this would introduce code complexity [1].
    
    We could have added noack and copied_seq to desc, and then called
    ops->read_sock. However, unfortunately, other modules didn’t fully
    initialize desc to zero. So, for now, we are directly calling
    tcp_read_sock_noack() in tcp_bpf.c.
    
    [1]: https://lore.kernel.org/bpf/20241218053408.437295-1-mrpre@163.com
    
    Fixes: e5c6de5fa025 ("bpf, sockmap: Incorrectly handling copied_seq")
    Suggested-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Jiayuan Chen <mrpre@163.com>
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Reviewed-by: Jakub Sitnicki <jakub@cloudflare.com>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://patch.msgid.link/20250122100917.49845-3-mrpre@163.com
    Signed-off-by: Feng Zhou <zhoufeng.zf@bytedance.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -1726,12 +1726,13 @@ static struct sk_buff *tcp_recv_skb(struct sock *sk, u32 seq, u32 *off)
  *	  or for 'peeking' the socket using this routine
  *	  (although both would be easy to implement).
  */
-int tcp_read_sock(struct sock *sk, read_descriptor_t *desc,
-		  sk_read_actor_t recv_actor)
+static int __tcp_read_sock(struct sock *sk, read_descriptor_t *desc,
+			   sk_read_actor_t recv_actor, bool noack,
+			   u32 *copied_seq)
 {
 	struct sk_buff *skb;
 	struct tcp_sock *tp = tcp_sk(sk);
-	u32 seq = tp->copied_seq;
+	u32 seq = *copied_seq;
 	u32 offset;
 	int copied = 0;
 
@@ -1785,9 +1786,12 @@ int tcp_read_sock(struct sock *sk, read_descriptor_t *desc,
 		sk_eat_skb(sk, skb);
 		if (!desc->count)
 			break;
-		WRITE_ONCE(tp->copied_seq, seq);
+		WRITE_ONCE(*copied_seq, seq);
 	}
-	WRITE_ONCE(tp->copied_seq, seq);
+	WRITE_ONCE(*copied_seq, seq);
+
+	if (noack)
+		goto out;
 
 	tcp_rcv_space_adjust(sk);
 
@@ -1796,9 +1800,24 @@ int tcp_read_sock(struct sock *sk, read_descriptor_t *desc,
 		tcp_recv_skb(sk, seq, &offset);
 		tcp_cleanup_rbuf(sk, copied);
 	}
+out:
 	return copied;
 }
+
+int tcp_read_sock(struct sock *sk, read_descriptor_t *desc,
+		  sk_read_actor_t recv_actor)
+{
+	return __tcp_read_sock(sk, desc, recv_actor, false,
+			       &tcp_sk(sk)->copied_seq);
+}
 EXPORT_SYMBOL(tcp_read_sock);
+
+int tcp_read_sock_noack(struct sock *sk, read_descriptor_t *desc,
+			sk_read_actor_t recv_actor, bool noack,
+			u32 *copied_seq)
+{
+	return __tcp_read_sock(sk, desc, recv_actor, noack, copied_seq);
+}
 
 int tcp_read_skb(struct sock *sk, skb_read_actor_t recv_actor)
 {
