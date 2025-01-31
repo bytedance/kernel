4f7ef3b69fe9c2401a14caccc3450b3ff43d6f9f
Changwoo Min <changwoo@igalia.com>
2025-01-31 16:09:30 +0900

    sched_ext: Add an event, SCX_EV_DISPATCH_LOCAL_DSQ_OFFLINE
    commit 9be0a1b0c8fbd21ef6d7f9428a76b759f9db0de3 upstream.
    
    Add a core event, SCX_EV_DISPATCH_LOCAL_DSQ_OFFLINE, which represents how
    many times a BPF scheduler tries to dispatch to an offlined local DSQ.
    
    __scx_add_event() is used since the caller holds an rq lock,
    so the preemption has already been disabled.
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit 9be0a1b0c8fbd21ef6d7f9428a76b759f9db0de3)
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -1467,6 +1467,12 @@ struct scx_event_stats {
 	 * the core scheduler code silently picks a fallback CPU.
 	 */
 	u64		SCX_EV_SELECT_CPU_FALLBACK;
+
+	/*
+	 * When dispatching to a local DSQ, the CPU may have gone offline in
+	 * the meantime. In this case, the task is bounced to the global DSQ.
+	 */
+	u64		SCX_EV_DISPATCH_LOCAL_DSQ_OFFLINE;
 };
 
 /*
@@ -2660,6 +2666,7 @@ static void dispatch_to_local_dsq(struct rq *rq, struct scx_dispatch_q *dst_dsq,
 	if (unlikely(!task_can_run_on_remote_rq(p, dst_rq, true))) {
 		dispatch_enqueue(find_global_dsq(p), p,
 				 enq_flags | SCX_ENQ_CLEAR_OPSS);
+		__scx_add_event(SCX_EV_DISPATCH_LOCAL_DSQ_OFFLINE, 1);
 		return;
 	}
 
@@ -5437,6 +5444,7 @@ static void scx_dump_state(struct scx_exit_info *ei, size_t dump_len)
 
 	scx_bpf_events(&events, sizeof(events));
 	scx_dump_event(s, &events, SCX_EV_SELECT_CPU_FALLBACK);
+	scx_dump_event(s, &events, SCX_EV_DISPATCH_LOCAL_DSQ_OFFLINE);
 
 	if (seq_buf_has_overflowed(&s) && dump_len >= sizeof(trunc_marker))
 		memcpy(ei->dump + dump_len - sizeof(trunc_marker),
@@ -7687,6 +7695,7 @@ __bpf_kfunc void scx_bpf_events(struct scx_event_stats *events,
 	for_each_possible_cpu(cpu) {
 		e_cpu = per_cpu_ptr(&event_stats_cpu, cpu);
 		scx_agg_event(&e_sys, e_cpu, SCX_EV_SELECT_CPU_FALLBACK);
+		scx_agg_event(&e_sys, e_cpu, SCX_EV_DISPATCH_LOCAL_DSQ_OFFLINE);
 	}
 
 	/*
