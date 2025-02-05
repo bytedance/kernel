3d06f43451a658f88e6945bcc059c24940f3f87a
Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
2025-02-05 11:25:16 +0000

    cpufreq/amd-pstate: Pass min/max_limit_perf as min/max_perf to amd_pstate_update
    commit e9869c836b2a460c48e2d69ae79d786303dbffda upstream.
    
    Currently, amd_pstate_update_freq passes the hardware perf limits as
    min/max_perf to amd_pstate_update, which eventually gets programmed into
    the min/max_perf fields of the CPPC_REQ register.
    
    Instead pass the effective perf limits i.e. min/max_limit_perf values to
    amd_pstate_update as min/max_perf.
    
    Signed-off-by: Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Link: https://lore.kernel.org/r/20250205112523.201101-6-dhananjay.ugwekar@amd.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Mukesh Ogare <OgreKumar.Mukesh@amd.com>
    Signed-off-by: mohanasv2 <mohanasv@amd.com>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -616,7 +616,7 @@ static int amd_pstate_update_freq(struct cpufreq_policy *policy,
 {
 	struct cpufreq_freqs freqs;
 	struct amd_cpudata *cpudata = policy->driver_data;
-	unsigned long max_perf, min_perf, des_perf, cap_perf;
+	unsigned long des_perf, cap_perf;
 
 	if (!cpudata->max_freq)
 		return -ENODEV;
@@ -625,8 +625,6 @@ static int amd_pstate_update_freq(struct cpufreq_policy *policy,
 		amd_pstate_update_min_max_limit(policy);
 
 	cap_perf = READ_ONCE(cpudata->highest_perf);
-	min_perf = READ_ONCE(cpudata->lowest_perf);
-	max_perf = cap_perf;
 
 	freqs.old = policy->cur;
 	freqs.new = target_freq;
@@ -643,8 +641,9 @@ static int amd_pstate_update_freq(struct cpufreq_policy *policy,
 	if (!fast_switch)
 		cpufreq_freq_transition_begin(policy, &freqs);
 
-	amd_pstate_update(cpudata, min_perf, des_perf,
-			max_perf, fast_switch, policy->governor->flags);
+	amd_pstate_update(cpudata, cpudata->min_limit_perf, des_perf,
+			  cpudata->max_limit_perf, fast_switch,
+			  policy->governor->flags);
 
 	if (!fast_switch)
 		cpufreq_freq_transition_end(policy, &freqs, false);
