0a5652a55c51f58c37b5f233ee498e55ebeb005d
Zhongkun He <hezhongkun.hzk@bytedance.com>
2025-02-12 16:32:59 +0800

    bytedance: memcg: use ratelimited stats flush in the reclaim
    The test process is seeing large amount of stalls in memcg
    stats flush from the memcg reclaim code path and kswapd.
    
    I have noticed system stalls lasting at least three minutes,
    during which only kswapd is running. So According to the
    analysis results, the cause of the problem is this func
    mem_cgroup_flush_stats() in shrink_node, which takes a long
    time to complete and disables interrupts.
    
    So I see the upstream code, found the same question, please
    see[1]. Unfortunately, there are so many changes in cgroup rstat
    flush. To simplify the solution, we replaced
    mem_cgroup_flush_stats_ratelimited() with
    mem_cgroup_flush_stats_delayed() which have the same effect on
    flush, delay the action.
    
    [1]:https://lore.kernel.org/all/20240813215358.2259750-1-shakeel.butt@linux.dev/T/#u
    
    Signed-off-by: Zhongkun He <hezhongkun.hzk@bytedance.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2349,10 +2349,11 @@ static void shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 
 again:
 	/*
-	 * Flush the memory cgroup stats, so that we read accurate per-memcg
-	 * lruvec stats for heuristics.
+	 * Flush the memory cgroup stats in rate-limited way as we don't need
+	 * most accurate stats here. We may switch to regular stats flushing
+	 * in the future once it is cheap enough.
 	 */
-	mem_cgroup_flush_stats();
+	mem_cgroup_flush_stats_delayed();
 
 	memset(&sc->nr, 0, sizeof(sc->nr));
 
