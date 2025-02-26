2ecc558208e5bf297513b06e5dff02b575ae7326
Chang S. Bae <chang.seok.bae@intel.com>
2025-02-25 17:07:21 -0800

    selftests/x86: Consolidate redundant signal helper functions
    commit dbd6b649e7d5b66c7fa95a65d67b59cf5b45f0ac upstream.
    
    The x86 selftests frequently register and clean up signal handlers, but
    the sethandler() and clearhandler() functions have been redundantly
    copied across multiple .c files.
    
    Move these functions to helpers.h to enable reuse across tests,
    eliminating around 250 lines of duplicate code.
    
    Converge the error handling by using ksft_exit_fail_msg(), which is
    functionally equivalent with err() within the selftest framework.
    
    This change is a prerequisite for the upcoming xstate selftest, which
    requires signal handling for registering and cleaning up handlers.
    
    Intel-SIG: commit dbd6b649e7d5 selftests/x86: Consolidate redundant signal helper functions.
    DMR ISA APX enabling - HOST
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20250226010731.2456-2-chang.seok.bae@intel.com
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- tools/testing/selftests/x86/sysret_rip.c
+++ tools/testing/selftests/x86/sysret_rip.c
@@ -21,7 +21,7 @@
 #include <sys/user.h>
 #include <sys/mman.h>
 #include <assert.h>
-
+#include "helpers.h"
 
 asm (
 	".pushsection \".text\", \"ax\"\n\t"
@@ -38,28 +38,6 @@ asm (
 
 extern const char test_page[];
 static void const *current_test_page_addr = test_page;
-
-static void sethandler(int sig, void (*handler)(int, siginfo_t *, void *),
-		       int flags)
-{
-	struct sigaction sa;
-	memset(&sa, 0, sizeof(sa));
-	sa.sa_sigaction = handler;
-	sa.sa_flags = SA_SIGINFO | flags;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(sig, &sa, 0))
-		err(1, "sigaction");
-}
-
-static void clearhandler(int sig)
-{
-	struct sigaction sa;
-	memset(&sa, 0, sizeof(sa));
-	sa.sa_handler = SIG_DFL;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(sig, &sa, 0))
-		err(1, "sigaction");
-}
 
 /* State used by our signal handlers. */
 static gregset_t initial_regs;
--- tools/testing/selftests/x86/test_vsyscall.c
+++ tools/testing/selftests/x86/test_vsyscall.c
@@ -39,18 +39,6 @@
 /* max length of lines in /proc/self/maps - anything longer is skipped here */
 #define MAPS_LINE_LEN 128
 
-static void sethandler(int sig, void (*handler)(int, siginfo_t *, void *),
-		       int flags)
-{
-	struct sigaction sa;
-	memset(&sa, 0, sizeof(sa));
-	sa.sa_sigaction = handler;
-	sa.sa_flags = SA_SIGINFO | flags;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(sig, &sa, 0))
-		err(1, "sigaction");
-}
-
 /* vsyscalls and vDSO */
 bool vsyscall_map_r = false, vsyscall_map_x = false;
 
