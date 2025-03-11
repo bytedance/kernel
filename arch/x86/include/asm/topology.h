095d54a0363e696d15cde0eb5d2091b21881cce3
Yicong Yang <yangyicong@hisilicon.com>
2025-03-11 15:51:40 +0800

    cpu/SMT: Provide a default topology_is_primary_thread()
    commit 4b455f59945aab5610828a1320b045c82cbe8852 upstream.
    
    Currently if architectures want to support HOTPLUG_SMT they need to
    provide a topology_is_primary_thread() telling the framework which
    thread in the SMT cannot offline. However arm64 doesn't have a
    restriction on which thread in the SMT cannot offline, a simplest
    choice is that just make 1st thread as the "primary" thread. So
    just make this as the default implementation in the framework and
    let architectures like x86 that have special primary thread to
    override this function (which they've already done).
    
    There's no need to provide a stub function if !CONFIG_SMP or
    !CONFIG_HOTPLUG_SMT. In such case the testing CPU is already
    the 1st CPU in the SMT so it's always the primary thread.
    
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Pierre Gondois <pierre.gondois@arm.com>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Yicong Yang <yangyicong@hisilicon.com>
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>
    Signed-off-by: Qi Xi <xiqi2@huawei.com>
    Signed-off-by: slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>
    (cherry picked from commit e5d8ea27dd5a085dc8b5a44aa98380533174dfea)

--- arch/x86/include/asm/topology.h
+++ arch/x86/include/asm/topology.h
@@ -154,7 +154,6 @@ static inline int topology_phys_to_logical_die(unsigned int die,
 		unsigned int cpu) { return 0; }
 static inline int topology_max_die_per_package(void) { return 1; }
 static inline int topology_max_smt_threads(void) { return 1; }
-static inline bool topology_is_primary_thread(unsigned int cpu) { return true; }
 static inline bool topology_smt_supported(void) { return false; }
 #endif
 
