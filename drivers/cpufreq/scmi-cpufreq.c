d9f85be8c125d3ffdd2248a8df49741757c563f2
Jacky Bai <ping.bai@nxp.com>
2025-03-13 09:39:28 +0800

    cpufreq: Init cpufreq only for present CPUs
    commit 45f589b7167f36290d29c79e3a442dc0b13c086a upstream.
    
    for_each_possible_cpu() is currently used to initialize cpufreq.
    However, in cpu_dev_register_generic(), for_each_present_cpu()
    is used to register CPU devices which means the CPU devices are
    only registered for present CPUs and not all possible CPUs.
    
    With nosmp or maxcpus=0, only the boot CPU is present, lead
    to the cpufreq probe failure or defer probe due to no cpu device
    available for not present CPUs.
    
    Change for_each_possible_cpu() to for_each_present_cpu() in the
    above cpufreq drivers to ensure it only registers cpufreq for
    CPUs that are actually present.
    
    Fixes: b0c69e1214bc ("drivers: base: Use present CPUs in GENERIC_CPU_DEVICES")
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: wangyi <wangyi395@huawei.com>
    Signed-off-by: slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/scmi-cpufreq.c
+++ drivers/cpufreq/scmi-cpufreq.c
@@ -87,7 +87,7 @@ scmi_get_sharing_cpus(struct device *cpu_dev, struct cpumask *cpumask)
 	if (domain < 0)
 		return domain;
 
-	for_each_possible_cpu(cpu) {
+	for_each_present_cpu(cpu) {
 		if (cpu == cpu_dev->id)
 			continue;
 
