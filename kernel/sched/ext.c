d648c28995a522235518c51dd3740e995d0e3c62
Andrea Righi <arighi@nvidia.com>
2025-04-30 22:21:56 +0200

    sched_ext: Add RCU protection to scx_root in DSQ iterator
    commit c01adf4097113f0732a0c975de686232829dcb72 upstream.
    
    Using a DSQ iterators from a timer callback can trigger the following
    lockdep splat when accessing scx_root:
    
     =============================
     WARNING: suspicious RCU usage
     6.14.0-virtme #1 Not tainted
     -----------------------------
     kernel/sched/ext.c:6907 suspicious rcu_dereference_check() usage!
    
     other info that might help us debug this:
    
     rcu_scheduler_active = 2, debug_locks = 1
     no locks held by swapper/0/0.
    
     stack backtrace:
     CPU: 0 UID: 0 PID: 0 Comm: swapper/0 Not tainted 6.14.0-virtme #1 PREEMPT(full)
     Sched_ext: tickless (enabled+all)
     Call Trace:
     <IRQ>
     dump_stack_lvl+0x6f/0xb0
     lockdep_rcu_suspicious.cold+0x4e/0xa3
     bpf_iter_scx_dsq_new+0xb1/0xd0
     bpf_prog_63f4fd1bccc101e7_dispatch_cpu+0x3e/0x156
     bpf_prog_8320d4217989178c_dispatch_all_cpus+0x153/0x1b6
     bpf_prog_97f847d871513f95_sched_timerfn+0x4c/0x264
     ? hrtimer_run_softirq+0x4f/0xd0
     bpf_timer_cb+0x7a/0x140
     __hrtimer_run_queues+0x1f9/0x3a0
     hrtimer_run_softirq+0x8c/0xd0
     handle_softirqs+0xd3/0x3d0
     __irq_exit_rcu+0x9a/0xc0
     irq_exit_rcu+0xe/0x20
     sysvec_apic_timer_interrupt+0x73/0x80
    
    Add a proper dereference check to explicitly validate RCU-safe access to
    scx_root from rcu_read_lock() contexts and also from contexts that hold
    rcu_read_lock_bh(), such as timer callbacks.
    
    Fixes: cdf5a6faa8cf0 ("sched_ext: Move dsq_hash into scx_sched")
    Signed-off-by: Andrea Righi <arighi@nvidia.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit c01adf4097113f0732a0c975de686232829dcb72)
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -6825,7 +6825,7 @@ __bpf_kfunc int bpf_iter_scx_dsq_new(struct bpf_iter_scx_dsq *it, u64 dsq_id,
 	 */
 	kit->dsq = NULL;
 
-	sch = rcu_dereference(scx_root);
+	sch = rcu_dereference_check(scx_root, rcu_read_lock_bh_held());
 	if (!sch)
 		return -ENODEV;
 
