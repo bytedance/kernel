61287ab445810883e68d4eed180886d6eaad2c36
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2025-05-22 20:08:10 +0800

    downstream: arm64: Create a list of SYM_CODE functions, check return PC against list
    backport from https://lore.kernel.org/all/
    20220617210717.27126-6-madvenka@linux.microsoft.com/
    
    SYM_CODE functions don't follow the usual calling conventions. Check if the
    return PC in a stack frame falls in any of these. If it does, consider the
    stack trace unreliable.
    
    Define a special section for unreliable functions
    =================================================
    
    Define a SYM_CODE_END() macro for arm64 that adds the function address
    range to a new section called "sym_code_functions".
    
    Linker file
    ===========
    
    Include the "sym_code_functions" section under read-only data in
    vmlinux.lds.S.
    
    Initialization
    ==============
    
    Define an early_initcall() to create a sym_code_functions[] array from
    the linker data.
    
    Unwinder check
    ==============
    
    Add a reliability check in unwind_check_reliability() that compares a
    return PC with sym_code_functions[]. If there is a match, then return
    failure.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
    Signed-off-by: Rui Qi <qirui.001@bytedance.com>

--- arch/arm64/include/asm/sections.h
+++ arch/arm64/include/asm/sections.h
@@ -22,6 +22,7 @@ extern char __irqentry_text_start[], __irqentry_text_end[];
 extern char __mmuoff_data_start[], __mmuoff_data_end[];
 extern char __entry_tramp_text_start[], __entry_tramp_text_end[];
 extern char __relocate_new_kernel_start[], __relocate_new_kernel_end[];
+extern char __sym_code_functions_start[], __sym_code_functions_end[];
 
 static inline size_t entry_tramp_text_size(void)
 {
--- arch/arm64/kernel/stacktrace.c
+++ arch/arm64/kernel/stacktrace.c
@@ -20,6 +20,11 @@
 
 extern void *kthread_return_to_user;
 
+struct code_range {
+	unsigned long start;
+	unsigned long end;
+};
+
 /*
  * Start an unwind from a pt_regs.
  *
@@ -212,7 +217,37 @@ noinline noinstr void arch_stack_walk(stack_trace_consume_fn consume_entry,
 
 static inline bool unwind_state_is_reliable(struct unwind_state *state)
 {
-	return __kernel_text_address(ptrauth_strip_kernel_insn_pac(state->pc));
+	struct code_range *r;
+	unsigned long pc;
+
+	pc = ptrauth_strip_kernel_insn_pac(state->pc);
+
+	if (!__kernel_text_address(pc))
+		return false;
+	/*
+	 * Check the return PC against sym_code_functions[]. If there is a
+	 * match, then the consider the stack frame unreliable.
+	 *
+	 * As SYM_CODE functions don't follow the usual calling conventions,
+	 * we assume by default that any SYM_CODE function cannot be unwound
+	 * reliably.
+	 *
+	 * Note that this includes:
+	 *
+	 * - Exception handlers and entry assembly
+	 * - Trampoline assembly (e.g., ftrace, kprobes)
+	 * - Hypervisor-related assembly
+	 * - Hibernation-related assembly
+	 * - CPU start-stop, suspend-resume assembly
+	 * - Kernel relocation assembly
+	 */
+	for (r = (struct code_range *)__sym_code_functions_start;
+		r < (struct code_range *)__sym_code_functions_end; r++) {
+		if (pc >= r->start && pc < r->end)
+			return false;
+	}
+
+	return true;
 }
 
 static __always_inline int
