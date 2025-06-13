a91d65d15d33c4f8d7113c195b39f76b91697dd2
Jie Zhan <zhanjie9@hisilicon.com>
2025-06-13 15:27:50 +0800

    downstream: cpufreq: CPPC: Fix error handling in cppc_scale_freq_workfn()
    Perf counters could be 0 if the cpu is in a low-power idle state.  Just try
    it again next time and update the frequency scale when the cpu is active
    and perf counters successfully return.
    
    Also, remove the FIE source on an actual failure.
    
    Fixes: 1eb5dde674f5 ("cpufreq: CPPC: Add support for frequency invariance")
    Signed-off-by: Jie Zhan <zhanjie9@hisilicon.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>
    Signed-off-by: chenyi <chenyi211@h-partners.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/cppc_cpufreq.c
+++ drivers/cpufreq/cppc_cpufreq.c
@@ -99,12 +99,23 @@ static void cppc_scale_freq_workfn(struct kthread_work *work)
 	struct cppc_cpudata *cpu_data;
 	unsigned long local_freq_scale;
 	u64 perf;
+	int ret;
 
 	cppc_fi = container_of(work, struct cppc_freq_invariance, work);
 	cpu_data = cppc_fi->cpu_data;
 
-	if (cppc_get_perf_ctrs(cppc_fi->cpu, &fb_ctrs)) {
+	ret = cppc_get_perf_ctrs(cppc_fi->cpu, &fb_ctrs);
+	/*
+	 * Perf counters could be 0 if the cpu is in a low-power idle state.
+	 * Just try it again next time.
+	 */
+	if (ret == -EFAULT)
+		return;
+
+	if (ret) {
 		pr_warn("%s: failed to read perf counters\n", __func__);
+		topology_clear_scale_freq_source(SCALE_FREQ_SOURCE_CPPC,
+						 cpu_data->shared_cpu_map);
 		return;
 	}
 
