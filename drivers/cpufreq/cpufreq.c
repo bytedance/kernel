da4fa97e919bb2532702089a8fb734847a23a1e0
Lifeng Zheng <zhenglifeng1@huawei.com>
2025-06-23 21:33:59 +0800

    cpufreq: Remove duplicate check in __cpufreq_offline()
    commit 5d6ecaaa922611ec3ca067723ccefafb543010ee upstream.
    
    The has_target() checks in __cpufreq_offline() are duplicate.
    
    Remove one of them and put the operations of exiting governor together
    with storing last governor's name.
    
    Signed-off-by: Lifeng Zheng <zhenglifeng1@huawei.com>
    Link: https://patch.msgid.link/20250623133402.3120230-5-zhenglifeng1@huawei.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>
    Signed-off-by: heppen <hepeng68@huawei.com>
    Signed-off-by: slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1683,14 +1683,13 @@ static void __cpufreq_offline(unsigned int cpu, struct cpufreq_policy *policy)
 		return;
 	}
 
-	if (has_target())
+	if (has_target()) {
 		strncpy(policy->last_governor, policy->governor->name,
 			CPUFREQ_NAME_LEN);
-	else
-		policy->last_policy = policy->policy;
-
-	if (has_target())
 		cpufreq_exit_governor(policy);
+	} else {
+		policy->last_policy = policy->policy;
+	}
 
 	/*
 	 * Perform the ->offline() during light-weight tear-down, as
