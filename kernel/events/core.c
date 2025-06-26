ed4fcceaf9a9d9acb5aa10420cdc8a6fa70f144f
Luo Gengkun <luogengkun@huaweicloud.com>
2025-06-26 13:54:03 +0000

    perf/core: Fix the WARN_ON_ONCE is out of lock protected region
    commit 7b4c5a37544ba22c6ebe72c0d4ea56c953459fa5 upstream.
    
    commit 3172fb986666 ("perf/core: Fix WARN in perf_cgroup_switch()") try to
    fix a concurrency problem between perf_cgroup_switch and
    perf_cgroup_event_disable. But it does not to move the WARN_ON_ONCE into
    lock-protected region, so the warning is still be triggered.
    
    Fixes: 3172fb986666 ("perf/core: Fix WARN in perf_cgroup_switch()")
    Signed-off-by: Luo Gengkun <luogengkun@huaweicloud.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20250626135403.2454105-1-luogengkun@huaweicloud.com
    Signed-off-by: Liangyan <liangyan.peng@bytedance.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -856,8 +856,6 @@ static void perf_cgroup_switch(struct task_struct *task)
 	if (READ_ONCE(cpuctx->cgrp) == NULL)
 		return;
 
-	WARN_ON_ONCE(cpuctx->ctx.nr_cgroups == 0);
-
 	cgrp = perf_cgroup_from_task(task, NULL);
 	if (READ_ONCE(cpuctx->cgrp) == cgrp)
 		return;
@@ -868,6 +866,8 @@ static void perf_cgroup_switch(struct task_struct *task)
 	 */
 	if (READ_ONCE(cpuctx->cgrp) == NULL)
 		return;
+
+	WARN_ON_ONCE(cpuctx->ctx.nr_cgroups == 0);
 
 	perf_ctx_disable(&cpuctx->ctx, true);
 
