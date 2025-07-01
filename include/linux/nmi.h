34b50a605a57775683889c46cc39641c1cbd4b4e
Yicong Yang <yangyicong@hisilicon.com>
2025-07-01 19:02:13 +0800

    watchdog/perf: Provide function for adjusting the event period
    commit 60bc47b5a0b164082d448815d7db3066266aa3ed upstream.
    
    Architecture's using perf events for hard lockup detection needs to
    convert the watchdog_thresh to the event's period, some architecture
    for example arm64 perform this conversion using the CPU's maximum
    frequency which will be acquired by cpufreq. However by the time
    the lockup detector's initialized the cpufreq driver may not be
    initialized, thus launch a watchdog with inaccurate period. Provide
    a function hardlockup_detector_perf_adjust_period() to allowing
    adjust the event period. Then architecture can update with more
    accurate period if cpufreq is initialized.
    
    Fixes: 94946f9eaac1 ("arm64: add hw_nmi_get_sample_period for preparation of lockup detector")
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>
    Signed-off-by: Qi Xi <xiqi2@huawei.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- include/linux/nmi.h
+++ include/linux/nmi.h
@@ -105,10 +105,12 @@ void watchdog_hardlockup_check(unsigned int cpu, struct pt_regs *regs);
 extern void hardlockup_detector_perf_stop(void);
 extern void hardlockup_detector_perf_restart(void);
 extern void hardlockup_detector_perf_cleanup(void);
+extern void hardlockup_detector_perf_adjust_period(int cpu, u64 period);
 #else
 static inline void hardlockup_detector_perf_stop(void) { }
 static inline void hardlockup_detector_perf_restart(void) { }
 static inline void hardlockup_detector_perf_cleanup(void) { }
+static inline void hardlockup_detector_perf_adjust_period(int cpu, u64 period) { }
 #endif
 
 void watchdog_hardlockup_stop(void);
