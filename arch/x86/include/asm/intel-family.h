f3c0510129095d680bdc90f613370c6c61185acd
Tony Luck <tony.luck@intel.com>
2025-07-30 08:04:37 -0700

    x86/cpu: Add new Intel CPU model numbers for Wildcatlake and Novalake
    commit 49f848788a4d157bb6648a57963cb060fed3d56e upstream.
    
    Wildcatlake is a mobile CPU. Novalake has both desktop and mobile
    versions.
    
      [ bp: Merge into a single patch. ]
    
    Intel-SIG: commit 49f848788a4d x86/cpu: Add new Intel CPU model numbers for Wildcatlake and Novalake
    Sync turbostat to version 2025.12.02
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/20250730150437.4701-1-tony.luck@intel.com
    [ Zhang Rui: amend commit log ]
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -199,6 +199,11 @@
 
 #define INTEL_PANTHERLAKE_L		IFM(6, 0xCC)
 
+#define INTEL_WILDCATLAKE_L		IFM(6, 0xD5)
+
+#define INTEL_NOVALAKE			IFM(18, 0x01)
+#define INTEL_NOVALAKE_L		IFM(18, 0x03)
+
 /* "Small Core" Processors (Atom/E-Core) */
 
 #define INTEL_FAM6_ATOM_BONNELL		0x1C /* Diamondville, Pineview */
