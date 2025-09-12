8214dd9eaa832b14f1b96f8e96aef34ce726d421
Aaron Lu <ziqianlu@bytedance.com>
2025-09-12 11:44:28 +0800

    sched/fair: Do not balance task to a throttled cfs_rq
    commit 0d4eaf8caf8cd633b23e949e2996b420052c2d45 upstream.
    
    When doing load balance and the target cfs_rq is in throttled hierarchy,
    whether to allow balancing there is a question.
    
    The good side to allow balancing is: if the target CPU is idle or less
    loaded and the being balanced task is holding some kernel resources,
    then it seems a good idea to balance the task there and let the task get
    the CPU earlier and release kernel resources sooner. The bad part is, if
    the task is not holding any kernel resources, then the balance seems not
    that useful.
    
    While theoretically it's debatable, a performance test[0] which involves
    200 cgroups and each cgroup runs hackbench(20 sender, 20 receiver) in
    pipe mode showed a performance degradation on AMD Genoa when allowing
    load balance to throttled cfs_rq. Analysis[1] showed hackbench doesn't
    like task migration across LLC boundary. For this reason, add a check in
    can_migrate_task() to forbid balancing to a cfs_rq that is in throttled
    hierarchy. This reduced task migration a lot and performance restored.
    
    [0]: https://lore.kernel.org/lkml/20250822110701.GB289@bytedance/
    [1]: https://lore.kernel.org/lkml/20250903101102.GB42@bytedance/
    
    Signed-off-by: Aaron Lu <ziqianlu@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: K Prateek Nayak <kprateek.nayak@amd.com>
    
    Conflicts:
    	kernel/sched/fair.c
    Signed-off-by: Ziqian Lu <ziqianlu@bytedance.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4973,6 +4973,11 @@ static inline int throttled_hierarchy(struct cfs_rq *cfs_rq)
 	return cfs_bandwidth_used() && cfs_rq->throttle_count;
 }
 
+static inline int lb_throttled_hierarchy(struct task_struct *p, int dst_cpu)
+{
+	return throttled_hierarchy(task_group(p)->cfs_rq[dst_cpu]);
+}
+
 static inline bool task_is_throttled(struct task_struct *p)
 {
 	return cfs_bandwidth_used() && p->throttled;
@@ -5962,6 +5967,11 @@ static inline int throttled_hierarchy(struct cfs_rq *cfs_rq)
 }
 
 void init_cfs_bandwidth(struct cfs_bandwidth *cfs_b) {}
+
+static inline int lb_throttled_hierarchy(struct task_struct *p, int dst_cpu)
+{
+	return 0;
+}
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 static void init_cfs_rq_runtime(struct cfs_rq *cfs_rq) {}
@@ -8321,10 +8331,14 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 
 	/*
 	 * We do not migrate tasks that are:
-	 * 1) cannot be migrated to this CPU due to cpus_ptr, or
-	 * 2) running (obviously), or
-	 * 3) are cache-hot on their current CPU.
+	 * 1) target cfs_rq is in throttled hierarchy, or
+	 * 2) cannot be migrated to this CPU due to cpus_ptr, or
+	 * 3) running (obviously), or
+	 * 4) are cache-hot on their current CPU.
 	 */
+	if (lb_throttled_hierarchy(p, env->dst_cpu))
+		return 0;
+
 	/* Disregard pcpu kthreads; they are where they need to be. */
 	if (kthread_is_per_cpu(p))
 		return 0;
