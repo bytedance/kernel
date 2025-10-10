88e859363ae88db08d37a353123ad34d6994d486
Raju Rangoju <Raju.Rangoju@amd.com>
2025-10-10 12:21:42 +0530

    amd-xgbe: Avoid spurious link down messages during interface toggle
    [ Upstream commit 2616222e423398bb374ffcb5d23dea4ba2c3e524 ]
    
    During interface toggle operations (ifdown/ifup), the driver currently
    resets the local helper variable 'phy_link' to -1. This causes the link
    state machine to incorrectly interpret the state as a link change event,
    resulting in spurious "Link is down" messages being logged when the
    interface is brought back up.
    
    Preserve the phy_link state across interface toggles to avoid treating
    the -1 sentinel value as a legitimate link state transition.
    
    Fixes: 88131a812b16 ("amd-xgbe: Perform phy connect/disconnect at dev open/stop")
    Signed-off-by: Raju Rangoju <Raju.Rangoju@amd.com>
    Reviewed-by: Dawid Osuchowski <dawid.osuchowski@linux.intel.com>
    Link: https://patch.msgid.link/20251010065142.1189310-1-Raju.Rangoju@amd.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/amd/xgbe/xgbe-mdio.c
+++ drivers/net/ethernet/amd/xgbe/xgbe-mdio.c
@@ -1660,6 +1660,7 @@ static int xgbe_phy_init(struct xgbe_prv_data *pdata)
 		pdata->phy.duplex = DUPLEX_FULL;
 	}
 
+	pdata->phy_link = 0;
 	pdata->phy.link = 0;
 
 	pdata->phy.pause_autoneg = pdata->pause_autoneg;
