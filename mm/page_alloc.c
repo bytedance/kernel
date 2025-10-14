7c40ac9f7261fd44cbdf06d06f02f93c2bcbc551
Joshua Hahn <joshua.hahnjy@gmail.com>
2025-10-14 07:50:10 -0700

    mm/page_alloc: batch page freeing in free_frozen_page_commit
    commit 91e691296646e68d4116b8303aa11d6de0376aef upstream.
    
    Before returning, free_frozen_page_commit calls free_pcppages_bulk using
    nr_pcp_free to determine how many pages can appropritately be freed, based
    on the tunable parameters stored in pcp.  While this number is an accurate
    representation of how many pages should be freed in total, it is not an
    appropriate number of pages to free at once using free_pcppages_bulk,
    since we have seen the value consistently go above 2000 in the Meta fleet
    on larger machines.
    
    As such, perform batched page freeing in free_pcppages_bulk by using
    pcp->batch.  In order to ensure that other processes are not starved of
    the zone lock, free both the zone lock and pcp lock to yield to other
    threads.
    
    Note that because free_frozen_page_commit now performs a spinlock inside
    the function (and can fail), the function may now return with a freed pcp.
    To handle this, return true if the pcp is locked on exit and false
    otherwise.
    
    In addition, since free_frozen_page_commit must now be aware of what UP
    flags were stored at the time of the spin lock, and because we must be
    able to report new UP flags to the callers, add a new unsigned long*
    parameter UP_flags to keep track of this.
    
    The following are a few synthetic benchmarks, made on three machines.  The
    first is a large machine with 754GiB memory and 316 processors.  The
    second is a relatively smaller machine with 251GiB memory and 176
    processors.  The third and final is the smallest of the three, which has
    62GiB memory and 36 processors.
    
    On all machines, I kick off a kernel build with -j$(nproc).  Negative
    delta is better (faster compilation)
    
    Large machine (754GiB memory, 316 processors)
    make -j$(nproc)
    +------------+---------------+-----------+
    | Metric (s) | Variation (%) | Delta(%)  |
    +------------+---------------+-----------+
    | real       |        0.8070 |  - 1.4865 |
    | user       |        0.2823 |  + 0.4081 |
    | sys        |        5.0267 |  -11.8737 |
    +------------+---------------+-----------+
    
    Medium machine (251GiB memory, 176 processors)
    make -j$(nproc)
    +------------+---------------+----------+
    | Metric (s) | Variation (%) | Delta(%) |
    +------------+---------------+----------+
    | real       |        0.2806 |  +0.0351 |
    | user       |        0.0994 |  +0.3170 |
    | sys        |        0.6229 |  -0.6277 |
    +------------+---------------+----------+
    
    Small machine (62GiB memory, 36 processors)
    make -j$(nproc)
    +------------+---------------+----------+
    | Metric (s) | Variation (%) | Delta(%) |
    +------------+---------------+----------+
    | real       |        0.1503 |  -2.6585 |
    | user       |        0.0431 |  -2.2984 |
    | sys        |        0.1870 |  -3.2013 |
    +------------+---------------+----------+
    
    Here, variation is the coefficient of variation, i.e.  standard deviation
    / mean.
    
    [joshua.hahnjy@gmail.com: simplify checks]
      Link: https://lkml.kernel.org/r/20251014192827.851389-1-joshua.hahnjy@gmail.com
    Link: https://lkml.kernel.org/r/20251014145011.3427205-4-joshua.hahnjy@gmail.com
    Signed-off-by: Joshua Hahn <joshua.hahnjy@gmail.com>
    Suggested-by: Chris Mason <clm@fb.com>
    Co-developed-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: "Kirill A. Shutemov" <kirill@shutemov.name>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: SeongJae Park <sj@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Liangyan <liangyan.peng@bytedance.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3419,12 +3419,22 @@ static int nr_pcp_high(struct per_cpu_pages *pcp, struct zone *zone,
 	return high;
 }
 
-static void free_unref_page_commit(struct zone *zone, struct per_cpu_pages *pcp,
+/*
+ * Tune pcp alloc factor and adjust count & free_count. Free pages to bring the
+ * pcp's watermarks below high.
+ *
+ * May return a freed pcp, if during page freeing the pcp spinlock cannot be
+ * reacquired. Return true if pcp is locked, false otherwise.
+ */
+static bool free_unref_page_commit(struct zone *zone, struct per_cpu_pages *pcp,
 				   struct page *page, int migratetype,
-				   unsigned int order)
+				   unsigned int order, unsigned long *UP_flags)
 {
 	int high, batch;
+	int to_free, to_free_batched;
 	int pindex;
+	int cpu = smp_processor_id();
+	int ret = true;
 	bool free_high = false;
 
 	/*
@@ -3457,14 +3467,46 @@ static void free_unref_page_commit(struct zone *zone, struct per_cpu_pages *pcp,
 	if (pcp->free_count < (batch << CONFIG_PCP_BATCH_SCALE_MAX))
 		pcp->free_count += (1 << order);
 	high = nr_pcp_high(pcp, zone, batch, free_high);
-	if (pcp->count >= high) {
-		free_pcppages_bulk(zone, nr_pcp_free(pcp, batch, high, free_high),
-				   pcp, pindex);
-		if (test_bit(ZONE_BELOW_HIGH, &zone->flags) &&
+	if (pcp->count < high)
+		return true;
+
+	to_free = nr_pcp_free(pcp, batch, high, free_high);
+	while (to_free > 0 && pcp->count > 0) {
+		to_free_batched = min(to_free, batch);
+		free_pcppages_bulk(zone, to_free_batched, pcp, pindex);
+		to_free -= to_free_batched;
+
+		if (to_free == 0 || pcp->count == 0)
+			break;
+
+		pcp_spin_unlock(pcp);
+		pcp_trylock_finish(*UP_flags);
+
+		pcp_trylock_prepare(*UP_flags);
+		pcp = pcp_spin_trylock(zone->per_cpu_pageset);
+		if (!pcp) {
+			pcp_trylock_finish(*UP_flags);
+			ret = false;
+			break;
+		}
+
+		/*
+		 * Check if this thread has been migrated to a different CPU.
+		 * If that is the case, give up and indicate that the pcp is
+		 * returned in an unlocked state.
+		 */
+		if (smp_processor_id() != cpu) {
+			pcp_spin_unlock(pcp);
+			pcp_trylock_finish(*UP_flags);
+			ret = false;
+			break;
+		}
+	}
+	if (test_bit(ZONE_BELOW_HIGH, &zone->flags) &&
 		    zone_watermark_ok(zone, 0, high_wmark_pages(zone),
 				      ZONE_MOVABLE, 0))
 			clear_bit(ZONE_BELOW_HIGH, &zone->flags);
-	}
+	return ret;
 }
 
 /*
@@ -3501,7 +3543,9 @@ void free_unref_page(struct page *page, unsigned int order)
 	pcp_trylock_prepare(UP_flags);
 	pcp = pcp_spin_trylock(zone->per_cpu_pageset);
 	if (pcp) {
-		free_unref_page_commit(zone, pcp, page, pcpmigratetype, order);
+		if (!free_unref_page_commit(zone, pcp, page, pcpmigratetype,
+						order, &UP_flags))
+			return;
 		pcp_spin_unlock(pcp);
 	} else {
 		free_one_page(zone, page, pfn, order, migratetype, FPI_NONE);
@@ -3579,14 +3623,18 @@ void free_unref_page_list(struct list_head *list)
 			migratetype = MIGRATE_MOVABLE;
 
 		trace_mm_page_free_batched(page);
-		free_unref_page_commit(zone, pcp, page, migratetype, 0);
+		if (!free_unref_page_commit(zone, pcp, page, migratetype,
+						0, &UP_flags)) {
+			pcp = NULL;
+			locked_zone = NULL;
+		}
 
 		/*
 		 * Guard against excessive lock hold times when freeing
 		 * a large list of pages. Lock will be reacquired if
 		 * necessary on the next iteration.
 		 */
-		if (++batch_count == SWAP_CLUSTER_MAX) {
+		if (++batch_count == SWAP_CLUSTER_MAX && pcp) {
 			pcp_spin_unlock(pcp);
 			pcp_trylock_finish(UP_flags);
 			batch_count = 0;
