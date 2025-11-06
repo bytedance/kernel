90ce948965dc437872ff975b55084dff5f11c6ae
Zeng Heng <zengheng4@huawei.com>
2025-11-06 15:46:09 +0800

    downstream: fs/resctrl: Fix the L3 CMAX conversion mechanism
    RDT formats the precision through bw_validate() during the validity check.
    But MPAM does not rely on such conversion, the CMAX interface converts
    percentages through percent_to_ca_max() and ca_max_to_percent(). So skip
    the conversion in bw_validate() to avoid loss of precision.
    
    Finally, percent_to_ca_max() and ca_max_to_percent() are restored to round
    up the user configuration to the required precision, avoiding to pass
    values smaller than the minimum threshold as parameters.
    
    Fixes: 642d6efc60a8 ("downstream: arm64/mpam: Add quirk for cmax and cmin")
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    Signed-off-by: Quanmin Yan <yanquanmin1@huawei.com>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- drivers/platform/mpam/mpam_resctrl.c
+++ drivers/platform/mpam/mpam_resctrl.c
@@ -642,7 +642,7 @@ static u16 percent_to_mbw_max(u32 pc, u8 wd)
 static u16 percent_to_ca_max(u32 pc, u8 wd)
 {
 	struct rdt_resource *l3 = resctrl_arch_get_resource(RDT_RESOURCE_L3);
-	u32 valid_max;
+	u32 valid_max, ca_max;
 
 	if (read_cpuid_implementor() != ARM_CPU_IMP_HISI)
 		return percent_to_mbw_max(pc, wd);
@@ -654,7 +654,8 @@ static u16 percent_to_ca_max(u32 pc, u8 wd)
 	if (pc >= MAX_MBA_BW)
 		return valid_max << (16 - wd);
 
-	return ((pc * valid_max + 50) / 100) << (16 - wd);
+	ca_max = DIV_ROUND_UP(pc * valid_max, 100);
+	return ca_max << (16 - wd);
 }
 
 static u16 ca_max_to_percent(u16 ca_max, u8 wd)
@@ -673,7 +674,7 @@ static u16 ca_max_to_percent(u16 ca_max, u8 wd)
 	if (ca_max >= valid_max)
 		return MAX_MBA_BW;
 
-	return (ca_max * 100 + valid_max / 2) / valid_max;
+	return (ca_max * 100) / valid_max;
 }
 
 /* Test whether we can export MPAM_CLASS_CACHE:{2,3}? */
--- fs/resctrl/ctrlmondata.c
+++ fs/resctrl/ctrlmondata.c
@@ -69,7 +69,11 @@ static bool bw_validate(char *buf, u32 *data, struct rdt_resource *r)
 		return false;
 	}
 
-	*data = roundup(bw, (unsigned long)r->membw.bw_gran);
+	if (!IS_ENABLED(CONFIG_ARM64_MPAM))
+		*data = roundup(bw, (unsigned long)r->membw.bw_gran);
+	else
+		*data = bw;
+
 	return true;
 }
 
