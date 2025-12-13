d29481fd40b751208d611d0823c947ec9b8eb07e
Slim6882 <yangjunshuo@huawei.com>
2025-12-13 17:10:55 +0800

    downstream: ACPI: AGDI: add sysfs entry to dump more information
    This patch creates three sysfs interface for agdi to support:
    1. whether the system will panic when enable/disable "panic_enable"
    2. support show memory usage
    3. support show all cpus backtrace
    
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Rui Qi <qirui.001@bytedance.com>

--- drivers/acpi/arm64/agdi.c
+++ drivers/acpi/arm64/agdi.c
@@ -12,16 +12,179 @@
 #include <linux/arm_sdei.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
+#include <asm/irq_regs.h>
+#include <linux/nmi.h>
+#include <linux/sched/debug.h>
+#include <linux/irq_work.h>
+#include <linux/workqueue.h>
+#include <linux/sysctl.h>
 #include <linux/platform_device.h>
 #include "init.h"
+
+static int panic_on_acpi_nmi = 1;
+static int showmem_on_acpi_nmi = 1;
+static int showcpus_on_acpi_nmi = 1;
+
+static struct irq_work dump_irq_work;
+/* Fallback when NMI is not supported */
+static struct work_struct show_cpus_work;
+
+#ifdef CONFIG_SYSCTL
+#include <linux/sysctl.h>
+static struct ctl_table_header *hdr;
+
+static struct ctl_table acpi_nmi_table[] = {
+	{
+		.procname	= "panic_enable",
+		.data		= &panic_on_acpi_nmi,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "show_mem_usage_enable",
+		.data		= &showmem_on_acpi_nmi,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "show_allcpus_backtrace_enable",
+		.data		= &showcpus_on_acpi_nmi,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{ }
+};
+static int register_nmi_sysctls(void)
+{
+	if (!hdr) {
+		hdr = register_sysctl("fs/acpi_nmi", acpi_nmi_table);
+		if (!hdr)
+			return -ENOMEM;
+	}
+	return 0;
+}
+
+static void unregister_nmi_sysctls(void)
+{
+	if (hdr)
+		unregister_sysctl_table(hdr);
+	hdr = NULL;
+}
+
+#else
+static int register_nmi_sysctls(void) { return 0; }
+static void unregister_nmi_sysctls(void) {}
+#endif
 
 struct agdi_data {
 	int sdei_event;
 };
 
+static void show_all_cpus(void)
+{
+	/*
+	 * Fall back to the workqueue based printing if the
+	 * backtrace printing did not succeed or the
+	 * architecture has no support for it:
+	 */
+	if (!trigger_all_cpu_backtrace()) {
+		struct pt_regs *regs = NULL;
+		int cpu;
+
+		preempt_disable();
+		cpu = smp_processor_id();
+		preempt_enable();
+
+		if (idle_cpu(cpu)) {
+			pr_emerg("CPU%d: backtrace skipped as idling\n", cpu);
+			return;
+		}
+
+		if (in_irq())
+			regs = get_irq_regs();
+
+		pr_emerg("CPU%d:\n", cpu);
+		if (regs)
+			show_regs(regs);
+		else
+			show_stack(NULL, NULL, KERN_DEFAULT);
+
+		queue_work(system_highpri_wq, &show_cpus_work);
+	}
+}
+
+static void show_memory(void)
+{
+	show_mem();
+}
+
+static void dump_in_irq(struct irq_work *irq_work)
+{
+	int old_lvl;
+
+	old_lvl = console_loglevel;
+	console_loglevel = CONSOLE_LOGLEVEL_MOTORMOUTH;
+	pr_emerg("Start dumping, triggered by BMC");
+	if (showmem_on_acpi_nmi)
+		show_memory();
+
+	if (showcpus_on_acpi_nmi)
+		show_all_cpus();
+
+	if (panic_on_acpi_nmi)
+		nmi_panic(NULL, "Panic triggered by BMC!!\n");
+
+	console_loglevel = old_lvl;
+}
+
+static DEFINE_RAW_SPINLOCK(show_lock);
+
+static void showacpu(void *dummy)
+{
+	unsigned long flags;
+	int old_lvl;
+
+	old_lvl = console_loglevel;
+	/* Idle CPUs have no interesting backtrace. */
+	if (idle_cpu(smp_processor_id())) {
+		console_loglevel = CONSOLE_LOGLEVEL_MOTORMOUTH;
+		pr_emerg("CPU%d: backtrace skipped as idling\n", smp_processor_id());
+		console_loglevel = old_lvl;
+		return;
+	}
+
+	raw_spin_lock_irqsave(&show_lock, flags);
+	console_loglevel = CONSOLE_LOGLEVEL_MOTORMOUTH;
+	pr_emerg("CPU%d:\n", smp_processor_id());
+	show_stack(NULL, NULL, KERN_DEFAULT);
+	console_loglevel = old_lvl;
+	raw_spin_unlock_irqrestore(&show_lock, flags);
+}
+
+static void showcpus_fb(struct work_struct *work)
+{
+	smp_call_function(showacpu, NULL, 0);
+}
+
+static void init_work_queue(void)
+{
+	init_irq_work(&dump_irq_work, dump_in_irq);
+	INIT_WORK(&show_cpus_work, showcpus_fb);
+}
+
 static int agdi_sdei_handler(u32 sdei_event, struct pt_regs *regs, void *arg)
 {
-	nmi_panic(regs, "Arm Generic Diagnostic Dump and Reset SDEI event issued");
+	pr_debug("SDEI callback\n");
+	irq_work_queue(&dump_irq_work);
 	return 0;
 }
 
@@ -37,6 +200,8 @@ static int agdi_sdei_probe(struct platform_device *pdev,
 		return err;
 	}
 
+	init_work_queue();
+
 	err = sdei_event_enable(adata->sdei_event);
 	if (err)  {
 		sdei_event_unregister(adata->sdei_event);
@@ -44,6 +209,16 @@ static int agdi_sdei_probe(struct platform_device *pdev,
 			adata->sdei_event);
 		return err;
 	}
+
+    /* Register sysctl interface */
+	err = register_nmi_sysctls();
+	if (err) {
+		dev_err(&pdev->dev, "Failed to register sysctl interface\n");
+		sdei_event_unregister(adata->sdei_event);
+		return err;
+	}
+
+	pr_debug("sdei-event #%d registered\n", adata->sdei_event);
 
 	return 0;
 }
@@ -62,6 +237,8 @@ static int agdi_remove(struct platform_device *pdev)
 {
 	struct agdi_data *adata = dev_get_platdata(&pdev->dev);
 	int err, i;
+
+	unregister_nmi_sysctls();
 
 	err = sdei_event_disable(adata->sdei_event);
 	if (err) {
