a21a272969864596769ad48fd59793d4f7ddd6c9
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2026-01-28 10:55:23 +0100

    iommu/amd: Use core's primary handler and set IRQF_ONESHOT
    commit 5bfcdccb4d18d3909b7f87942be67fd6bdc00c1d upstream.
    
    request_threaded_irq() is invoked with a primary and a secondary handler
    and no flags are passed. The primary handler is the same as
    irq_default_primary_handler() so there is no need to have an identical
    copy.
    
    The lack of the IRQF_ONESHOT can be dangerous because the interrupt
    source is not masked while the threaded handler is active. This means,
    especially on LEVEL typed interrupt lines, the interrupt can fire again
    before the threaded handler had a chance to run.
    
    Use the default primary interrupt handler by specifying NULL and set
    IRQF_ONESHOT so the interrupt source is masked until the secondary
    handler is done.
    
    Fixes: 72fe00f01f9a3 ("x86/amd-iommu: Use threaded interupt handler")
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Link: https://patch.msgid.link/20260128095540.863589-4-bigeasy@linutronix.de
    Signed-off-by: Jinmeng Zhou <zhoujinmeng@bytedance.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -921,11 +921,6 @@ irqreturn_t amd_iommu_int_thread(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
-irqreturn_t amd_iommu_int_handler(int irq, void *data)
-{
-	return IRQ_WAKE_THREAD;
-}
-
 /****************************************************************************
  *
  * IOMMU command queuing functions
