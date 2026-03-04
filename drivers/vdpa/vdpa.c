095e67aa74c3c30b3f386223375315aed1c63dff
Lin Ma <linma@zju.edu.cn>
2026-03-04 20:05:20 +0800

    vdpa: Add max vqp attr to vdpa_nl_policy for nlattr length check
    [ Upstream commit 5d6ba607d6cb5c58a4ddf33381e18c83dbb4098f ]
    
    The vdpa_nl_policy structure is used to validate the nlattr when parsing
    the incoming nlmsg. It will ensure the attribute being described produces
    a valid nlattr pointer in info->attrs before entering into each handler
    in vdpa_nl_ops.
    
    That is to say, the missing part in vdpa_nl_policy may lead to illegal
    nlattr after parsing, which could lead to OOB read just like CVE-2023-3773.
    
    This patch adds the missing nla_policy for vdpa max vqp attr to avoid
    such bugs.
    
    Fixes: ad69dd0bf26b ("vdpa: Introduce query of device config layout")
    Signed-off-by: Lin Ma <linma@zju.edu.cn>
    Cc: stable@vger.kernel.org
    Message-Id: <20230727175757.73988-7-dtatulea@nvidia.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    [ Minor context conflict resolved. ]
    Signed-off-by: Alva Lan <alvalan9@foxmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/vdpa/vdpa.c
+++ drivers/vdpa/vdpa.c
@@ -686,6 +686,7 @@ static const struct nla_policy vdpa_nl_policy[VDPA_ATTR_MAX + 1] = {
 	[VDPA_ATTR_MGMTDEV_DEV_NAME] = { .type = NLA_STRING },
 	[VDPA_ATTR_DEV_NAME] = { .type = NLA_STRING },
 	[VDPA_ATTR_DEV_DEL_TIMEOUT] = { .type = NLA_U16 },
+	[VDPA_ATTR_DEV_NET_CFG_MAX_VQP] = { .type = NLA_U16 },
 };
 
 static const struct genl_ops vdpa_nl_ops[] = {
--- include/uapi/linux/vdpa.h
+++ include/uapi/linux/vdpa.h
@@ -34,6 +34,7 @@ enum vdpa_attr {
 	VDPA_ATTR_DEV_MAX_VQ_SIZE,		/* u16 */
 
 	VDPA_ATTR_DEV_DEL_TIMEOUT,		/* u16 */
+	VDPA_ATTR_DEV_NET_CFG_MAX_VQP,		/* u16 */
 
 	/* new attributes must be added above here */
 	VDPA_ATTR_MAX,
