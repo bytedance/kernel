806793f3ccebda0bc3ed9d235ca17d6beb74187e
Guangshuo Li <lgs201920130244@gmail.com>
2026-03-31 15:40:19 -0400

    net: mana: fix use-after-free in add_adev() error path
    [ Upstream commit c4ea7d8907cf72b259bf70bd8c2e791e1c4ff70f ]
    
    If auxiliary_device_add() fails, add_adev() jumps to add_fail and calls
    auxiliary_device_uninit(adev).
    
    The auxiliary device has its release callback set to adev_release(),
    which frees the containing struct mana_adev. Since adev is embedded in
    struct mana_adev, the subsequent fall-through to init_fail and access
    to adev->id may result in a use-after-free.
    
    Fix this by saving the allocated auxiliary device id in a local
    variable before calling auxiliary_device_add(), and use that saved id
    in the cleanup path after auxiliary_device_uninit().
    
    Fixes: a69839d4327d ("net: mana: Add support for auxiliary device")
    Cc: stable@vger.kernel.org
    Reviewed-by: Long Li <longli@microsoft.com>
    Signed-off-by: Guangshuo Li <lgs201920130244@gmail.com>
    Link: https://patch.msgid.link/20260323165730.945365-1-lgs201920130244@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/microsoft/mana/mana_en.c
+++ drivers/net/ethernet/microsoft/mana/mana_en.c
@@ -2760,6 +2760,7 @@ static int add_adev(struct gdma_dev *gd)
 	struct auxiliary_device *adev;
 	struct mana_adev *madev;
 	int ret;
+	int id;
 
 	madev = kzalloc(sizeof(*madev), GFP_KERNEL);
 	if (!madev)
@@ -2769,7 +2770,8 @@ static int add_adev(struct gdma_dev *gd)
 	ret = mana_adev_idx_alloc();
 	if (ret < 0)
 		goto idx_fail;
-	adev->id = ret;
+	id = ret;
+	adev->id = id;
 
 	adev->name = "rdma";
 	adev->dev.parent = gd->gdma_context->dev;
@@ -2793,7 +2795,7 @@ add_fail:
 	auxiliary_device_uninit(adev);
 
 init_fail:
-	mana_adev_idx_free(adev->id);
+	mana_adev_idx_free(id);
 
 idx_fail:
 	kfree(madev);
