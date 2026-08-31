intel/td-partitioning

x86/virt/tdx: Skip sysfs registration if it is already done
Sysfs registration is called from TDX module initialization. But with TDX
module update support, initialization will happen after every update. So,
skip sysfs registration if it is already done to suppress a kernel warning
that the sysfs is already registered.

Signed-off-by: Chao Gao <chao.gao@intel.com>

arch/x86/virt/vmx/tdx/tdx.c
@@ -1944,7 +1944,7 @@ static int tdx_sysfs_init(void)
 {
 	int ret;
 
-	if (!sysinfo)
+	if (!sysinfo || tdx_kobj)
 		return 0;
 
 	tdx_kobj = kobject_create_and_add("tdx", firmware_kobj);

bytedance/kernel

Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>

arch/x86/virt/vmx/tdx/tdx.c
@@ -2108,7 +2108,7 @@ int tdx_sysfs_init(void)
 {
 	int ret;
 
-	if (!tdx_info)
+	if (!tdx_info || tdx_kobj)
 		return 0;
 
 	tdx_kobj = kobject_create_and_add("tdx", firmware_kobj);