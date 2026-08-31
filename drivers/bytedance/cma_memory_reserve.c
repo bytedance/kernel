611c2f0d1d674322ff46cd4318616ca3ab7a296d
Jinhui Guo <guojinhui.liam@bytedance.com>
2026-08-31 19:02:29 +0800

    bytedance: opensource: add all bytedance commits Copyright
    Copyright notices are added to all files involving non-upstream patches
    according to legal requirements.
    
    Signed-off-by: Jinhui Guo <guojinhui.liam@bytedance.com>

--- drivers/bytedance/cma_memory_reserve.c
+++ drivers/bytedance/cma_memory_reserve.c
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0-only
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2024, ByteDance Ltd. and/or its affiliates. All rights reserved. */
 /*
  * CMA Memory Reserve Driver - Per NUMA Node
  *
