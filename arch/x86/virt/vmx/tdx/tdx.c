intel/td-partitioning

x86/virt/tdx: Fold two sub-steps of TDMR construction into construct_tdmrs()
TDMRs don't change across TDX module updates because they are
determined based on CMRs which are constant for a system. TDMRs
can be used unless PAMT entry size is changed. So, no need to
re-construct TDMRs, i.e., call build_tdx_memlist(), alloc_tdmr_list()
and construct_tdmrs() when initializing TDX module after updates.

Fold the first two functions into construct_tdmrs() in preparation for
skipping them as a whole.

No functional change intended.

Signed-off-by: Chao Gao <chao.gao@intel.com>

arch/x86/virt/vmx/tdx/tdx.c
@@ -1042,20 +1042,35 @@ static int construct_tdmrs(struct list_head *tmb_list,
 {
 	int ret;
 
-	ret = fill_out_tdmrs(tmb_list, tdmr_list);
+	ret = build_tdx_memlist(tmb_list);
 	if (ret)
 		return ret;
+
+	/* Allocate enough space for constructing TDMRs */
+	ret = alloc_tdmr_list(tdmr_list, sysinfo);
+	if (ret)
+		return ret;
+
+	ret = fill_out_tdmrs(tmb_list, tdmr_list);
+	if (ret)
+		goto free_tdmrs;
 
 	ret = tdmrs_set_up_pamt_all(tdmr_list, tmb_list,
 			sysinfo->pamt_entry_size);
 	if (ret)
-		return ret;
+		goto free_tdmrs;
 
 	ret = tdmrs_populate_rsvd_areas_all(tdmr_list, tmb_list,
 			sysinfo->max_reserved_per_tdmr);
 	if (ret)
-		tdmrs_free_pamt_all(tdmr_list);
+		goto free_pamts;
 
+	return 0;
+
+free_pamts:
+	tdmrs_free_pamt_all(tdmr_list);
+free_tdmrs:
+	free_tdmr_list(tdmr_list);
 	return ret;
 }
 
@@ -1238,19 +1253,10 @@ static int init_tdx_module(void)
 	 */
 	get_online_mems();
 
-	ret = build_tdx_memlist(&tdx_memlist);
-	if (ret)
-		goto out_put_tdxmem;
-
-	/* Allocate enough space for constructing TDMRs */
-	ret = alloc_tdmr_list(&tdx_tdmr_list, sysinfo);
-	if (ret)
-		goto out_free_tdxmem;
-
 	/* Cover all TDX-usable memory regions in TDMRs */
 	ret = construct_tdmrs(&tdx_memlist, &tdx_tdmr_list, sysinfo);
 	if (ret)
-		goto out_free_tdmrs;
+		goto out_put_tdxmem;
 
 	/* Pass the TDMRs and the global KeyID to the TDX module */
 	ret = config_tdx_module(&tdx_tdmr_list, tdx_global_keyid);
@@ -1325,9 +1331,7 @@ out_reset_pamts:
 	atomic_dec_return(&tdx_may_has_private_mem);
 out_free_pamts:
 	tdmrs_free_pamt_all(&tdx_tdmr_list);
-out_free_tdmrs:
 	free_tdmr_list(&tdx_tdmr_list);
-out_free_tdxmem:
 	free_tdx_memlist(&tdx_memlist);
 out_put_tdxmem:
 	put_online_mems();

bytedance/kernel

Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>

arch/x86/virt/vmx/tdx/tdx.c
@@ -1026,19 +1026,35 @@ static int construct_tdmrs(struct list_head *tmb_list,
 {
 	int ret;
 
-	ret = fill_out_tdmrs(tmb_list, tdmr_list);
+	ret = build_tdx_memlist(tmb_list);
 	if (ret)
 		return ret;
+
+	/* Allocate enough space for constructing TDMRs */
+	ret = alloc_tdmr_list(tdmr_list, tdmr_sysinfo);
+	if (ret)
+		return ret;
+
+	ret = fill_out_tdmrs(tmb_list, tdmr_list);
+	if (ret)
+		goto free_tdmrs;
 
 	ret = tdmrs_set_up_pamt_all(tdmr_list, tmb_list,
 			tdmr_sysinfo->pamt_entry_size);
 	if (ret)
-		return ret;
+	goto free_tdmrs;
 
 	ret = tdmrs_populate_rsvd_areas_all(tdmr_list, tmb_list,
 			tdmr_sysinfo->max_reserved_per_tdmr);
 	if (ret)
-		tdmrs_free_pamt_all(tdmr_list);
+		goto free_pamts;
+
+	return 0;
+
+free_pamts:
+	tdmrs_free_pamt_all(tdmr_list);
+free_tdmrs:
+	free_tdmr_list(tdmr_list);
 
 	/*
 	 * The tdmr_info_list is read-only from here on out.
@@ -1228,19 +1244,10 @@ static int init_tdx_module(void)
 	 */
 	get_online_mems();
 
-	ret = build_tdx_memlist(&tdx_memlist);
-	if (ret)
-		goto out_put_tdxmem;
-
-	/* Allocate enough space for constructing TDMRs */
-	ret = alloc_tdmr_list(&tdx_tdmr_list, &sysinfo.tdmr_info);
-	if (ret)
-		goto err_free_tdxmem;
-
 	/* Cover all TDX-usable memory regions in TDMRs */
 	ret = construct_tdmrs(&tdx_memlist, &tdx_tdmr_list, &sysinfo.tdmr_info);
 	if (ret)
-		goto err_free_tdmrs;
+		goto out_put_tdxmem;
 
 	/* Pass the TDMRs and the global KeyID to the TDX module */
 	ret = config_tdx_module(&tdx_tdmr_list, tdx_global_keyid);
@@ -1292,9 +1299,7 @@ err_reset_pamts:
 	mark_may_have_private_memory(false);
 err_free_pamts:
 	tdmrs_free_pamt_all(&tdx_tdmr_list);
-err_free_tdmrs:
 	free_tdmr_list(&tdx_tdmr_list);
-err_free_tdxmem:
 	free_tdx_memlist(&tdx_memlist);
 	goto out_put_tdxmem;
 }